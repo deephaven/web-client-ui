9e0f7f4b1a5fa31c91a13ee1d470cf0b302359c8
Davor Spasovski <davor@spasovski.net>
2021-05-19 18:48:50 -0500

    convert util Pending to TS

--- packages/utils/src/Pending.ts
+++ packages/utils/src/Pending.ts
@@ -1,23 +1,33 @@
 import PromiseUtils from './PromiseUtils';
 
+interface CancelablePromise<T> extends Promise<T> {
+  cancel: () => void;
+}
+
 /**
  * Helper class for managing pending promises.
  * IDS-6391 When cancel is called, it calls cancel on ALL promises, including ones that have already resolved.
  * This behaviour may change when IDS-6806 is implemented.
  */
 class Pending {
-  pending = [];
+  pending: Array<CancelablePromise<unknown>> = [];
 
-  resolved = [];
+  resolved: Array<CancelablePromise<unknown>> = [];
 
   /**
    * Begins tracking a promise. After the promise has resolved, it's removed from tracking.
-   * @param {any} item Item to track.
-   * @param {function} cleanup The cleanup function to use when the promise is cancelled
+   * @param item Item to track.
+   * @param cleanup The cleanup function to use when the promise is cancelled
    * @returns CancelablePromise Returns a cancelable promise.
    */
-  add(item, cleanup = null) {
-    const promise = PromiseUtils.makeCancelable(item, cleanup);
+  add(
+    item: unknown,
+    cleanup: (val: unknown) => void | null
+  ): CancelablePromise<unknown> {
+    const promise = PromiseUtils.makeCancelable(
+      item,
+      cleanup
+    ) as CancelablePromise<unknown>;
     this.pending.push(promise);
     promise.then(
       () => {
@@ -34,7 +44,7 @@ class Pending {
    * Remove a promise from tracking.
    * @param {Promise} promise Promise to stop tracking
    */
-  remove(promise) {
+  remove(promise: Promise<unknown>): void {
     for (let i = 0; i < this.pending.length; i += 1) {
       if (this.pending[i] === promise) {
         this.pending.splice(i, 1);
@@ -43,7 +53,7 @@ class Pending {
     }
   }
 
-  resolve(promise) {
+  resolve(promise: CancelablePromise<unknown>): void {
     this.remove(promise);
     this.resolved.push(promise);
   }
@@ -51,7 +61,7 @@ class Pending {
   /**
    * Cancel all pending promises and remove them from tracking.
    */
-  cancel() {
+  cancel(): void {
     const allPromises = [...this.pending, ...this.resolved];
     for (let i = 0; i < allPromises.length; i += 1) {
       allPromises[i].cancel();
