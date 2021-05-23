8442b49a4010d8c6d72e44fbd84d6502cfbf0584
Davor Spasovski <davor@spasovski.net>
2021-05-23 15:46:13 -0500

    address review comments

--- packages/utils/src/PromiseUtils.ts
+++ packages/utils/src/PromiseUtils.ts
@@ -1,24 +1,27 @@
 import CanceledPromiseError from './CanceledPromiseError';
 import TimeoutError from './TimeoutError';
 
-interface CancelablePromise<T> extends Promise<T> {
+export interface CancelablePromise<T> extends Promise<T> {
   cancel: () => void;
 }
 
 class PromiseUtils {
-  static isCancelablePromise(promise: CancelablePromise<unknown>): boolean {
+  static isCancelablePromise(
+    promise: unknown
+  ): promise is CancelablePromise<unknown> {
+    const cancelablePromise = promise as CancelablePromise<unknown>;
     return (
       this.isPromise(promise) &&
-      promise.cancel != null &&
-      typeof promise.cancel === 'function'
+      cancelablePromise.cancel != null &&
+      typeof cancelablePromise.cancel === 'function'
     );
   }
 
-  static isPromise(promise: Promise<unknown>): boolean {
+  static isPromise(promise: unknown): promise is Promise<unknown> {
     return (
       promise != null &&
-      promise.then != null &&
-      typeof promise.then === 'function'
+      (promise as Promise<unknown>).then != null &&
+      typeof (promise as Promise<unknown>).then === 'function'
     );
   }
 
@@ -29,7 +32,7 @@ class PromiseUtils {
    * @param cleanup Function to cleanup the resolved item after cancelation. Called after both this promise is cancelled and the wrapped item was resolved (order does not matter).
    */
   static makeCancelable<T>(
-    promise: Promise<T>,
+    promise: Promise<T> | T,
     cleanup: (val: T) => void | null
   ): CancelablePromise<T> {
     let hasCanceled = false;
