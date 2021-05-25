57745cdd77451f5fd7b4350e9f4ad331c5325193
Davor Spasovski <davor@spasovski.net>
2021-05-25 14:40:46 -0500

    re-add makeCancelable test

---
+++ packages/utils/src/PromiseUtils.test.js
@@ -0,0 +1,13 @@
+import PromiseUtils from './PromiseUtils';
+
+it('wraps promises properly', () => {
+  const makeCancelable = item => {
+    // Need to add a catch block or the tests crash.
+    // https://github.com/facebook/jest/issues/5311
+    const cancelable = PromiseUtils.makeCancelable(item);
+    cancelable.catch(() => {});
+    return cancelable;
+  };
+
+  expect(typeof makeCancelable().cancel).toEqual('function');
+});
