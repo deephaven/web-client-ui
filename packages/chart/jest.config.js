a82dd0b918e6af0958b1a036b921cc7e998585f5
mikebender <mikebender@deephaven.io>
2021-05-25 14:10:38 -0400

    Fix up unit test errors

--- packages/chart/jest.config.js
+++ packages/chart/jest.config.js
@@ -6,10 +6,6 @@ module.exports = {
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js|jsx)$', // Everything in __tests__ or every .test|spec.ts|tsx|js
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   moduleNameMapper: {
-    '^monaco-editor/esm/vs/editor/editor.api$':
-      '<rootDir>/src/__mocks__/monaco-editor.js',
-    '^monaco-editor/esm/vs/editor/(.*)':
-      '<rootDir>/src/__mocks__/monaco-editor-empty.js',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/__mocks__/fileMock.js',
