90d999e5f774553e1c910b34c8778a775127992b
Matthew Runyon <matthewrunyon@deephaven.io>
2021-06-08 18:00:19 -0500

    ThemeExport TS

--- packages/components/src/ThemeExport.ts
+++ packages/components/src/ThemeExport.ts
@@ -1,5 +1,7 @@
 import ThemeExport from './ThemeExport.module.scss';
 
+// Note that TypeScript does not know what keys exist on ThemeExport.module.scss
+// It only knows it's an object with strings for keys and values
 export default Object.freeze({
   transitionMs: 150,
   transitionMidMs: 200,
