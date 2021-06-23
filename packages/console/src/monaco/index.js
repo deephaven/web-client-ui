e89c9014104184f18bb097ac3481131e6ca64043
mikebender <mikebender@deephaven.io>
2021-06-23 16:39:27 -0400

    Fix linting error from last checkin
    Some formatting issues

--- packages/console/src/monaco/index.js
+++ packages/console/src/monaco/index.js
@@ -1,3 +1,3 @@
-export { default as MonacoUtils } from './MonacoUtils'
-export { default as MonacoCompletionProvider } from './MonacoCompletionProvider'
-export { default as MonacoTheme } from './MonacoTheme.module.scss'
+export { default as MonacoUtils } from './MonacoUtils';
+export { default as MonacoCompletionProvider } from './MonacoCompletionProvider';
+export { default as MonacoTheme } from './MonacoTheme.module.scss';
