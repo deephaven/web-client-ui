52aef3b30e6eac2423e97291f824d0016735d86d
Matthew Runyon <matthewrunyon@deephaven.io>
2021-06-22 19:18:53 -0500

    Console shortcuts

--- packages/console/src/ConsoleShortcuts.ts
+++ packages/console/src/ConsoleShortcuts.ts
@@ -30,9 +30,38 @@ const CONSOLE = {
   }),
 };
 
+const COMMAND_HISTORY = {
+  name: 'Command History',
+  FOCUS_SEARCH: ShortcutRegistry.createAndAdd({
+    id: 'COMMAND_HISTORY.FOCUS_SEARCH',
+    name: 'Focus Search',
+    shortcut: [MODIFIER.CTRL, KEY.H],
+    macShortcut: [MODIFIER.CTRL, MODIFIER.OPTION, KEY.H],
+  }),
+  SEND_TO_CONSOLE: ShortcutRegistry.createAndAdd({
+    id: 'COMMAND_HISTORY.SEND_TO_CONSOLE',
+    name: 'Send to Console',
+    shortcut: [KEY.ENTER],
+    macShortcut: [KEY.ENTER],
+  }),
+  SEND_TO_NOTEBOOK: ShortcutRegistry.createAndAdd({
+    id: 'COMMAND_HISTORY.SEND_TO_NOTEBOOK',
+    name: 'Send to Notebook',
+    shortcut: [MODIFIER.CTRL, KEY.ENTER],
+    macShortcut: [MODIFIER.CMD, KEY.ENTER],
+  }),
+  RUN: ShortcutRegistry.createAndAdd({
+    id: 'COMMAND_HISTORY.RUN',
+    name: 'Run',
+    shortcut: [MODIFIER.ALT, KEY.R],
+    macShortcut: [MODIFIER.OPTION, KEY.R],
+  }),
+};
+
 const SHORTCUTS = {
   CODE_STUDIO,
   CONSOLE,
+  COMMAND_HISTORY,
 };
 
 export default SHORTCUTS;
--- packages/console/src/csv/CsvOverlay.jsx
+++ packages/console/src/csv/CsvOverlay.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { ContextActions } from '@deephaven/components';
+import { ContextActions, GLOBAL_SHORTCUTS } from '@deephaven/components';
 import {
   dhFileCsv,
   dhFileDownload,
@@ -176,8 +176,7 @@ class CsvOverlay extends Component {
         title: 'Paste',
         icon: vsClippy,
         action: this.handleMenuPaste,
-        shortcut: '⌃V',
-        macShortcut: '⌘V',
+        shortcut: GLOBAL_SHORTCUTS.PASTE,
         disabled: uploadInProgress,
       },
     ];
