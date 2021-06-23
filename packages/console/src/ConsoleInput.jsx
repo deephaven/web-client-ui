31c0960d6bbc55197e12ad6793380772dd3a1c9f
James Nelson <james@wetheinter.net>
2021-06-23 10:46:53 -0600

    Revive autocomplete deephaven-core#682 (#67)
    Revive autocomplete (deephaven-core#682)

--- packages/console/src/ConsoleInput.jsx
+++ packages/console/src/ConsoleInput.jsx
@@ -6,8 +6,7 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import Log from '@deephaven/log';
 import { PromiseUtils } from '@deephaven/utils';
 import { getCommandHistoryStorage } from '@deephaven/redux';
-import MonacoUtils from './monaco/MonacoUtils';
-import MonacoTheme from './monaco/MonacoTheme.module.scss';
+import { MonacoCompletionProvider, MonacoTheme, MonacoUtils } from './monaco';
 import './ConsoleInput.scss';
 import StoragePropTypes from './StoragePropTypes';
 
@@ -42,7 +41,7 @@ export class ConsoleInput extends PureComponent {
     this.state = {
       commandEditorHeight: LINE_HEIGHT,
       isFocused: false,
-      // model: null,
+      model: null,
     };
   }
 
@@ -89,7 +88,7 @@ export class ConsoleInput extends PureComponent {
   }
 
   initCommandEditor() {
-    const { language } = this.props;
+    const { language, session } = this.props;
     const commandSettings = {
       copyWithSyntaxHighlighting: 'false',
       cursorStyle: 'block',
@@ -124,7 +123,7 @@ export class ConsoleInput extends PureComponent {
     );
 
     MonacoUtils.setEOL(this.commandEditor);
-    // MonacoUtils.openDocument(this.commandEditor, session);
+    MonacoUtils.openDocument(this.commandEditor, session);
 
     this.commandEditor.onDidChangeModelContent(() => {
       this.isCommandModified = true;
@@ -214,13 +213,13 @@ export class ConsoleInput extends PureComponent {
 
     this.updateDimensions();
 
-    // this.setState({ model: this.commandEditor.getModel() });
+    this.setState({ model: this.commandEditor.getModel() });
   }
 
   destroyCommandEditor() {
-    // const { session } = this.props;
+    const { session } = this.props;
     if (this.commandEditor) {
-      // MonacoUtils.closeDocument(this.commandEditor, session);
+      MonacoUtils.closeDocument(this.commandEditor, session);
       this.commandEditor.dispose();
       this.commandEditor = null;
     }
@@ -379,7 +378,8 @@ export class ConsoleInput extends PureComponent {
   }
 
   render() {
-    const { commandEditorHeight, isFocused } = this.state;
+    const { language, session } = this.props;
+    const { commandEditorHeight, isFocused, model } = this.state;
     return (
       <div className="console-input-wrapper">
         <div
@@ -392,6 +392,13 @@ export class ConsoleInput extends PureComponent {
             ref={this.commandContainer}
             style={{ height: commandEditorHeight }}
           />
+          {model && (
+            <MonacoCompletionProvider
+              model={model}
+              language={language}
+              session={session}
+            />
+          )}
         </div>
       </div>
     );
