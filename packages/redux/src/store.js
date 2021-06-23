b382fb69baba5558675abad3ce8c0291c1cbd872
mikebender <mikebender@deephaven.io>
2021-06-23 15:40:22 -0400

    Fix redux store error

--- packages/redux/src/store.js
+++ packages/redux/src/store.js
@@ -1,6 +1,12 @@
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import rootMiddleware from './middleware';
+import reducers from './reducers';
 import reducerRegistry from './reducerRegistry';
+
+// TODO #70: Separate all reducers into their respective modules, register from there
+Object.entries(reducers).map(([name, reducer]) =>
+  reducerRegistry.register(name, reducer)
+);
 
 /* eslint-disable-next-line no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -10,8 +16,8 @@ const store = createStore(
   composeEnhancers(applyMiddleware(...rootMiddleware))
 );
 
-reducerRegistry.setListener(reducers => {
-  store.replaceReducer(combineReducers(reducers));
+reducerRegistry.setListener(newReducers => {
+  store.replaceReducer(combineReducers(newReducers));
 });
 
 export default store;
