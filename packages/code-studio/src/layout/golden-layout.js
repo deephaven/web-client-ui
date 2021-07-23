a18b2bf11ab4c149540ed8a0f33d99d5d051c665
Matthew Runyon <matthewrunyon@deephaven.io>
2021-07-22 19:10:34 -0500

    Replaced usages of golden-layout with @deephaven/golden-layout

--- packages/code-studio/src/layout/golden-layout.js
+++ packages/code-studio/src/layout/golden-layout.js
@@ -1,7 +1,7 @@
 import './jquery';
 import './ReactDOM';
 
-import 'golden-layout';
-import 'golden-layout/src/css/goldenlayout-base.css';
-import 'golden-layout/src/css/goldenlayout-dark-theme.css';
+import '@deephaven/golden-layout';
+import '@deephaven/golden-layout/src/css/goldenlayout-base.css';
+import '@deephaven/golden-layout/src/css/goldenlayout-dark-theme.css';
 import './GoldenLayout.scss';
