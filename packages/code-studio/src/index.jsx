255564491bfb5066671aef93db70057a0317da7f
mikebender <mikebender@deephaven.io>
2021-07-28 10:27:15 -0400

    WIP trying out some things to show when the API isn't found
    Not a great solution yet

--- packages/code-studio/src/index.jsx
+++ packages/code-studio/src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import 'fira';
 import '@deephaven/components/dist/BaseStyleSheet.css';
 import { MonacoUtils } from '@deephaven/console';
+import dh from '@deephaven/jsapi-shim';
 import { store } from '@deephaven/redux';
 import AppRouter from './main/AppRouter';
 import DownloadServiceWorkerUtils from './DownloadServiceWorkerUtils';
@@ -12,10 +13,18 @@ import { unregister } from './serviceWorker';
 
 logInit();
 
+console.log('mjb adding window listeniner...');
+
+window.addEventListener('mjb error', e => console.log('window error', e));
+
 ReactDOM.render(
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>,
+  dh ? (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  ) : (
+    <div>API not found</div>
+  ),
   document.getElementById('root')
 );
 unregister();
