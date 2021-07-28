46a4255c14969952e9bf2e7c6c272617d7df9a21
Matthew Runyon <matthewrunyon@deephaven.io>
2021-07-28 15:47:19 -0500

    Fix rebase merge conflict to use updated ItemList test

--- packages/components/src/ItemList.test.tsx
+++ packages/components/src/ItemList.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 import ItemList from './ItemList';
 
 function makeItems(count = 20) {
@@ -13,20 +13,26 @@ function makeItems(count = 20) {
 }
 
 function makeItemList({
+  isDoubleClickSelect = false,
+  isMultiSelect = false,
   itemCount = 100,
   rowHeight = 20,
   offset = 0,
   items = makeItems(),
-  onSelect = () => undefined,
-  onViewportChange = () => undefined,
+  onSelect = jest.fn(),
+  onSelectionChange = jest.fn(),
+  onViewportChange = jest.fn(),
 } = {}) {
   return mount(
     <ItemList
+      isDoubleClickSelect={isDoubleClickSelect}
+      isMultiSelect={isMultiSelect}
       itemCount={itemCount}
       rowHeight={rowHeight}
       offset={offset}
       items={items}
       onSelect={onSelect}
+      onSelectionChange={onSelectionChange}
       onViewportChange={onViewportChange}
     />
   );
@@ -37,38 +43,130 @@ it('mounts and unmounts properly', () => {
   itemList.unmount();
 });
 
-it('Sends the proper signal when an item is clicked', () => {
-  const onSelect = jest.fn();
-  const itemList = makeItemList({ onSelect });
+describe('mouse', () => {
+  function clickItem(
+    itemList: ReactWrapper,
+    itemIndex: number,
+    options = {}
+  ): void {
+    itemList
+      .find('.item-list-item')
+      .at(itemIndex)
+      .simulate('mousedown', options);
 
-  itemList.find('.item-list-item').at(3).simulate('mousedown', {});
+    itemList.find('.item-list-item').at(itemIndex).simulate('mouseup', options);
+  }
 
-  itemList.find('.item-list-item').at(3).simulate('mouseup', {});
+  function doubleClickItem(
+    itemList: ReactWrapper,
+    itemIndex: number,
+    options = {}
+  ): void {
+    itemList
+      .find('.item-list-item')
+      .at(itemIndex)
+      .simulate('dblclick', options);
+  }
 
-  expect(onSelect).toHaveBeenCalledWith(3);
+  it('sends onSelect when an item is clicked', () => {
+    const onSelect = jest.fn();
+    const itemList = makeItemList({ onSelect });
 
-  itemList.unmount();
+    clickItem(itemList, 3);
+
+    expect(onSelect).toHaveBeenCalledWith(3);
+
+    itemList.unmount();
+  });
+
+  it('sends onSelect only when double clicked if isDoubleClickSelect is true', () => {
+    const onSelect = jest.fn();
+    const itemList = makeItemList({ onSelect, isDoubleClickSelect: true });
+
+    clickItem(itemList, 3);
+
+    expect(onSelect).not.toHaveBeenCalled();
+
+    doubleClickItem(itemList, 3);
+
+    expect(onSelect).toHaveBeenCalledWith(3);
+
+    itemList.unmount();
+  });
+
+  it('extends the selection when shift clicked', () => {
+    const onSelect = jest.fn();
+    const onSelectionChange = jest.fn();
+    const itemList = makeItemList({
+      isMultiSelect: true,
+      onSelect,
+      onSelectionChange,
+    });
+
+    clickItem(itemList, 3);
+
+    expect(onSelect).toHaveBeenCalledWith(3);
+    expect(onSelectionChange).toHaveBeenCalledWith([[3, 3]]);
+
+    onSelectionChange.mockClear();
+    onSelect.mockClear();
+
+    clickItem(itemList, 6, { shiftKey: true });
+
+    expect(onSelect).not.toHaveBeenCalled();
+    expect(onSelectionChange).toHaveBeenCalledWith([[3, 6]]);
+
+    itemList.unmount();
+  });
+
+  it('selects multiple items with Ctrl+Click', () => {
+    const onSelect = jest.fn();
+    const onSelectionChange = jest.fn();
+    const itemList = makeItemList({
+      isMultiSelect: true,
+      onSelect,
+      onSelectionChange,
+    });
+
+    clickItem(itemList, 3);
+
+    expect(onSelect).toHaveBeenCalledWith(3);
+    expect(onSelectionChange).toHaveBeenCalledWith([[3, 3]]);
+
+    onSelectionChange.mockClear();
+    onSelect.mockClear();
+
+    clickItem(itemList, 6, { ctrlKey: true });
+
+    expect(onSelect).not.toHaveBeenCalled();
+    expect(onSelectionChange).toHaveBeenCalledWith([
+      [3, 3],
+      [6, 6],
+    ]);
+
+    itemList.unmount();
+  });
 });
 
 it('handles keyboard up and down properly', () => {
   const itemList = makeItemList();
 
-  expect(itemList.state('keyboardIndex')).toBe(null);
+  expect(itemList.state('focusIndex')).toBe(null);
 
   itemList.simulate('keydown', { key: 'ArrowDown' });
 
-  expect(itemList.state('keyboardIndex')).toBe(0);
+  expect(itemList.state('focusIndex')).toBe(0);
 
   itemList.simulate('keydown', { key: 'ArrowDown' });
   itemList.simulate('keydown', { key: 'ArrowDown' });
   itemList.simulate('keydown', { key: 'ArrowDown' });
 
-  expect(itemList.state('keyboardIndex')).toBe(3);
+  expect(itemList.state('focusIndex')).toBe(3);
 
   itemList.simulate('keydown', { key: 'ArrowUp' });
   itemList.simulate('keydown', { key: 'ArrowUp' });
 
-  expect(itemList.state('keyboardIndex')).toBe(1);
+  expect(itemList.state('focusIndex')).toBe(1);
 
   itemList.unmount();
 });
