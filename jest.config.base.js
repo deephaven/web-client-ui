5eb385de6aecf7c84e615e01a4e20519f784d4b5
Matthew Runyon <matthewrunyon@deephaven.io>
2021-08-12 17:05:05 -0500

    Make jest config always generate URLs relative to the config file name

--- jest.config.base.js
+++ jest.config.base.js
@@ -8,15 +8,18 @@ module.exports = {
   },
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.resolve(
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.join(
+      __dirname,
       './__mocks__/fileMock.js'
     ),
-    '^monaco-editor/esm/vs/editor/editor.api$': path.resolve(
+    '^monaco-editor/esm/vs/editor/editor.api$': path.join(
+      __dirname,
       './__mocks__/monaco-editor.js'
     ),
-    '^monaco-editor/esm/vs/editor/(.*)': path.resolve(
+    '^monaco-editor/esm/vs/editor/(.*)': path.join(
+      __dirname,
       './__mocks__/monaco-editor-empty.js'
     ),
   },
-  setupFilesAfterEnv: [path.resolve('./jest.setup.js')],
+  setupFilesAfterEnv: [path.join(__dirname, './jest.setup.js')],
 };
