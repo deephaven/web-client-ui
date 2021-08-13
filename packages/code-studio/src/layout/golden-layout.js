e3d538ea497a57cde58374a73ae953ad30ba8375
Matthew Runyon <matthewrunyon@deephaven.io>
2021-08-13 16:08:12 -0500

    Update golden-layout css imports

--- packages/code-studio/src/layout/golden-layout.js
+++ packages/code-studio/src/layout/golden-layout.js
@@ -2,6 +2,6 @@ import './jquery';
 import './ReactDOM';
 
 import '@deephaven/golden-layout';
-import '@deephaven/golden-layout/src/css/goldenlayout-base.css';
-import '@deephaven/golden-layout/src/css/goldenlayout-dark-theme.css';
+import '@deephaven/golden-layout/dist/css/goldenlayout-base.css';
+import '@deephaven/golden-layout/dist/css/goldenlayout-dark-theme.css';
 import './GoldenLayout.scss';
