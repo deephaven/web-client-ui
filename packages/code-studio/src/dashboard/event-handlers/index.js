a7eacfcf1d6b7c302aa3ba42d2001332a3821403
Mike Bender <mikebender@deephaven.io>
2021-09-21 13:30:04 -0400

    Delete all the event handlers
    They've been replaced by plugins

--- packages/code-studio/src/dashboard/event-handlers/index.js
+++
@@ -1,8 +0,0 @@
-export { default as ChartEventHandler } from './ChartEventHandler';
-export { default as ConsoleEventHandler } from './ConsoleEventHandler';
-export { default as ControlEventHandler } from './ControlEventHandler';
-export { default as InputFilterEventHandler } from './InputFilterEventHandler';
-export { default as IrisGridEventHandler } from './IrisGridEventHandler';
-export { default as LayoutEventHandler } from './LayoutEventHandler';
-export { default as NotebookEventHandler } from './NotebookEventHandler';
-export { default as PandasEventHandler } from './PandasEventHandler';
