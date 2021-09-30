7bc622b6c9dcf92bbc1ed90a7a7e1d072836b84c
mikebender <mikebender@deephaven.io>
2021-09-30 15:39:31 -0400

    Fix up failing test
    Can't use type imported from outside of jest.mock

--- packages/dashboard/src/Dashboard.test.tsx
+++ packages/dashboard/src/Dashboard.test.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import * as ReactReduxType from 'react-redux';
 import { mount, ReactWrapper } from 'enzyme';
 import Dashboard, { DashboardProps } from './Dashboard';
 
 const mockDispatch = jest.fn();
 jest.mock('react-redux', () => ({
-  ...(jest.requireActual('react-redux') as typeof ReactReduxType),
+  ...(jest.requireActual('react-redux') as Record<string, unknown>),
   useDispatch: () => mockDispatch,
   useSelector: jest.fn(),
   useStore: () => ({}),
