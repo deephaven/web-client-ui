6a991e91dde917405fb0991242d7099e00702d8a
mikebender <mikebender@deephaven.io>
2021-10-04 09:28:28 -0400

    Export shortcuts from packages
    Enterprise needs to be able to import those shortcuts, or we're going to have problems registering shortcuts

--- packages/file-explorer/src/index.ts
+++ packages/file-explorer/src/index.ts
@@ -4,6 +4,7 @@ export * from './FileExplorer';
 export * from './FileListContainer';
 export * from './FileList';
 export * from './FileStorage';
+export { default as SHORTCUTS } from './FileExplorerShortcuts';
 export { default as NewItemModal } from './NewItemModal';
 export { default as FileExplorerToolbar } from './FileExplorerToolbar';
 export { default as FileUtils } from './FileUtils';
