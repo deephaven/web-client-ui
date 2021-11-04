1d874ede742636976f1479910a0238f8f9abbebf
Mike Bender <mikebender@deephaven.io>
2021-11-04 12:28:10 -0400

    Add a label check action (#273)
    - Custom bash script for speed (whole action takes <5s)
    - Checks if one of the lerna-changelog tags is present

---
+++ .github/scripts/label-check-one-of.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Checks that exactly one of the provided arguments is true
+# Iterates through all environment variables with the prefix LABEL_, expecting only one to be true
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+COUNT_TRUE=0
+ALL_LABELS=""
+
+for label in ${!LABEL_*}; do
+  ALL_LABELS+=", ${label#*_}"
+  if [ "${!label}" == "true" ] ; then
+    COUNT_TRUE=$((COUNT_TRUE + 1))
+  fi
+done
+
+if [ $COUNT_TRUE -ne 1 ] ; then
+  >&2 echo "Expected only one of the following labels, instead found ${COUNT_TRUE}: ${ALL_LABELS:2}"
+  exit 1
+fi
