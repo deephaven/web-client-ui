4846c3f00c8bc463656edcfaea81085abd1cfebe
Matthew Runyon <matthewrunyon@deephaven.io>
2021-12-16 18:25:03 -0600

    Change to TS to fix editor complaints

--- packages/jsapi-shim/src/dh.ts
+++ packages/jsapi-shim/src/dh.ts
@@ -1,10 +1,17 @@
 // The Deephaven API script isn't packaged as a module (yet), and is just included in index.html, exported to the global namespace
 // This include file is simply a wrapper so that it behaves like a module, and can be mocked easily for unit tests.
 // https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#using-global-variables
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
-// eslint-disable-next-line import/no-mutable-exports
-let dh;
+
+declare global {
+  interface Window {
+    dh: any;
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, import/no-mutable-exports
+let dh: any;
 try {
   dh = window.dh;
   if (!dh) {
