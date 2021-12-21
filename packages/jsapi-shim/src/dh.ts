c314e8005f614577081a155e16c2a786b7437961
Matthew Runyon <matthewrunyon@deephaven.io>
2021-12-21 11:40:23 -0600

    Make jsapi-shim more robust (#357)

--- packages/jsapi-shim/src/dh.ts
+++ packages/jsapi-shim/src/dh.ts
@@ -1,6 +1,12 @@
 // The Deephaven API script isn't packaged as a module (yet), and is just included in index.html, exported to the global namespace
 // This include file is simply a wrapper so that it behaves like a module, and can be mocked easily for unit tests.
 // https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#using-global-variables
-const { dh } = window;
+
+declare global {
+  // eslint-disable-next-line vars-on-top,no-var,@typescript-eslint/no-explicit-any
+  var dh: any;
+}
+
+const { dh } = globalThis;
 
 export default dh;
