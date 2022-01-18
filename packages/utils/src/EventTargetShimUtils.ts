c355aac1e934a84b35bfbeb386ac48080f4761d0
Matthew Runyon <matthewrunyon@deephaven.io>
2022-01-18 17:30:37 -0600

    Fix babel public class fields plugin

--- packages/utils/src/EventTargetShimUtils.ts
+++ packages/utils/src/EventTargetShimUtils.ts
@@ -2,20 +2,25 @@ import type { Event, EventTarget } from 'event-target-shim';
 
 /**
  * A CustomEvent extension which combines the browser CustomEvent and event-target-shim's Event types for type safety
- * CustomEvent does not
+ * CustomEvent does not have a generic type and augmenting the dom types seemed like not the best idea
  */
 export class EventShimCustomEvent<T extends string, D = unknown>
   extends CustomEvent<D>
   implements Event<T> {
-  type!: T;
+  // The fields declared are so TS plays nicely w/ event-target-shim and the browser CustomEvent
+  // They don't actually do anything other than tell TS to not complain that they aren't set in the constructor
+  // If declare is removed, then the properties are initialized to undefined which breaks this class
+  // This will be the default for tsc and babel at some point
+  // https://github.com/babel/babel/issues/12128#issuecomment-702119272
+  declare type: T;
 
-  target!: EventTarget | null;
+  declare target: EventTarget | null;
 
-  srcElement!: EventTarget | null;
+  declare srcElement: EventTarget | null;
 
-  currentTarget!: EventTarget | null;
+  declare currentTarget: EventTarget | null;
 
-  composedPath!: () => EventTarget[];
+  declare composedPath: () => EventTarget[];
 
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(typeArg: T, eventInitDict?: CustomEventInit<D>) {
