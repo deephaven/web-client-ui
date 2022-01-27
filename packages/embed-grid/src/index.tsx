d35692cbf3cc1a95d50f7432b5806a9268372cb1
mikebender <mikebender@deephaven.io>
2022-01-27 10:40:30 -0500

    Fix the React.StrictMode warning

--- packages/embed-grid/src/index.tsx
+++ packages/embed-grid/src/index.tsx
@@ -10,9 +10,4 @@ import '@deephaven/components/dist/BaseStyleSheet.css';
 import './index.scss';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
