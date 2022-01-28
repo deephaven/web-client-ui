14273a8df9df1067b97bf3cc2b6f0f7198755076
Mike Bender <mikebender@deephaven.io>
2022-01-28 11:38:51 -0500

    Fix the pandas plugin to actually open tables (#402)

--- packages/dashboard-core-plugins/src/PandasPlugin.tsx
+++ packages/dashboard-core-plugins/src/PandasPlugin.tsx
@@ -1,10 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { DragEvent, useCallback, useEffect } from 'react';
 import {
   DashboardPluginComponentProps,
   DashboardUtils,
+  LayoutUtils,
   PanelHydrateFunction,
+  useListener,
 } from '@deephaven/dashboard';
+import { IrisGridModel } from '@deephaven/iris-grid';
+import shortid from 'shortid';
 import { PandasPanel } from './panels';
+import { PandasEvent } from './events';
 
 export type PandasPluginComponentProps = DashboardPluginComponentProps & {
   hydrate: PanelHydrateFunction;
@@ -12,8 +17,46 @@ export type PandasPluginComponentProps = DashboardPluginComponentProps & {
 
 export const PandasPlugin = ({
   hydrate = DashboardUtils.hydrate,
+  id,
+  layout,
   registerComponent,
 }: PandasPluginComponentProps): JSX.Element => {
+  const handleOpen = useCallback(
+    (
+      title: string,
+      makeModel: () => IrisGridModel,
+      metadata: Record<string, unknown> = {},
+      panelId = shortid.generate(),
+      dragEvent?: DragEvent
+    ) => {
+      const config = {
+        type: 'react-component',
+        component: PandasPanel.COMPONENT,
+        props: {
+          localDashboardId: id,
+          id: panelId,
+          metadata,
+          makeModel,
+        },
+        title,
+        id: panelId,
+      };
+
+      const { root } = layout;
+      LayoutUtils.openComponent({ root, config, dragEvent });
+    },
+    [id, layout]
+  );
+
+  const handleClose = useCallback(
+    (panelId: string) => {
+      const config = { component: PandasPanel.COMPONENT, id: panelId };
+      const { root } = layout;
+      LayoutUtils.closeComponent(root, config);
+    },
+    [layout]
+  );
+
   useEffect(() => {
     const cleanups = [
       registerComponent(PandasPanel.COMPONENT, PandasPanel, hydrate),
@@ -23,6 +66,9 @@ export const PandasPlugin = ({
       cleanups.forEach(cleanup => cleanup());
     };
   }, [hydrate, registerComponent]);
+
+  useListener(layout.eventHub, PandasEvent.OPEN, handleOpen);
+  useListener(layout.eventHub, PandasEvent.CLOSE, handleClose);
 
   return <></>;
 };
