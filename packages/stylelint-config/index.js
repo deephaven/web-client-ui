3963b8b412b24d4475837169cb538a24c755adb3
Mike Bender <mikebender@deephaven.io>
2022-02-22 10:57:13 -0500

    feb dependabot alerts (#443)
    * Fix lodash security vulnerability CVE-2019-10744
    
    Updated gulp in the golden-layout package.json to remove dependency on lodash
    
    * Fix xmlhttprequest-ssl vulnerability CVE-2021-31597
    
    - An old version of karma depended on a vulnerable version of xmlhttprequest-ssl. Fix the vulnerability by updating versions of karma
    - Needed to include jasmine as a dependency as well with latest version of karma-jasmine
    - Ran golden-layout tests to ensure they pass
    
    * Run npm audit fix, fix npm audit vulnerabilities
    
    - Needed to update stylelint, removed @types/stylelint since it's now built in
    - Need to fix some stylelint issues
    
    * Update stylelint
    
    - Disable a bunch of new rules - we should do these in a followup
    - Fix up a couple new rules that produced a few errors
    
    * Update packages/stylelint-config/index.js

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier-scss'],
   rules: {
     'at-rule-no-unknown': [
       true,
@@ -13,5 +13,19 @@ module.exports = {
         ignorePseudoClasses: ['export'],
       },
     ],
+    // TODO #447: Re-enable these rules
+    'color-function-notation': null,
+    'declaration-block-no-redundant-longhand-properties': null,
+    'keyframes-name-pattern': null,
+    'property-no-vendor-prefix': null,
+    'rule-empty-line-before': null,
+    'scss/at-import-partial-extension': null,
+    'scss/double-slash-comment-empty-line-before': null,
+    'scss/double-slash-comment-whitespace-inside': null,
+    'scss/dollar-variable-empty-line-before': null,
+    'scss/no-global-function-names': null,
+    'selector-class-pattern': null,
+    'selector-no-vendor-prefix': null,
+    'shorthand-property-no-redundant-values': null,
   },
 };
