348e6c2a04595d2e14add4c3d737de58a91f5cbf
Matthew Runyon <matthewrunyon@deephaven.io>
2022-03-15 12:47:33 -0500

    Remove unused useMemo

--- packages/iris-grid/src/IrisGridCellOverflowModal.tsx
+++ packages/iris-grid/src/IrisGridCellOverflowModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Editor } from '@deephaven/console';
 import { Modal, ModalBody, ModalHeader } from 'reactstrap';
 import * as monaco from 'monaco-editor';
