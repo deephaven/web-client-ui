3b7da6ae92375c4f6f344acfd538358d462cfc7c
Mike Bender <mikebender@deephaven.io>
2022-03-15 15:32:38 -0400

    Fix sass deprecation warnings
    - Update Bootstrap version to 4.6.1
    - Use sass-migrator to migrate division used in TimeSlider.scss and AppMainContainer.scss
    ```
    npx sass-migrator division packages/components/src/TimeSlider.scss
    npx sass-migrator division packages/code-studio/src/main/AppMainContainer.scss
    ```
    - Visual smoke test to ensure visual consistency
    - Use scss aware at-rule-no-unknown
    - Fixes #484

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,12 +1,8 @@
 module.exports = {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier-scss'],
   rules: {
-    'at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: ['function', 'if', 'each', 'include', 'mixin'],
-      },
-    ],
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
     'selector-pseudo-class-no-unknown': [
       true,
       {
