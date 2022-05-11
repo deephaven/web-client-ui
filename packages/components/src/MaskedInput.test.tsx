fb8d8f25684d1479dbacaecf239e1c7276810f16
Tony Zhou <tonyzhou@deephaven.io>
2022-05-11 16:16:25 -0400

    convert enzyme to react-testing-library in web-client-ui:

--- packages/components/src/MaskedInput.test.tsx
+++ packages/components/src/MaskedInput.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render } from '@testing-library/react';
 import MaskedInput from './MaskedInput';
 
 const TIME_PATTERN = '([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]';
@@ -9,12 +9,11 @@ function makeMaskedInput({
   pattern = TIME_PATTERN,
   example = '12:34:56',
 } = {}) {
-  return mount(
+  return render(
     <MaskedInput value={value} pattern={pattern} example={example} />
   );
 }
 
 it('mounts and unmounts properly', () => {
-  const maskedInput = makeMaskedInput();
-  maskedInput.unmount();
+  makeMaskedInput();
 });
