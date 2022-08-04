314cd34323be67afeb5a5cb85d51d31f7cb69c19
Matthew Runyon <matthewrunyon@deephaven.io>
2022-08-04 13:45:32 -0500

    Remove environment variables from packages (#702)

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,6 +2,13 @@ import 'regenerator-runtime/runtime';
 import '@testing-library/jest-dom';
 import 'jest-canvas-mock';
 import './__mocks__/dh-core';
+import Log from '@deephaven/log';
+
+let logLevel = parseInt(process.env.DH_LOG_LEVEL ?? '', 10);
+if (!Number.isFinite(logLevel)) {
+  logLevel = -1;
+}
+Log.setLogLevel(logLevel);
 
 // disable annoying dnd-react warnings
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
