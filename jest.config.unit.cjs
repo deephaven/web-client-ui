cb94786a213a958e69103e03b6a47f7d043b5019
Matthew Runyon <matthewrunyon@deephaven.io>
2022-08-19 00:13:12 -0500

    Add jest caching for actions

--- jest.config.unit.cjs
+++ jest.config.unit.cjs
@@ -3,7 +3,7 @@ const baseConfig = require('./jest.config.base.cjs');
 module.exports = {
   ...baseConfig,
   projects: ['<rootDir>/packages/*/jest.config.cjs'],
-  collectCoverage: true,
+  collectCoverage: process.env.CI === 'true',
   collectCoverageFrom: ['./src/**/*.{js,ts,jsx,tsx}'], // This is relative to individual project root due to how Jest handles it
   coverageDirectory: './coverage',
 };
