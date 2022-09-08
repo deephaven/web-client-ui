569370a7c8662c5fcdde46f069f0b0cb8c79a677
mikebender <mikebender@deephaven.io>
2022-09-08 12:38:22 -0400

    Use the base monaco API instead, it includes all the KeyMod and KeyCodes

--- packages/mocks/src/monaco-editor.js
+++ packages/mocks/src/monaco-editor.js
@@ -3,43 +3,12 @@
  * If the issue gets resolved, this mock shouldn't be needed:
  * https://github.com/facebook/create-react-app/issues/5881
  */
-const editor = {
-  create: () => ({
-    addAction: () => {},
-    addCommand: () => {},
-    focus: () => {},
-    layout: () => {},
-    dispose: () => {},
-    getModel: () => ({
-      getLineCount: () => 0,
-      getModeId: () => 'modeId',
-      getValue: () => '',
-    }),
-    onDidChangeModelContent: () => () => {},
-    onKeyDown: () => {},
-  }),
-};
+import { createMonacoBaseAPI } from '../../../node_modules/monaco-editor/esm/vs/editor/common/services/editorBaseApi.js';
 
-const languages = {
+const monaco = createMonacoBaseAPI();
+
+monaco.languages = {
   registerCompletionItemProvider: () => {},
-};
-
-const KeyMod = {
-  CtrlCmd: 2048,
-  Shift: 1024,
-  Alt: 512,
-  WinCtrl: 256,
-};
-
-const KeyCode = {
-  KEY_F: 'F',
-};
-
-const monaco = {
-  editor,
-  languages,
-  KeyMod,
-  KeyCode,
 };
 
 module.exports = monaco;
