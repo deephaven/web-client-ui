9f60b5810cf7a77f8bc04de9a459e5728444ad45
Matthew Runyon <matthewrunyon@deephaven.io>
2022-09-12 01:52:26 -0500

    Fix monaco worker in jest

--- jest.config.base.cjs
+++ jest.config.base.cjs
@@ -19,6 +19,8 @@ module.exports = {
       'node_modules',
       'monaco-editor/esm/vs/editor/editor.api.js'
     ),
+    // Handle monaco worker files
+    '\\.worker.*$': 'identity-obj-proxy',
     '^@deephaven/golden-layout$': path.join(
       __dirname,
       './packages/golden-layout/dist/goldenlayout.js'
