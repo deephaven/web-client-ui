b7fe8785ffb4c09ba5645e16b05ea0ee57226e41
Mike Bender <mikebender@deephaven.io>
2022-10-03 09:35:00 -0400

    gRPC file storage (#787)
    - Requires Colin's changes to implement gRPC storage service: Implement gRPC storage API for notebooks and layouts deephaven-core#2843
    - Start up jetty server by running ./gradlew server-jetty-app:run -Pgroovy
    - Add the dh.types for JS API storage service
    - Remove webDAV entirely, gRPC storage service works over netty or jetty
    - Smoke tested with file explorer, creating nested directories, moving files/directories, creating files, saving files, deleting files, - deleting directory
    - Smoke tested with layouts, adding a default layout to the layouts directory. Loading the web UI from an incognito loaded that layout correctly, as well as the Reset button from the layouts drop down.

---
+++ packages/code-studio/src/storage/grpc/GrpcFileStorage.ts
@@ -0,0 +1,125 @@
+/* eslint-disable class-methods-use-this */
+
+import throttle from 'lodash.throttle';
+import {
+  FileNotFoundError,
+  FileStorage,
+  File,
+  FileStorageItem,
+  FileStorageTable,
+  FileUtils,
+} from '@deephaven/file-explorer';
+import { StorageService } from '@deephaven/jsapi-shim';
+import Log from '@deephaven/log';
+import GrpcFileStorageTable from './GrpcFileStorageTable';
+
+const log = Log.module('GrpcFileStorage');
+
+export class GrpcFileStorage implements FileStorage {
+  private static readonly REFRESH_THROTTLE = 150;
+
+  private readonly storageService: StorageService;
+
+  private tables = [] as GrpcFileStorageTable[];
+
+  private readonly root: string;
+
+  /**
+   * FileStorage implementation using gRPC
+   * @param storageService Storage service to use
+   * @param root Root path for this instance. Should not contain trailing slash.
+   */
+  constructor(storageService: StorageService, root = '') {
+    this.storageService = storageService;
+    this.root = root;
+  }
+
+  private removeRoot(filename: string): string {
+    return FileUtils.removeRoot(this.root, filename);
+  }
+
+  private addRoot(path: string): string {
+    return FileUtils.addRoot(this.root, path);
+  }
+
+  async createDirectory(path: string): Promise<FileStorageItem> {
+    await this.storageService.createDirectory(this.addRoot(path));
+    this.refreshTables();
+    return {
+      type: 'directory',
+      id: path,
+      filename: path,
+      basename: FileUtils.getBaseName(path),
+    };
+  }
+
+  async getTable(): Promise<FileStorageTable> {
+    const table = new GrpcFileStorageTable(this.storageService, this.root);
+    this.tables.push(table);
+    return table;
+  }
+
+  async saveFile(file: File): Promise<File> {
+    const fileContents = dh.storage.FileContents.text(file.content);
+    await this.storageService.saveFile(
+      this.addRoot(file.filename),
+      fileContents,
+      true
+    );
+    this.refreshTables();
+    return file;
+  }
+
+  async loadFile(name: string): Promise<File> {
+    const fileContents = await this.storageService.loadFile(this.addRoot(name));
+    const content = await fileContents.text();
+    return {
+      filename: name,
+      basename: FileUtils.getBaseName(name),
+      content,
+    };
+  }
+
+  async deleteFile(name: string): Promise<void> {
+    await this.storageService.deleteItem(this.addRoot(name));
+    this.refreshTables();
+  }
+
+  async moveFile(name: string, newName: string): Promise<void> {
+    await this.storageService.moveItem(
+      this.addRoot(name),
+      this.addRoot(newName)
+    );
+    this.refreshTables();
+  }
+
+  async info(name: string): Promise<FileStorageItem> {
+    const allItems = await this.storageService.listItems(
+      this.addRoot(FileUtils.getPath(name)),
+      FileUtils.getBaseName(name)
+    );
+    if (allItems.length === 0) {
+      throw new FileNotFoundError();
+    }
+    if (allItems.length > 1) {
+      log.error(
+        'More than one matching file found, should never happen.',
+        allItems
+      );
+      throw new Error('More than one matching file found');
+    }
+    const itemDetails = allItems[0];
+    return {
+      filename: this.removeRoot(itemDetails.filename),
+      basename: this.removeRoot(itemDetails.basename),
+      id: this.removeRoot(itemDetails.filename),
+      type: itemDetails.type,
+    };
+  }
+
+  private refreshTables = throttle(() => {
+    this.tables.every(table => table.refresh().catch(() => undefined));
+  }, GrpcFileStorage.REFRESH_THROTTLE);
+}
+
+export default GrpcFileStorage;
---
+++ packages/code-studio/src/storage/grpc/GrpcFileStorageTable.test.ts
@@ -0,0 +1,150 @@
+import { FileUtils } from '@deephaven/file-explorer';
+import { ItemDetails, StorageService } from '@deephaven/jsapi-shim';
+import GrpcFileStorageTable from './GrpcFileStorageTable';
+
+let storageService: StorageService;
+function makeTable(baseRoot = '', root = ''): GrpcFileStorageTable {
+  return new GrpcFileStorageTable(storageService, baseRoot, root);
+}
+
+function makeStorageService(): StorageService {
+  return {
+    listItems: jest.fn(async () => []),
+    loadFile: jest.fn(async () => {
+      throw new Error('No file loaded');
+    }),
+    deleteItem: jest.fn(async () => undefined),
+    saveFile: jest.fn(async () => undefined),
+    moveItem: jest.fn(async () => undefined),
+    createDirectory: jest.fn(async () => undefined),
+  };
+}
+
+beforeEach(() => {
+  storageService = makeStorageService();
+});
+
+it('Does not get contents until a viewport is set', () => {
+  const table = makeTable();
+  expect(storageService.listItems).not.toHaveBeenCalled();
+  table.setViewport({ top: 0, bottom: 10 });
+  expect(storageService.listItems).toHaveBeenCalled();
+});
+
+describe('directory expansion tests', () => {
+  function makeFile(name: string, path = ''): ItemDetails {
+    return {
+      basename: name,
+      filename: `${path}/${name}`,
+      dirname: path,
+      etag: '',
+      size: 1,
+      type: 'file',
+    };
+  }
+
+  function makeDirectory(name: string, path = ''): ItemDetails {
+    const file = makeFile(name, path);
+    file.type = 'directory';
+    return file;
+  }
+
+  function makeDirectoryContents(
+    path = '/',
+    numDirs = 3,
+    numFiles = 2
+  ): Array<ItemDetails> {
+    const results = [] as ItemDetails[];
+
+    for (let i = 0; i < numDirs; i += 1) {
+      const name = `dir${i}`;
+      results.push(makeDirectory(name, path));
+    }
+
+    for (let i = 0; i < numFiles; i += 1) {
+      const name = `file${i}`;
+      results.push(makeFile(name, path));
+    }
+
+    return results;
+  }
+
+  function expectItem(itemDetails: ItemDetails) {
+    return expect.objectContaining({
+      basename: itemDetails.basename,
+      filename: itemDetails.filename,
+      type: itemDetails.type,
+    });
+  }
+
+  beforeEach(() => {
+    storageService.listItems = jest.fn(async path => {
+      const depth = path.length > 0 ? FileUtils.getDepth(path) + 1 : 1;
+      const dirContents = makeDirectoryContents(path, 5 - depth, 10 - depth);
+      // console.log('dirContents for ', path, dirContents);
+      return dirContents;
+    });
+  });
+
+  it('expands multiple directories correctly', async () => {
+    const table = makeTable();
+    const handleUpdate = jest.fn();
+    table.onUpdate(handleUpdate);
+    table.setViewport({ top: 0, bottom: 5 });
+    await table.getViewportData();
+    expect(handleUpdate).toHaveBeenCalledWith({
+      offset: 0,
+      items: [
+        expectItem(makeDirectory('dir0')),
+        expectItem(makeDirectory('dir1')),
+        expectItem(makeDirectory('dir2')),
+        expectItem(makeDirectory('dir3')),
+        expectItem(makeFile('file0')),
+      ],
+    });
+    handleUpdate.mockReset();
+
+    await table.setExpanded('/dir1/', true);
+    await table.getViewportData();
+    expect(handleUpdate).toHaveBeenCalledWith({
+      offset: 0,
+      items: [
+        expectItem(makeDirectory('dir0')),
+        expectItem(makeDirectory('dir1')),
+        expectItem(makeDirectory('dir0', '/dir1')),
+        expectItem(makeDirectory('dir1', '/dir1')),
+        expectItem(makeDirectory('dir2', '/dir1')),
+      ],
+    });
+    handleUpdate.mockReset();
+
+    await table.setExpanded('/dir1/dir1/', true);
+    await table.getViewportData();
+    expect(handleUpdate).toHaveBeenCalledWith({
+      offset: 0,
+      items: expect.arrayContaining([
+        expectItem(makeDirectory('dir0')),
+        expectItem(makeDirectory('dir1')),
+        expectItem(makeDirectory('dir0', '/dir1')),
+        expectItem(makeDirectory('dir1', '/dir1')),
+        expectItem(makeDirectory('dir0', '/dir1/dir1')),
+      ]),
+    });
+    handleUpdate.mockReset();
+
+    // Now collapse it all
+    await table.setExpanded('/dir1/', false);
+    await table.getViewportData();
+    expect(handleUpdate).toHaveBeenCalledWith({
+      offset: 0,
+      items: expect.arrayContaining([
+        expectItem(makeDirectory('dir0')),
+        expectItem(makeDirectory('dir1')),
+        expectItem(makeDirectory('dir2')),
+        expectItem(makeDirectory('dir3')),
+        expectItem(makeFile('file0')),
+      ]),
+    });
+    handleUpdate.mockReset();
+  });
+});
