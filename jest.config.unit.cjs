61aaf3dbf7234ea611287246ac051ec985cc755b
Matthew Runyon <matthewrunyon@deephaven.io>
2022-11-07 18:04:28 -0600

    Fixed jest duplicate mocks log spam

--- jest.config.unit.cjs
+++ jest.config.unit.cjs
@@ -2,6 +2,7 @@ const baseConfig = require('./jest.config.base.cjs');
 
 module.exports = {
   ...baseConfig,
+  roots: ['<rootDir>/packages/*/src/', '<rootDir>/packages/*/test/'],
   projects: ['<rootDir>/packages/*/jest.config.cjs'],
   collectCoverage: process.env.CI === 'true',
   collectCoverageFrom: ['./src/**/*.{js,ts,jsx,tsx}'], // This is relative to individual project root due to how Jest handles it
