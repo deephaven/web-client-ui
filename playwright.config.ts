db7eef84ee26cac34a4b09fe7936d79755ca7c96
mikebender <mikebender@deephaven.io>
2022-11-25 15:24:38 -0500

    Some small fixes to try
    - Increase webserver timeout
    - Set baseURL
    - Limit to 1 worker
    - reuseExistingServer (for local dev)

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -38,7 +38,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:4000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -106,6 +106,10 @@ const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run start',
     port: 4000,
+    // Long timeout to make sure it builds/starts correctly
+    // This can probably be shortened
+    timeout: 5 * 60 * 1000,
+    reuseExistingServer: !process.env.CI,
   },
 };
 
