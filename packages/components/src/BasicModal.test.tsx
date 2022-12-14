cfe904acf180b06d56ff30a7435628b86ecf5750
Vlad Babich <vladbabich@deephaven.io>
2022-12-14 09:15:19 -0700

    Fix default button focusing on BasicModal re-render

---
+++ packages/components/src/BasicModal.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
+import BasicModal from './BasicModal';
+
+const DEFAULT_BODY_TEXT = 'DEFAULT_TEXT';
+const DEFAULT_TEST_ID = 'DEFAULT_TEST_ID';
+
+function makeWrapper(
+  isOpen = true,
+  bodyText = DEFAULT_BODY_TEXT,
+  dataTestId = DEFAULT_TEST_ID,
+  onConfirm = jest.fn()
+) {
+  return render(
+    <BasicModal
+      data-testid={dataTestId}
+      isOpen={isOpen}
+      headerText=""
+      bodyText={bodyText}
+      onConfirm={onConfirm}
+    />
+  );
+}
+
+it('mounts and unmounts without failing', () => {
+  makeWrapper();
+});
+
+it('focuses default button on first render after opening', () => {
+  const { getByTestId } = makeWrapper();
+  expect(getByTestId(`${DEFAULT_TEST_ID}-btn-confirm`)).toHaveFocus();
+});
+
+it('does not re-focus default button on re-render', () => {
+  const { getByTestId, rerender } = makeWrapper();
+  userEvent.tab();
+  expect(getByTestId(`${DEFAULT_TEST_ID}-btn-confirm`)).not.toHaveFocus();
+  rerender(
+    <BasicModal
+      isOpen
+      headerText=""
+      bodyText={DEFAULT_BODY_TEXT}
+      data-testid={DEFAULT_TEST_ID}
+      onConfirm={jest.fn()}
+    />
+  );
+  expect(getByTestId(`${DEFAULT_TEST_ID}-btn-confirm`)).not.toHaveFocus();
+});
