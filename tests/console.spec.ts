e4f6ccb2157f0b2ca85dd55f9bb9db0cd0f44e03
Mike Bender <mikebender@deephaven.io>
2023-01-05 12:31:31 -0500

    DH-13988 Fix issue with old buttons not getting disabled in console history (#978)
    - We were mutating objects within the state, violating the immutability principle.
    - Clone any object that we were mutating.
    - Cleaned up some of the code.
    - All tests pass
    - Tested by running the steps in the description of #928, also tested importing a CSV and giving it the same name as a prior object. Old objects were updated correctly.
    - Updated e2e tests:
      - Refactored creating a table command into it's own function
      - Fixed up old print commands tests - wasn't verifying the result correctly
      - Added test to check that an object button was created when creating a table, and that the old button was disabled when re-running the same command twice
    - Fixes #928 
    - Fixes [DH-13988](https://deephaven.atlassian.net/browse/DH-13988)
    
    Co-authored-by: Vlad Babich <vladimir.babich@gmail.com>

--- tests/console.spec.ts
+++ tests/console.spec.ts
@@ -1,27 +1,60 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 import shortid from 'shortid';
-import { typeInMonaco } from './utils';
+import { generateVarName, makeTableCommand, typeInMonaco } from './utils';
 
-test('print commands get logged', async ({ page }) => {
+let page: Page;
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
   await page.goto('');
+});
 
-  // create a locator
-  const consoleInput = page.locator('.console-input');
+test.afterAll(async () => {
+  await page.close();
+});
 
-  // Monaco editor doesn't have a native input, so need to just click into it and type on the page
-  // https://github.com/microsoft/playwright/issues/14126
-  await consoleInput.click();
+test.describe('console input tests', () => {
+  test.beforeEach(async () => {
+    // Find the console input
+    const consoleInput = page.locator('.console-input');
 
-  const message = `Hello ${shortid()}!`;
-  const command = `print("${message}")`;
+    // Monaco editor doesn't have a native input, so need to just click into it and type on the page
+    // https://github.com/microsoft/playwright/issues/14126
+    await consoleInput.click();
+  });
 
-  await typeInMonaco(page, command);
-  await page.keyboard.press('Enter');
+  test('print commands get logged', async () => {
+    const message = `Hello ${shortid()}!`;
+    const command = `print("${message}")`;
 
-  // Expect the output to show up in the log
-  await expect(
-    await page
-      .locator('.console-history .log-message')
-      .filter({ hasText: message })
-  ).not.toBeNull();
+    await typeInMonaco(page, command);
+    await page.keyboard.press('Enter');
+
+    // Expect the output to show up in the log
+    await expect(
+      page.locator('.console-history .log-message').filter({ hasText: message })
+    ).toHaveCount(1);
+  });
+
+  test('object button is created when creating a table', async () => {
+    const tableName = generateVarName('t');
+    const command = makeTableCommand(tableName);
+
+    await typeInMonaco(page, command);
+    await page.keyboard.press('Enter');
+
+    // Expect a button to show up in the console history
+    const btnLocator = await page
+      .locator('.console-history .btn-console-object')
+      .filter({ hasText: tableName });
+    await expect(btnLocator).toHaveCount(1);
+    expect(await btnLocator.nth(0).isDisabled()).toBe(false);
+
+    // Enter the same command again; the old button should be disabled
+    await typeInMonaco(page, command);
+    await page.keyboard.press('Enter');
+    await expect(btnLocator).toHaveCount(2);
+    await expect(btnLocator.nth(0)).toBeDisabled();
+    await expect(btnLocator.nth(1)).not.toBeDisabled();
+  });
 });
