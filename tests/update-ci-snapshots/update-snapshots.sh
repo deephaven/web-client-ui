0445e77cabf04ebbfab1eaf2b6c6fbb9d0bfd581
Mike Bender <mikebender@deephaven.io>
2023-02-06 15:07:35 -0500

    Seems to be working! Now to clean up

--- tests/update-ci-snapshots/update-snapshots.sh
+++ tests/update-ci-snapshots/update-snapshots.sh
@@ -5,34 +5,34 @@ set -o nounset
 
 # Need to go to a different directory to source nvm.sh, otherwise it exits with code 3
 # https://github.com/nvm-sh/nvm/issues/1985#issuecomment-456022013
-pushd /;
+# pushd /;
 
 echo "MJB update-snapshots.sh!";
 
 pwd;
 
-# echo "ls -la $(ls -la $HOME/.nvm)"
+# # echo "ls -la $(ls -la $HOME/.nvm)"
 
-# export NVM_DIR="$HOME/.nvm"; \
-#     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";
-# Start up nvm, and do a `--install` so it uses the correct version right away
+# # export NVM_DIR="$HOME/.nvm"; \
+# #     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";
+# # Start up nvm, and do a `--install` because we're running in the directory with .nvmrc so it uses the correct version right away
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --install # This loads nvm
 
 echo "MJB nvm $(nvm --version)";
-popd;
-pwd;
+# # popd;
+# pwd;
 
-pushd /work/;
+# # pushd /work/;
 
-# nvm install;
+# # nvm install;
 
 echo "MJB post nvm install $(npm --version) and $(node --version)";
 
-# Clear node_modules so we don't conflict 
-# rm -rf node_modules;
+# # Clear node_modules so we don't conflict 
+# # rm -rf node_modules;
 npm ci
 VITE_CORE_API_URL=http://host.docker.internal:10000/jsapi npm run build
 
 npm run e2e:update-snapshots
-popd;
+# # popd;
