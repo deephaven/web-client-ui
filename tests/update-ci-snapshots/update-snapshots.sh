d76731a799de7ab12e9b32a12f9cc11d29205bae
Mike Bender <mikebender@deephaven.io>
2023-02-06 15:18:48 -0500

    Clean up the docker setup files

--- tests/update-ci-snapshots/update-snapshots.sh
+++ tests/update-ci-snapshots/update-snapshots.sh
@@ -3,36 +3,13 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# Need to go to a different directory to source nvm.sh, otherwise it exits with code 3
-# https://github.com/nvm-sh/nvm/issues/1985#issuecomment-456022013
-# pushd /;
-
-echo "MJB update-snapshots.sh!";
-
-pwd;
-
-# # echo "ls -la $(ls -la $HOME/.nvm)"
-
-# # export NVM_DIR="$HOME/.nvm"; \
-# #     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";
-# # Start up nvm, and do a `--install` because we're running in the directory with .nvmrc so it uses the correct version right away
+# Use nvm with the `--install` flag to install the correct version of node/npm based on the `.nvmrc`
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --install # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --install
 
-echo "MJB nvm $(nvm --version)";
-# # popd;
-# pwd;
-
-# # pushd /work/;
-
-# # nvm install;
-
-echo "MJB post nvm install $(npm --version) and $(node --version)";
-
-# # Clear node_modules so we don't conflict 
-# # rm -rf node_modules;
+# Install and build the app
 npm ci
 VITE_CORE_API_URL=http://host.docker.internal:10000/jsapi npm run build
 
+# Update the snapshots
 npm run e2e:update-snapshots
-# # popd;
