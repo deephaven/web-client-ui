f6e928472ba0c86c83da349fd4cfa0b6925a4309
Emily Huang <emilyjiajiahuang@gmail.com>
2023-02-07 13:34:44 -0500

    Add tests for FileExplorer

--- packages/dashboard-core-plugins/src/panels/MockFileStorage.ts
+++ packages/dashboard-core-plugins/src/panels/MockFileStorage.ts
@@ -1,8 +1,10 @@
 import {
   File,
+  FileNotFoundError,
   FileStorage,
   FileStorageItem,
   FileStorageTable,
+  FileUtils,
 } from '@deephaven/file-explorer';
 import MockFileStorageTable from './MockFileStorageTable';
 
@@ -26,16 +28,37 @@ export class MockFileStorage implements FileStorage {
     throw new Error('Method not implemented.');
   }
 
-  deleteFile(name: string): Promise<void> {
-    throw new Error('Method not implemented.');
+  async deleteFile(name: string): Promise<void> {
+    this.items = this.items.filter(value => value.filename !== name);
   }
 
-  moveFile(name: string, newName: string): Promise<void> {
-    throw new Error('Method not implemented.');
+  async moveFile(name: string, newName: string): Promise<void> {
+    for (let i = 0; i < this.items.length; i += 1) {
+      if (this.items[i].filename === name) {
+        this.items[i].filename = newName;
+        this.items[i].basename = FileUtils.getBaseName(newName);
+        this.items[i].id = newName;
+        break;
+      }
+    }
   }
 
-  info(name: string): Promise<FileStorageItem> {
-    throw new Error('Method not implemented.');
+  async info(name: string): Promise<FileStorageItem> {
+    const allItems = this.items.filter(value => value.filename === name);
+    if (allItems.length === 0) {
+      throw new FileNotFoundError();
+    }
+    if (allItems.length > 1) {
+      throw new Error('More than one matching file found');
+    }
+    const itemDetails = allItems[0];
+
+    return {
+      filename: itemDetails.filename,
+      basename: itemDetails.basename,
+      id: itemDetails.filename,
+      type: itemDetails.type,
+    };
   }
 
   createDirectory(name: string): Promise<FileStorageItem> {
--- packages/file-explorer/src/FileExplorer.test.tsx
+++ packages/file-explorer/src/FileExplorer.test.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event/';
 import { MockFileStorage } from '@deephaven/dashboard-core-plugins';
+import { ContextMenuRoot } from '@deephaven/components';
 import FileStorage, {
   DirectoryStorageItem,
   FileStorageItem,
 } from './FileStorage';
-import FileExplorer from './FileExplorer';
+import FileExplorer, { FileExplorerProps } from './FileExplorer';
 
 function makeFileName(index = 0): string {
   return `testfile${index}`;
@@ -72,13 +77,34 @@ function makeMockFileStorage(): FileStorage {
   };
 }
 
-function renderFileExplorer(storage: FileStorage) {
-  const onSelect = jest.fn();
-  return render(<FileExplorer storage={storage} onSelect={onSelect} />);
+function renderFileExplorer({
+  storage,
+  onSelect = jest.fn(),
+  onRename = jest.fn(),
+  onDelete = jest.fn(),
+}: Partial<FileExplorerProps>) {
+  if (storage !== undefined) {
+    return render(
+      <>
+        <FileExplorer
+          storage={storage}
+          onSelect={onSelect}
+          onRename={onRename}
+          onDelete={onDelete}
+        />
+        <ContextMenuRoot />
+      </>
+    );
+  }
 }
 
+// Not working?
+it('mounts without onDelete and onRename functions', () => {
+  render(<FileExplorer storage={makeMockFileStorage()} onSelect={jest.fn()} />);
+});
+
 it('mounts and unmounts successfully without crashing', () => {
-  renderFileExplorer(makeMockFileStorage());
+  renderFileExplorer({ storage: makeMockFileStorage() });
 });
 
 it('mounts properly and shows file list', async () => {
@@ -86,36 +112,178 @@ it('mounts properly and shows file list', async () => {
 
   const fileStorage = new MockFileStorage(files);
 
-  renderFileExplorer(fileStorage);
+  renderFileExplorer({ storage: fileStorage as FileStorage });
 
   const foundItems = await screen.findAllByRole('listitem');
   expect(foundItems).toHaveLength(files.length);
 });
+
+// it('logs error when the table does not initialize', () => {
+//   const mockFileStorage = makeMockFileStorage();
+//   mockFileStorage.getTable = jest.fn(() => {
+//     setTimeout(() => {
+//       throw new Error("Can't initialize table");
+//     }, 3000);
+//   });
+
+//   // mockFileStorage.getTable = jest.fn(async () => ({
+//   //   setSearch: jest.fn(),
+//   //   setExpanded: jest.fn(),
+//   //   collapseAll: jest.fn(),
+//   //   getViewportData: jest.fn(),
+//   //   getSnapshot: jest.fn(),
+//   //   setViewport: jest.fn(),
+//   //   setFilters: jest.fn(),
+//   //   setSorts: jest.fn(),
+//   //   setReversed: jest.fn(),
+//   //   close: jest.fn(),
+//   //   onUpdate: jest.fn(),
+//   //   size: 0,
+//   // }));
+//   expect(() =>
+//     renderFileExplorer({ storage: mockFileStorage })
+//   ).not.toThrowError();
+// });
+
 describe('context menu actions work properly', () => {
-  // let user: ReturnType<typeof userEvent.setup>;
-  // let dirs: DirectoryStorageItem[] = [];
+  let user: ReturnType<typeof userEvent.setup>;
+  let dirs: DirectoryStorageItem[] = [];
   let files: FileStorageItem[] = [];
   let items: FileStorageItem[] = [];
+  const onRename = jest.fn();
+  const onDelete = jest.fn();
 
   beforeEach(async () => {
-    // user = userEvent.setup();
-    // dirs = makeDirectories();
+    user = userEvent.setup();
+    dirs = makeDirectories();
     files = makeFiles();
-    // files.push(makeNested([2], 2));
-    // files.push(makeNested([0, 3], 4));
-    // items = dirs.concat(files);
-    items = files;
+    files.push(makeNested([2], 2));
+    files.push(makeNested([0, 3], 4));
+    items = dirs.concat(files);
 
     const fileStorage = new MockFileStorage(items);
-    renderFileExplorer(fileStorage);
+    renderFileExplorer({ storage: fileStorage, onRename, onDelete });
     const foundItems = await screen.findAllByRole('listitem');
     expect(foundItems).toHaveLength(items.length);
   });
 
   it('should be able to rename a file', async () => {
     const file = await screen.findByText('testfile0');
-    fireEvent.contextMenu(file);
-    // user.pointer({ keys: '[MouseRight]', target: file });
-    expect(await screen.findByText(/rename/i)).toBeInTheDocument();
+    await user.pointer({ keys: '[MouseRight]', target: file });
+
+    const renameButton = await screen.findByText(/rename/i);
+    expect(renameButton).toBeInTheDocument();
+
+    await user.click(renameButton);
+    await user.keyboard('test{enter}');
+    expect(onRename).toBeCalledWith('/testfile0', '/testfile0test');
+  });
+
+  it('should be able to rename a directory', async () => {
+    const file = await screen.findByText('testdir0');
+    await user.pointer({ keys: '[MouseRight]', target: file });
+
+    const renameButton = await screen.findByText(/rename/i);
+    expect(renameButton).toBeInTheDocument();
+
+    await user.click(renameButton);
+    await user.keyboard('test{enter}');
+    expect(onRename).toBeCalledWith('/testdir0/', '/testdir0test/');
+  });
+
+  it('should throw an error when renaming a file as a file that already exists', async () => {
+    const file = await screen.findByText('testfile0');
+    await user.pointer({ keys: '[MouseRight]', target: file });
+
+    const renameButton = await screen.findByText(/rename/i);
+    expect(renameButton).toBeInTheDocument();
+
+    await user.click(renameButton);
+    await user.keyboard('{Backspace}{1}{Enter}');
+    expect(
+      await screen.findByText('Error: Name already exists')
+    ).toBeInTheDocument();
+    expect(onRename).not.toBeCalledWith('/testfile0', '/testfile1');
+  });
+
+  it('should open a modal when deleting an item', async () => {
+    const file = await screen.findByText('testfile0');
+    await user.pointer({ keys: '[MouseRight]', target: file });
+
+    const deleteButton = await screen.findByRole('button', { name: /delete/i });
+    expect(deleteButton).toBeInTheDocument();
+    await user.click(deleteButton);
+    expect(
+      await screen.findByText(
+        /Are you sure you want to delete "\/testfile0"\?/i
+      )
+    );
+  });
+
+  it('should close the modal on cancelling the delete', async () => {
+    const file = await screen.findByText('testfile0');
+    await user.pointer({ keys: '[MouseRight]', target: file });
+
+    const deleteButton = await screen.findByRole('button', { name: /delete/i });
+    expect(deleteButton).toBeInTheDocument();
+    await user.click(deleteButton);
+
+    const cancelButton = await screen.findByRole('button', { name: /cancel/i });
+    expect(cancelButton).toBeInTheDocument();
+    await user.click(cancelButton);
+
+    await waitForElementToBeRemoved(cancelButton);
+    expect(cancelButton).not.toBeInTheDocument();
+  });
+
+  it('should delete the item upon confirmation', async () => {
+    const file = await screen.findByText('testfile0');
+    await user.pointer({ keys: '[MouseRight]', target: file });
+
+    const deleteButton = await screen.findByRole('button', { name: /delete/i });
+    expect(deleteButton).toBeInTheDocument();
+    await user.click(deleteButton);
+
+    const modalDeleteButton = await screen.findByRole('button', {
+      name: /delete/i,
+    });
+    await user.click(modalDeleteButton);
+    await waitForElementToBeRemoved(modalDeleteButton);
+    expect(onDelete).toBeCalledTimes(1);
   });
 });
+
+// describe('drag and drop works properly', () => {
+//   let user: ReturnType<typeof userEvent.setup>;
+//   let dirs: DirectoryStorageItem[] = [];
+//   let files: FileStorageItem[] = [];
+//   let items: FileStorageItem[] = [];
+//   const onRename = jest.fn();
+//   const onDelete = jest.fn();
+
+//   beforeEach(async () => {
+//     user = userEvent.setup();
+//     dirs = makeDirectories();
+//     files = makeFiles();
+//     files.push(makeNested([2], 2));
+//     files.push(makeNested([0, 3], 4));
+//     items = dirs.concat(files);
+
+//     const fileStorage = new MockFileStorage(items);
+//     renderFileExplorer({ storage: fileStorage, onRename, onDelete });
+//     const foundItems = await screen.findAllByRole('listitem');
+//     expect(foundItems).toHaveLength(items.length);
+//   });
+
+//   // it('should drag a file into a directory', async () => {
+//   //   const file = await screen.findByText('testfile0');
+//   //   const directory = await screen.findByText('testdir0');
+//   //   // await user.pointer({ keys: '[MouseLeft>]', target: file });
+//   //   // fireEvent.mouseDown(file);
+//   //   // fireEvent.mouseMove(directory);
+//   //   // fireEvent.mouseUp(directory);
+//   //   // await user.pointer({ keys: '[/MouseLeft]' });
+
+//   //   // expect(file).not.toBeInTheDocument();
+//   // });
+// });
