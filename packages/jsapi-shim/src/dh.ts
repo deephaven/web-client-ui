73352df601a3140a8dd8e591813967ca85a96e4b
mikebender <mikebender@deephaven.io>
2023-02-08 09:25:43 -0500

    Remove commented out code

--- packages/jsapi-shim/src/dh.ts
+++ packages/jsapi-shim/src/dh.ts
@@ -2,14 +2,6 @@
 // This include file is simply a wrapper so that it behaves like a module, and can be mocked easily for unit tests
 import type dhType from './dh.types';
 
-// declare global {
-//   // eslint-disable-next-line vars-on-top,no-var
-//   var dh: dhType;
-// }
-
-// eslint-disable-next-line import/no-unresolved, @typescript-eslint/ban-ts-comment
-// @ts-ignore
-// eslint-disable-next-line import/no-unresolved
 const dhModule = await import(
   /* @vite-ignore */
   `${import.meta.env.VITE_CORE_API_URL}/${import.meta.env.VITE_CORE_API_NAME}`
