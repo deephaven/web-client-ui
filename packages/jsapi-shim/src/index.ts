7de114a057abea48a436cdb3fdd40bc04d3156f5
Vlad Babich <vladbabich@deephaven.io>
2023-02-16 14:10:00 -0700

    fix: Remove default export in jsapi-types (#1092)
    Fixes DHE build issue

--- packages/jsapi-shim/src/index.ts
+++ packages/jsapi-shim/src/index.ts
@@ -4,4 +4,4 @@ export default shim;
 export { default as dh } from './dh';
 export { default as PropTypes } from './PropTypes';
 export * from './dh.types';
-export type { default as dhType } from './dh.types';
+export type { dh as dhType } from './dh.types';
