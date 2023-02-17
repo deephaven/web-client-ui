b942f47b849ee5a877f5df3dd2827086b2ff9774
Emily Huang <emilyjiajiahuang@gmail.com>
2023-02-17 12:23:16 -0500

    Add color formats for white

--- packages/utils/src/ColorUtils.test.ts
+++ packages/utils/src/ColorUtils.test.ts
@@ -4,7 +4,6 @@ describe('isDark', () => {
   it('returns true if the background is dark', () => {
     expect(ColorUtils.isDark('#000000')).toBe(true);
     expect(ColorUtils.isDark('#000')).toBe(true);
-    // expect(ColorUtils.isDark('white')).toBe(true);
     expect(ColorUtils.isDark('rgb(0,0,0)')).toBe(true);
     expect(ColorUtils.isDark('rgba(0,0,0,1)')).toBe(true);
     expect(ColorUtils.isDark('hsl(0,0%,0%)')).toBe(true);
@@ -12,6 +11,10 @@ describe('isDark', () => {
 
   it('returns false if the background is bright', () => {
     expect(ColorUtils.isDark('#ffffff')).toBe(false);
+    expect(ColorUtils.isDark('#fff')).toBe(false);
+    expect(ColorUtils.isDark('rgb(255,255,255)')).toBe(false);
+    expect(ColorUtils.isDark('rgba(255,255,255,1)')).toBe(false);
+    expect(ColorUtils.isDark('hsl(0,100%,100%)')).toBe(false);
   });
 
   it('throws an error if the color is not a valid value', () => {
