6566f37d4e5ab64d38ac9c5c959fe165f7f78a9c
Emily Huang <emilyjiajiahuang@gmail.com>
2023-02-17 10:20:07 -0500

    Add more color formats

--- packages/utils/src/ColorUtils.test.ts
+++ packages/utils/src/ColorUtils.test.ts
@@ -3,6 +3,11 @@ import ColorUtils from './ColorUtils';
 describe('isDark', () => {
   it('returns true if the background is dark', () => {
     expect(ColorUtils.isDark('#000000')).toBe(true);
+    expect(ColorUtils.isDark('#000')).toBe(true);
+    // expect(ColorUtils.isDark('white')).toBe(true);
+    expect(ColorUtils.isDark('rgb(0,0,0)')).toBe(true);
+    expect(ColorUtils.isDark('rgba(0,0,0,1)')).toBe(true);
+    expect(ColorUtils.isDark('hsl(0,0%,0%)')).toBe(true);
   });
 
   it('returns false if the background is bright', () => {
