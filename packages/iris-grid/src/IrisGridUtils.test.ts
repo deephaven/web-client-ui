4acdaa837661807e1b1ec250a76e9dae9f4347b4
Brian Ingles <brianingles@deephaven.io>
2023-03-06 14:04:48 -0600

    Simplified test
    [#965]
    
    [#965]

--- packages/iris-grid/src/IrisGridUtils.test.ts
+++ packages/iris-grid/src/IrisGridUtils.test.ts
@@ -156,11 +156,10 @@ describe('sort exporting/importing', () => {
     const sort = [columns[3].sort(), columns[7].sort().abs().desc()];
     const table = makeTable({ columns, sort });
     const exportedSort = IrisGridUtils.dehydrateSort(sort);
-    const expectExportedSort: DehydratedSort[] = [
+    expect(exportedSort).toEqual<DehydratedSort[]>([
       { column: columns[3].name, isAbs: false, direction: 'ASC' },
       { column: columns[7].name, isAbs: true, direction: 'DESC' },
-    ];
-    expect(exportedSort).toEqual(expectExportedSort);
+    ]);
 
     const importedSort = IrisGridUtils.hydrateSort(table.columns, exportedSort);
     expect(importedSort).toEqual([
