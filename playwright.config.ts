f10c946f42328af82d38e183f48022d4c0e1629c
Matthew Runyon <matthewrunyon@deephaven.io>
2023-03-07 15:35:40 -0600

    Add snapshot test and update readme for snapshot tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -110,7 +110,7 @@ const config: PlaywrightTestConfig = {
   webServer: {
     // Only start the main code-studio server right now
     // To test embed-grid and embed-chart, should have an array set for `webServer` and run them all separately as there's a port check
-    command: 'VITE_PROXY_URL=http://localhost:10000 npm run preview:app',
+    command: 'npm run preview:app -- -- -- --no-open', // Passing flags through npm is fun
     port: 4000,
     timeout: 60 * 1000,
     reuseExistingServer: !process.env.CI,
