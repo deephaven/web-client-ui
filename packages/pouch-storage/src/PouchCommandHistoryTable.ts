b9a89ea5a7777abd1c55f800f6169ea871aa192d
Brian Ingles <brianingles@deephaven.io>
2023-03-13 15:01:51 -0500

    Cleaned up PouchCommandHistoryTable
    [#325]

--- packages/pouch-storage/src/PouchCommandHistoryTable.ts
+++ packages/pouch-storage/src/PouchCommandHistoryTable.ts
@@ -14,6 +14,7 @@ import PouchStorageTable, {
   PouchStorageItem,
 } from './PouchStorageTable';
 
+// Max number of history items that will be loaded
 const DISPLAY_COMMAND_HISTORY_LIMIT = 1000;
 
 const log = Log.module('PouchCommandHistoryTable');
@@ -30,9 +31,9 @@ export class PouchCommandHistoryTable
   }
 
   /**
-   * Singleton cache for CommandHistoryStorageItem arrays keyed by databaseName.
-   * This allows us to share results across multiple instances of
-   * PouchCommandHistoryTable.
+   * Cache for command history query results keyed by db name. The cached data
+   * will be shared across all `PouchCommandHistoryTable` instances that have
+   * the same db name.
    */
   static cache: Record<
     string,
@@ -40,6 +41,10 @@ export class PouchCommandHistoryTable
   > = {};
 
   private searchText = '';
+
+  private get cacheKey(): string {
+    return this.db.name;
+  }
 
   setSearchDebounceTimeout?: number;
 
@@ -51,7 +56,7 @@ export class PouchCommandHistoryTable
       log.debug('Clearing cache and setting search filters', searchText);
       log.debug('db.name', this.db.name);
 
-      PouchCommandHistoryTable.cache[this.db.name] = null;
+      PouchCommandHistoryTable.cache[this.cacheKey] = null;
 
       this.searchText = searchText;
       this.setFilters(
@@ -75,51 +80,72 @@ export class PouchCommandHistoryTable
   ): void {
     log.debug('Clearing cache and refreshing data', event);
 
-    PouchCommandHistoryTable.cache[this.db.name] = null;
+    PouchCommandHistoryTable.cache[this.cacheKey] = null;
 
     super.dbUpdate(event);
   }
 
+  /**
+   * Fetch command history data from `PouchCommandHistoryTable.cache` or from
+   * PouchDB if data is not found in the cache.
+   * @param selector
+   */
   private async fetchData(
     selector: PouchDB.Find.Selector
   ): Promise<
     PouchDB.Find.FindResponse<CommandHistoryStorageItem & PouchStorageItem>
   > {
-    if (PouchCommandHistoryTable.cache[this.db.name]) {
+    if (PouchCommandHistoryTable.cache[this.cacheKey]) {
       log.debug('Fetching from cache', this.searchText, this.viewport);
     } else {
       log.debug('Fetching from PouchDB', this.searchText, this.viewport);
 
-      if (this.searchText) {
-        PouchCommandHistoryTable.cache[this.db.name] = this.db.find({
-          selector,
-          fields: ['id', 'name'],
-          limit: DISPLAY_COMMAND_HISTORY_LIMIT,
-          sort: [{ id: 'desc' }],
-        });
-      } else {
-        PouchCommandHistoryTable.cache[this.db.name] = this.db
-          .allDocs({
-            include_docs: true,
-            descending: true,
-            limit: DISPLAY_COMMAND_HISTORY_LIMIT + 1, // +1 for an extra row returned by allDocs
+      // PouchDB `find` queries require scanning the entire table which can have
+      // a significant performance impact on large tables. Therefore we only
+      // call db.find when there is a search applied. Otherwise we use the faster
+      // `allDocs` method. We also apply an optimzation by reverse sorting the
+      // query and then reversing back the results.
+      const queryResult = this.searchText
+        ? this.db.find({
+            selector,
+            fields: ['id', 'name'],
+            limit: DISPLAY_COMMAND_HISTORY_LIMIT,
+            sort: [{ id: 'desc' }],
           })
-          .then(result => {
-            result.rows.reverse();
-            return {
+        : this.db
+            .allDocs({
+              include_docs: true,
+              descending: true,
+              // There is an extra row returned by `allDocs` that is not part of
+              // the command history. We add 1 to the limit to account for it but
+              // filter it out of the result set below.
+              limit: DISPLAY_COMMAND_HISTORY_LIMIT + 1,
+            })
+            .then(result => ({
               docs: result.rows
                 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                 .map(row => row.doc!)
                 .filter(({ name }) => name),
-            };
-          });
-      }
+            }));
+
+      // reverse the doc order and cache the query results.
+      PouchCommandHistoryTable.cache[this.cacheKey] = queryResult.then(
+        result => {
+          result.docs.reverse();
+          return result;
+        }
+      );
     }
 
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return PouchCommandHistoryTable.cache[this.db.name]!;
+    return PouchCommandHistoryTable.cache[this.cacheKey]!;
   }
 
+  /**
+   * Override `PouchStorageTable.fetchInfo` so we can make use of
+   * `PouchCommandHistoryTable.cache`
+   * @param selector
+   */
   async fetchInfo(
     selector: PouchDB.Find.Selector
   ): Promise<
@@ -129,8 +155,9 @@ export class PouchCommandHistoryTable
   }
 
   /**
-   * Fetch command history storage items.
-   * This override provides caching + optimizations specific to command history.
+   * Override `PouchStorageTable.fetchViewportData` to fetch command history
+   * storage items. This override provides caching + optimizations specific to
+   * command history.
    *
    * 1. The table doesn't support sorting, so ignore the sort param.
    * 2. We cache the results to avoid excessive PouchDB queries during scrolling.
@@ -148,7 +175,7 @@ export class PouchCommandHistoryTable
     _sort: PouchDBSort
   ): Promise<ViewportData<CommandHistoryStorageItem>> {
     const data = await this.fetchData(selector);
-    log.debug('data', data);
+    log.debug('fetchViewportData', data);
     return {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       items: data.docs.slice(viewport.top, viewport.bottom + 1),
