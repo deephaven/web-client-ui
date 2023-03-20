0405851e8e8c24c96c71520b928a6b3df6f973fc
Emily Huang <emilyjiajiahuang@gmail.com>
2023-03-20 14:06:11 -0400

    Add in assertion

--- packages/iris-grid/src/IrisGridRenderer.ts
+++ packages/iris-grid/src/IrisGridRenderer.ts
@@ -255,9 +255,8 @@ class IrisGridRenderer extends GridRenderer {
 
     const { allColumnWidths, allColumnXs, maxY } = metrics;
 
-    const x = allColumnXs.get(hoverSelectColumn);
-    const columnWidth = allColumnWidths.get(hoverSelectColumn);
-    if (x == null || columnWidth == null) return;
+    const x = getOrThrow(allColumnXs, hoverSelectColumn);
+    const columnWidth = getOrThrow(allColumnWidths, hoverSelectColumn);
 
     context.fillStyle = theme.linkerColumnHoverBackgroundColor;
     context.fillRect(x, 0, columnWidth, maxY);
