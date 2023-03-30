d5c26701bb46c19a34fb803fb3a14a7f6d42aedb
Vlad Babich <vladbabich@deephaven.io>
2023-03-30 16:56:57 -0600

    Unit tests

--- packages/jsapi-utils/src/MessageUtils.ts
+++ packages/jsapi-utils/src/MessageUtils.ts
@@ -8,26 +8,30 @@ export const LOGIN_OPTIONS_RESPONSE = 'loginOptions';
 export const SESSION_DETAILS_REQUEST = 'requestSessionDetailsFromParent';
 export const SESSION_DETAILS_RESPONSE = 'sessionDetails';
 
+export interface Message<T> {
+  message: string;
+  payload?: T;
+}
+
+export function makeMessage<T>(message: string, payload?: T): Message<T> {
+  return { message, payload };
+}
+
 /**
  * Request data from the parent window and wait for response
  * @param request Request message to send to the parent window
  * @param response Response message to wait for
- * @returns Payload of the given type
+ * @returns Payload of the given type, or undefined
  */
 export async function requestParentResponse<T>(
   request: string,
   response: string
-): Promise<T> {
+): Promise<T | undefined> {
   if (window.opener == null) {
     throw new Error('window.opener is null, unable to send request.');
   }
   return new Promise(resolve => {
-    const listener = (
-      event: MessageEvent<{
-        message: string;
-        payload: T;
-      }>
-    ) => {
+    const listener = (event: MessageEvent<Message<T>>) => {
       const { data } = event;
       log.debug('Received message', data);
       if (data?.message !== response) {
