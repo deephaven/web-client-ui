d51bd7d96963bee2969415da1d39c556f2bc3b16
mikebender <mikebender@deephaven.io>
2023-04-05 10:56:48 -0400

    Get rid of the redirect
    - It was causing the favicon to not load correctly...

--- packages/code-studio/src/main/AppRouter.tsx
+++ packages/code-studio/src/main/AppRouter.tsx
@@ -11,9 +11,8 @@ function AppRouter(): ReactElement {
   return (
     <Router basename={import.meta.env.BASE_URL}>
       <Switch>
-        <Route exact path="/" component={AppInit} />
         <Route path="/notebook/:notebookPath+" component={AppInit} />
-        <Redirect from="*" to="/" />
+        <Route path="*" component={AppInit} />
       </Switch>
     </Router>
   );
