f704370cea0ffc1f0abf3a72b57291e5174724c6
Brian Ingles <brianingles@deephaven.io>
2023-04-10 12:32:10 -0500

    Refactored custom Spectrum theming
    #1210

--- packages/components/src/SpectrumProvider.tsx
+++ packages/components/src/SpectrumProvider.tsx
@@ -1,28 +1,69 @@
 import React from 'react';
-import cl from 'classnames';
-import { defaultTheme, Provider } from '@adobe/react-spectrum';
-import styles from './SpectrumProvider.module.scss';
+import { defaultTheme, Provider, ProviderProps } from '@adobe/react-spectrum';
+import Log from '@deephaven/log';
+
+import darkDH from '../scss/SpectrumThemeDark.module.scss';
+import lightDH from '../scss/SpectrumThemeLight.module.scss';
+
+const log = Log.module('SpectrumProvider');
+
+const { global, light, dark, medium, large } = defaultTheme;
+
+// Extend light + dark theme variables with DH defaults.
+const themeDHDefault = {
+  global,
+  light: {
+    ...light,
+    ...lightDH,
+  },
+  dark: {
+    ...dark,
+    ...darkDH,
+  },
+  // scales
+  medium,
+  large,
+};
 
 export interface SpectrumProviderProps {
   colorScheme?: 'dark' | 'light';
+  theme?: ProviderProps['theme'];
+  /**
+   * Since this provider may be used to wrap components that are not at the root
+   * of the application, we may want to leave the background color alone so that
+   * components can "inherit" parent component background colors.
+   */
+  transparentBackground?: boolean;
 }
 
 /**
  * Custom React Spectrum provider with theme customization overrides.
  */
 function SpectrumProvider({
-  colorScheme = 'dark',
   children,
+  colorScheme = 'dark',
+  theme: themeCustom,
+  transparentBackground = false,
 }: React.PropsWithChildren<SpectrumProviderProps>): JSX.Element {
+  const style = React.useMemo(
+    () => ({
+      backgroundColor: transparentBackground ? 'transparent' : undefined,
+    }),
+    [transparentBackground]
+  );
+
+  const theme = React.useMemo(
+    () => ({
+      ...themeDHDefault,
+      ...themeCustom,
+    }),
+    [themeCustom]
+  );
+
+  log.debug('Theme', theme);
+
   return (
-    <Provider
-      UNSAFE_className={cl(
-        styles.component,
-        styles[`component-${colorScheme}`]
-      )}
-      colorScheme={colorScheme}
-      theme={defaultTheme}
-    >
+    <Provider UNSAFE_style={style} colorScheme={colorScheme} theme={theme}>
       {children}
     </Provider>
   );
