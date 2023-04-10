f215eaab2ea57b424b1903147fbcd7a4b7045b1f
Brian Ingles <brianingles@deephaven.io>
2023-04-10 15:47:34 -0500

    Added theme comment
    #1210

--- packages/components/src/SpectrumProvider.tsx
+++ packages/components/src/SpectrumProvider.tsx
@@ -10,6 +10,34 @@ const log = Log.module('SpectrumProvider');
 const { global, light, dark, medium, large } = defaultTheme;
 
 // Extend light + dark theme variables with DH defaults.
+// A theme is just a mapped collection of css class names that are generated
+// from various css modules.
+// e.g.
+// {
+//   global: {
+//     spectrum: 'spectrum_9e130c',
+//     'spectrum--medium': 'spectrum--medium_9e130c',
+//     'spectrum--large': 'spectrum--large_9e130c',
+//     'spectrum--darkest': 'spectrum--darkest_9e130c',
+//     'spectrum--dark': 'spectrum--dark_9e130c',
+//     'spectrum--light': 'spectrum--light_9e130c',
+//     'spectrum--lightest': 'spectrum--lightest_9e130c',
+//   },
+//   light: {
+//     'spectrum--light': 'spectrum--light_a40724',
+//     'dh-spectrum-theme--light': '_dh-spectrum-theme--light_1hblg_22',
+//   },
+//   dark: {
+//     'spectrum--darkest': 'spectrum--darkest_256eeb',
+//     'dh-spectrum-theme--dark': '_dh-spectrum-theme--dark_f7kge_22',
+//   },
+//   medium: {
+//     'spectrum--medium': 'spectrum--medium_4b172c',
+//   },
+//   large: {
+//     'spectrum--large': 'spectrum--large_c40598',
+//   },
+// }
 const themeDHDefault = {
   global,
   light: {
