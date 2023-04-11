94afdf3d03fe264bc4155f1598ba1119e0a5dfd0
Brian Ingles <brianingles@deephaven.io>
2023-04-11 11:11:04 -0500

    Split out Spectrum custom theme into a util module
    #1210

---
+++ packages/components/src/SpectrumUtils.ts
@@ -0,0 +1,54 @@
+import { defaultTheme } from '@adobe/react-spectrum';
+import darkDH from '../scss/SpectrumThemeDark.module.scss';
+import lightDH from '../scss/SpectrumThemeLight.module.scss';
+
+const { global, light, dark, medium, large } = defaultTheme;
+
+/**
+ * Extend light + dark theme variables with DH defaults.
+ *
+ * A theme is just a mapped collection of css class names that are generated
+ * from a collection of css modules.
+ *
+ * e.g.
+ * {
+ *   global: {
+ *     spectrum: 'spectrum_9e130c',
+ *     'spectrum--medium': 'spectrum--medium_9e130c',
+ *     'spectrum--large': 'spectrum--large_9e130c',
+ *     'spectrum--darkest': 'spectrum--darkest_9e130c',
+ *     'spectrum--dark': 'spectrum--dark_9e130c',
+ *     'spectrum--light': 'spectrum--light_9e130c',
+ *     'spectrum--lightest': 'spectrum--lightest_9e130c',
+ *   },
+ *   light: {
+ *     'spectrum--light': 'spectrum--light_a40724',
+ *     'dh-spectrum-theme--light': '_dh-spectrum-theme--light_1hblg_22',
+ *   },
+ *   dark: {
+ *     'spectrum--darkest': 'spectrum--darkest_256eeb',
+ *     'dh-spectrum-theme--dark': '_dh-spectrum-theme--dark_f7kge_22',
+ *   },
+ *   medium: {
+ *     'spectrum--medium': 'spectrum--medium_4b172c',
+ *   },
+ *   large: {
+ *     'spectrum--large': 'spectrum--large_c40598',
+ *   },
+ * }
+ */
+/* eslint-disable import/prefer-default-export */
+export const themeDHDefault = {
+  global,
+  light: {
+    ...light,
+    ...lightDH,
+  },
+  dark: {
+    ...dark,
+    ...darkDH,
+  },
+  // scales
+  medium,
+  large,
+};
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -43,6 +43,7 @@ export * from './SelectValueList';
 export * from './shortcuts';
 export { default as SocketedButton } from './SocketedButton';
 export { default as SpectrumProvider } from './SpectrumProvider';
+export * from './SpectrumUtils';
 export { default as ThemeExport } from './ThemeExport';
 export { default as TimeInput } from './TimeInput';
 export { default as TimeSlider } from './TimeSlider';
