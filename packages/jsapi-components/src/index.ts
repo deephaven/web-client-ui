36a5613b28541a02f9c0b9df881837f79c028254
Brian Ingles <brianingles@deephaven.io>
2023-04-14 17:44:02 -0500

    useViewportData hook

--- packages/jsapi-components/src/index.ts
+++ packages/jsapi-components/src/index.ts
@@ -3,3 +3,4 @@ export { default as useTable } from './useTable';
 export { default as useTableColumn } from './useTableColumn';
 export { default as useTableListener } from './useTableListener';
 export { default as useSelectDistinctTable } from './useSelectDistinctTable';
+export { default as useViewportData } from './useViewportData';
---
+++ packages/jsapi-components/src/useViewportData.ts
@@ -0,0 +1,74 @@
+import { useCallback, useEffect } from 'react';
+import { useListData } from '@adobe/react-spectrum';
+import { Table, TreeTable } from '@deephaven/jsapi-shim';
+import {
+  KeyedItem,
+  RowDeserializer,
+  createOnTableUpdatedHandler,
+  defaultRowDeserializer,
+  generateEmptyKeyedItems,
+  getSize,
+  isClosed,
+  padFirstAndLastRow,
+} from '@deephaven/jsapi-utils';
+import useTableListener from './useTableListener';
+
+export interface UseViewportDataProps<T> {
+  table: Table | TreeTable | null;
+  viewportSize?: number;
+  viewportPadding?: number;
+  deserializeRow?: RowDeserializer<T>;
+}
+
+export default function useViewportData<T>({
+  table,
+  viewportSize = 10,
+  viewportPadding = 50,
+  deserializeRow = defaultRowDeserializer,
+}: UseViewportDataProps<T>) {
+  const viewport = useListData<KeyedItem<T>>({});
+
+  // We only want this to fire 1x once the table exists. Note that `useListData`
+  // has no way to respond to a reference change of the `table` instance so we
+  // have to manually delete any previous keyed items from the list.
+  useEffect(() => {
+    if (table) {
+      if (viewport.items.length) {
+        viewport.remove(...viewport.items.keys());
+      }
+
+      viewport.insert(0, ...generateEmptyKeyedItems<T>(getSize(table)));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [table]);
+
+  const setViewport = useCallback(
+    (firstRow: number) => {
+      const [first, last] = padFirstAndLastRow(
+        firstRow,
+        viewportSize,
+        viewportPadding
+      );
+      table?.setViewport(first, last);
+    },
+    [table, viewportPadding, viewportSize]
+  );
+
+  useTableListener(
+    table,
+    dh.Table.EVENT_UPDATED,
+    createOnTableUpdatedHandler(table, viewport, deserializeRow)
+  );
+
+  useEffect(() => {
+    if (table && !isClosed(table)) {
+      table.setViewport(0, viewportSize + viewportPadding - 1);
+    }
+  }, [table, viewportPadding, viewportSize]);
+
+  return {
+    viewport,
+    size: getSize(table),
+    setViewport,
+  };
+}
