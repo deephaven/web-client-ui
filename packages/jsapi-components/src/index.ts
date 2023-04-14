a70f7234ea6861a664c5614db5c6c93e55c1127a
Brian Ingles <brianingles@deephaven.io>
2023-04-14 17:43:45 -0500

    useSelectDistinctTable hook

--- packages/jsapi-components/src/index.ts
+++ packages/jsapi-components/src/index.ts
@@ -2,3 +2,4 @@ export { default as TableInput } from './TableInput';
 export { default as useTable } from './useTable';
 export { default as useTableColumn } from './useTableColumn';
 export { default as useTableListener } from './useTableListener';
+export { default as useSelectDistinctTable } from './useSelectDistinctTable';
---
+++ packages/jsapi-components/src/useSelectDistinctTable.ts
@@ -0,0 +1,31 @@
+import { useCallback, useEffect } from 'react';
+import { Table, TreeTable } from '@deephaven/jsapi-shim';
+import { usePromiseFactory } from '@deephaven/react-hooks';
+
+/**
+ * Creates and subscribes to a `selectDistinct` derived table and unsubscribes
+ * on unmount.
+ * @param table The table to call `selectDistinct` on.
+ * @param columnNames The list of column names to pass to `selectDistinct`.
+ */
+export default function useSelectDistinctTable(
+  table: Table | TreeTable | null,
+  ...columnNames: string[]
+) {
+  const selectDistinct = useCallback(
+    async () => table?.selectDistinct(table.findColumns(columnNames)) ?? null,
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [table, ...columnNames]
+  );
+
+  const { data: distinctTable } = usePromiseFactory(selectDistinct, []);
+
+  useEffect(
+    () => () => {
+      distinctTable?.close();
+    },
+    [distinctTable]
+  );
+
+  return distinctTable;
+}
