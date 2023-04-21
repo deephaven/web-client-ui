1f4e4ae1b2555fd846e603b3bbe921ad2b54fdc7
Brian Ingles <brianingles@deephaven.io>
2023-04-21 12:02:27 -0500

    useRollupTable hook

--- packages/jsapi-components/src/useSetPaddedViewportCallback.ts
+++ packages/jsapi-components/src/useSetPaddedViewportCallback.ts
@@ -25,6 +25,7 @@ export default function useSetPaddedViewportCallback(
         viewportPadding,
         getSize(table)
       );
+
       table?.setViewport(first, last);
     },
     [table, viewportPadding, viewportSize]
