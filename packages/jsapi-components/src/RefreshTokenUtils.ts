fc7dfe34123bf79d134a1cf56f461746d6d5fa1c
mikebender <mikebender@deephaven.io>
2023-05-01 10:19:32 -0400

    Wrap parsing the refresh token with a try/catch
    - Probably not a problem, as we write the cookie, but on the outside chance someone modified their cookies should handle it safely
    - Also refactored deleting the refresh token a bit

--- packages/jsapi-components/src/RefreshTokenUtils.ts
+++ packages/jsapi-components/src/RefreshTokenUtils.ts
@@ -1,5 +1,6 @@
 import { createContext } from 'react';
 import Cookies from 'js-cookie';
+import Log from '@deephaven/log';
 
 export const REFRESH_TOKEN_KEY = 'io.deephaven.web.client.auth.refreshToken';
 
@@ -7,13 +8,22 @@ export type RefreshToken = { bytes: string; expiry: number };
 
 export const RefreshTokenContext = createContext<RefreshToken | null>(null);
 
+const log = Log.module('RefreshTokenUtils');
+
 /**
  * Read the refresh token from our cookie store
  * @returns RefreshToken if it exists, null otherwise
  */
 export function readRefreshToken(): RefreshToken | null {
   const cookieToken = Cookies.get(REFRESH_TOKEN_KEY);
-  return cookieToken != null ? JSON.parse(cookieToken) : null;
+  try {
+    if (cookieToken != null) {
+      return JSON.parse(cookieToken);
+    }
+  } catch (e) {
+    log.error('Error parsing refresh token', cookieToken, e);
+  }
+  return null;
 }
 
 /**
@@ -21,19 +31,20 @@ export function readRefreshToken(): RefreshToken | null {
  * @param token The refresh token to store
  */
 export function storeRefreshToken(token: RefreshToken | null) {
-  if (token != null) {
-    const cookieToken = JSON.stringify({
-      bytes: token.bytes,
-      expiry: token.expiry,
-    });
-    const expires = new Date(token.expiry);
-
-    Cookies.set(REFRESH_TOKEN_KEY, cookieToken, {
-      secure: true,
-      sameSite: 'strict',
-      expires,
-    });
-  } else {
+  if (token == null) {
     Cookies.remove(REFRESH_TOKEN_KEY);
+    return;
   }
+
+  const cookieToken = JSON.stringify({
+    bytes: token.bytes,
+    expiry: token.expiry,
+  });
+  const expires = new Date(token.expiry);
+
+  Cookies.set(REFRESH_TOKEN_KEY, cookieToken, {
+    secure: true,
+    sameSite: 'strict',
+    expires,
+  });
 }
