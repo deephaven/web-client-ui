1683829aeb778ceb96d19b8e62131a48ef881105
mikebender <mikebender@deephaven.io>
2023-05-03 15:11:36 -0400

    Fix up tests for useDebouncedValue

--- packages/react-hooks/src/useDebouncedValue.test.ts
+++ packages/react-hooks/src/useDebouncedValue.test.ts
@@ -9,45 +9,57 @@ afterAll(() => {
   jest.useRealTimers();
 });
 
-// it('should return the initial value', () => {
-//   const value = 'mock value';
-//   const { result } = renderHook(() => useDebouncedValue(value));
-//   expect(result.current).toBe(value);
-// });
+it('should return the initial value', () => {
+  const value = 'mock value';
+  const { result } = renderHook(() => useDebouncedValue(value));
+  expect(result.current).toBe(value);
+});
 
-// it('should return the initial value after the debounce time has elapsed', () => {
-//   const value = 'mock value';
-//   const { result, rerender } = renderHook(() => useDebouncedValue(value));
-//   expect(result.current).toBe(value);
-//   rerender();
-//   jest.advanceTimersByTime(DEFAULT_DEBOUNCE_MS + 5);
-//   expect(result.current).toBe(value);
-// });
+it('should return the initial value after the debounce time has elapsed', () => {
+  const value = 'mock value';
+  const { result, rerender } = renderHook(() => useDebouncedValue(value));
+  expect(result.current).toBe(value);
+  rerender();
+  act(() => {
+    jest.advanceTimersByTime(DEFAULT_DEBOUNCE_MS + 5);
+  });
+  expect(result.current).toBe(value);
+});
 
-it('should return the updated value after the debounce time has elapsed', async () => {
+it('should return the updated value after the debounce time has elapsed', () => {
   const value = 'mock value';
   const newValue = 'mock new value';
-  const {
-    result,
-    rerender,
-    waitForNextUpdate,
-  } = renderHook(({ val = value } = {}) => useDebouncedValue(val));
+  const { result, rerender } = renderHook((val = value) =>
+    useDebouncedValue(val)
+  );
   expect(result.current).toBe(value);
-  console.log('MJB rerendering new value');
   rerender(newValue);
-  jest.advanceTimersByTime(DEFAULT_DEBOUNCE_MS + 5);
-  console.log('MJB after timers');
-  await waitForNextUpdate();
+  act(() => {
+    jest.advanceTimersByTime(DEFAULT_DEBOUNCE_MS + 5);
+  });
   expect(result.current).toBe(newValue);
 });
 
-// it('should not return an intermediate value if the debounce time has not elapsed', () => {
-//   const value = 'mock value';
-//   const intermediateValue = 'mock intermediate value';
-//   const newValue = 'mock new value';
-//   const { result, rerender } = renderHook(() => useDebouncedValue(value));
-//   expect(result.current).toBe(value);
-//   rerender(newValue);
-//   jest.advanceTimersByTime(DEFAULT_DEBOUNCE_MS + 5);
-//   expect(result.current).toBe(newValue);
-// });
+it('should not return an intermediate value if the debounce time has not elapsed', () => {
+  const value = 'mock value';
+  const intermediateValue = 'mock intermediate value';
+  const newValue = 'mock new value';
+  const { result, rerender } = renderHook((val = value) =>
+    useDebouncedValue(val)
+  );
+  expect(result.current).toBe(value);
+  rerender(intermediateValue);
+  act(() => {
+    jest.advanceTimersByTime(DEFAULT_DEBOUNCE_MS - 5);
+  });
+  expect(result.current).toBe(value);
+  rerender(newValue);
+  act(() => {
+    jest.advanceTimersByTime(DEFAULT_DEBOUNCE_MS - 5);
+  });
+  expect(result.current).toBe(value);
+  act(() => {
+    jest.advanceTimersByTime(DEFAULT_DEBOUNCE_MS + 5);
+  });
+  expect(result.current).toBe(newValue);
+});
--- packages/react-hooks/src/useDebouncedValue.ts
+++ packages/react-hooks/src/useDebouncedValue.ts
@@ -12,16 +12,12 @@ export function useDebouncedValue<T>(
   value: T,
   debounceMs = DEFAULT_DEBOUNCE_MS
 ) {
-  console.log('render', value);
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
   useEffect(() => {
-    console.log('MJB setting timeout', value);
     const timeoutId = setTimeout(() => {
-      console.log('MJB setting debounced value');
       setDebouncedValue(value);
     }, debounceMs);
     return () => {
-      console.log('MJB clearing timeout');
       clearTimeout(timeoutId);
     };
   }, [value, debounceMs]);
