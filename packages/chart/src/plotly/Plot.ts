94a29ee6d3ff6ac923e2a863dba2d08b1b39bf9c
Matthew Runyon <matthewrunyon@deephaven.io>
2023-05-19 14:41:55 -0500

    Address review comment

--- packages/chart/src/plotly/Plot.ts
+++ packages/chart/src/plotly/Plot.ts
@@ -4,4 +4,13 @@
 import createPlotlyComponent from 'react-plotly.js/factory.js';
 import Plotly from './Plotly';
 
-export default createPlotlyComponent(Plotly);
+// Webpack 5 (used in docusaurus) gives an object w/ a default key
+// This is probably something on react-plotly.js's side
+// Or because we lazy load this and Docusaurus ends up w/ some complications between ESM and CJS
+export default typeof createPlotlyComponent === 'function'
+  ? createPlotlyComponent(Plotly)
+  : // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    (createPlotlyComponent.default(Plotly) as ReturnType<
+      typeof createPlotlyComponent
+    >);
