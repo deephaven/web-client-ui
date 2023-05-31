ef89cbc809ea36d95284f93f86aa41e0db689a03
Mike Bender <mikebender@deephaven.io>
2023-05-31 09:38:48 -0400

    Revert "refactor: Clean up golden-layout css (#1322)"
    This reverts commit d1838d7151e31de1e26da8b60f336e991bb25404.

--- packages/dashboard/src/layout/index.ts
+++ packages/dashboard/src/layout/index.ts
@@ -1,3 +1,4 @@
+export { default as GoldenLayoutThemeExport } from './GoldenLayoutThemeExport';
 export { default as LayoutUtils } from './LayoutUtils';
 export { default as GLPropTypes } from './GLPropTypes';
 
