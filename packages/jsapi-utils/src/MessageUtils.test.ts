1cb0f3d28b6d6411d630c45211450a33b2d9573a
mikebender <mikebender@deephaven.io>
2023-06-21 12:19:01 -0400

    fix: AuthPluginParent wasn't working when embedded in an iframe
    - Check for window.opener _or_ window.parent when using AuthPluginParent
    - Reorder plugin priority to use AuthPluginParent first if specified
    - Tested using an example html page that both embeds and pops open a new tab

--- packages/jsapi-utils/src/MessageUtils.test.ts
+++ packages/jsapi-utils/src/MessageUtils.test.ts
@@ -1,3 +1,4 @@
+import { TestUtils } from '@deephaven/utils';
 import {
   makeMessage,
   makeResponse,
@@ -7,11 +8,11 @@ import {
 
 it('Throws an exception if called on a window without parent', async () => {
   await expect(requestParentResponse('request')).rejects.toThrow(
-    'window.opener is null, unable to send request.'
+    'window parent is null, unable to send request.'
   );
 });
 
-describe('requestParentResponse', () => {
+describe('requestParentResponse with opener', () => {
   let addListenerSpy: jest.SpyInstance;
   let removeListenerSpy: jest.SpyInstance;
   let listenerCallback;
@@ -34,6 +35,81 @@ describe('requestParentResponse', () => {
     removeListenerSpy.mockRestore();
     mockPostMessage.mockClear();
     window.opener = originalWindowOpener;
+    messageId = undefined;
+  });
+
+  it('Posts message to parent and subscribes to response', async () => {
+    requestParentResponse('request');
+    expect(mockPostMessage).toHaveBeenCalledWith(
+      expect.objectContaining(makeMessage('request', messageId)),
+      '*'
+    );
+    expect(addListenerSpy).toHaveBeenCalledWith(
+      'message',
+      expect.any(Function)
+    );
+  });
+
+  it('Resolves with the payload from the parent window response and unsubscribes', async () => {
+    const PAYLOAD = 'PAYLOAD';
+    const promise = requestParentResponse('request');
+    listenerCallback({
+      data: makeResponse(messageId, PAYLOAD),
+    });
+    const result = await promise;
+    expect(result).toBe(PAYLOAD);
+    expect(removeListenerSpy).toHaveBeenCalledWith('message', listenerCallback);
+  });
+
+  it('Ignores unrelated response, rejects on timeout', async () => {
+    jest.useFakeTimers();
+    const promise = requestParentResponse('request');
+    listenerCallback({
+      data: makeMessage('wrong-id'),
+    });
+    jest.runOnlyPendingTimers();
+    await expect(promise).rejects.toThrow('Request timed out');
+    jest.useRealTimers();
+  });
+
+  it('Times out if no response', async () => {
+    jest.useFakeTimers();
+    const promise = requestParentResponse('request');
+    jest.runOnlyPendingTimers();
+    expect(removeListenerSpy).toHaveBeenCalled();
+    await expect(promise).rejects.toThrow('Request timed out');
+    jest.useRealTimers();
+  });
+});
+
+describe('requestParentResponse with parent', () => {
+  let addListenerSpy: jest.SpyInstance;
+  let removeListenerSpy: jest.SpyInstance;
+  let windowParentSpy: jest.SpyInstance;
+  let listenerCallback;
+  let messageId;
+  const mockPostMessage = jest.fn((data: Message<unknown>) => {
+    messageId = data.id;
+  });
+  beforeEach(() => {
+    addListenerSpy = jest
+      .spyOn(window, 'addEventListener')
+      .mockImplementation((event, cb) => {
+        listenerCallback = cb;
+      });
+    removeListenerSpy = jest.spyOn(window, 'removeEventListener');
+    windowParentSpy = jest.spyOn(window, 'parent', 'get').mockReturnValue(
+      TestUtils.createMockProxy<Window>({
+        postMessage: mockPostMessage,
+      })
+    );
+  });
+
+  afterEach(() => {
+    addListenerSpy.mockRestore();
+    removeListenerSpy.mockRestore();
+    windowParentSpy.mockRestore();
+    mockPostMessage.mockClear();
     messageId = undefined;
   });
 
