8af698edf949a09ed844e7e3b50ab698b98e712c
Brian Ingles <brianingles@deephaven.io>
2023-07-03 11:45:37 -0500

    feat: "Extends" TypeScript util type
    #1393

--- packages/utils/src/TypeUtils.ts
+++ packages/utils/src/TypeUtils.ts
@@ -1,4 +1,19 @@
 /**
+ * Util type to create a "subtype" of T. Useful for creating subsets of union
+ * types.
+ *
+ * e.g.
+ * declare type Direction = 'north' | 'south' | 'east' | 'west'
+ *
+ * // This works
+ * type Down = Subset<Direction, 'south'> // 'south'
+ *
+ * // Compiler will complain
+ * type NotDirection = Subset<Direction, 'blah'>
+ */
+export type Extends<T, U extends T> = U;
+
+/**
  * Util type to extract the value from an object.
  *
  * e.g. Given
