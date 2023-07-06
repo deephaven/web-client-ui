154ccfccd5a6f9996d67f0fcc71d031985bfd6a5
Matthew Runyon <matthewrunyon@deephaven.io>
2023-07-06 11:34:41 -0500

    feat: Outdent code when running selection from a notebook (#1391)
    Fixes #1326

--- packages/console/src/notebook/ScriptEditor.tsx
+++ packages/console/src/notebook/ScriptEditor.tsx
@@ -11,6 +11,7 @@ import Editor from './Editor';
 import { MonacoProviders, MonacoUtils } from '../monaco';
 import './ScriptEditor.scss';
 import SHORTCUTS from '../ConsoleShortcuts';
+import ScriptEditorUtils from './ScriptEditorUtils';
 
 const log = Log.module('ScriptEditor');
 
@@ -133,12 +134,13 @@ class ScriptEditor extends Component<ScriptEditorProps, ScriptEditorState> {
     if (endColumn === 1 && endLineNumber > startLineNumber) {
       endLineNumber -= 1;
     }
-    const startLineMinColumn = model?.getLineMinColumn(startLineNumber);
-    const endLineMaxColumn = model?.getLineMaxColumn(endLineNumber);
+    const startLineMinColumn = model.getLineMinColumn(startLineNumber);
+    const endLineMaxColumn = model.getLineMaxColumn(endLineNumber);
     const wholeLineRange = range
       .setStartPosition(startLineNumber, startLineMinColumn)
       .setEndPosition(endLineNumber, endLineMaxColumn);
-    return model?.getValueInRange(wholeLineRange);
+
+    return ScriptEditorUtils.outdentCode(model.getValueInRange(wholeLineRange));
   }
 
   handleEditorInitialized(innerEditor: editor.IStandaloneCodeEditor): void {
