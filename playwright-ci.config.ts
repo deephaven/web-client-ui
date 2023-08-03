0fcfc52e744dbb28c33971616e22b474860d2132
Matthew Runyon <matthewrunyon@deephaven.io>
2023-08-03 12:30:27 -0500

    Fix docker build using stale packages from local build

--- playwright-ci.config.ts
+++ playwright-ci.config.ts
@@ -3,7 +3,6 @@ import DefaultConfig from './playwright.config';
 
 const config: PlaywrightTestConfig = {
   ...DefaultConfig,
-  snapshotDir: './tests',
   webServer: {
     // Only start the main code-studio server right now
     // To test embed-grid and embed-chart, should have an array set for `webServer` and run them all separately as there's a port check
