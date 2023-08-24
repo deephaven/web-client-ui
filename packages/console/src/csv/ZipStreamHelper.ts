fa167421685ddfd08a2800d40c8b626cfcaeafe7
Matthew Runyon <matthewrunyon@deephaven.io>
2023-08-23 22:10:21 -0500

    Add comments

--- packages/console/src/csv/ZipStreamHelper.ts
+++ packages/console/src/csv/ZipStreamHelper.ts
@@ -1,5 +1,10 @@
 import type { JSZipObject, OnUpdateCallback, JSZipStreamHelper } from 'jszip';
 
+/**
+ * This is used to help papaparse understand our stream.
+ * It uses these fields for feature detection, but never actually calls read()
+ * https://github.com/mholt/PapaParse/blob/master/papaparse.js#L244
+ */
 interface ZipStreamHelper extends JSZipStreamHelper<string> {
   readable: boolean;
   read(): void;
@@ -14,6 +19,7 @@ export default function makeZipStreamHelper(
     zipObj as JSZipObject & {
       // The type could be anything except nodebuffer from https://stuk.github.io/jszip/documentation/api_zipobject/internal_stream.html
       // We only need it as a string though
+      // JSZip types don't include this method for some reason
       internalStream(type: 'string'): JSZipStreamHelper<string>;
     }
   ).internalStream('string') as ZipStreamHelper;
