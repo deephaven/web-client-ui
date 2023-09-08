1f584e54fc7d54b8d010bf1921cb6861e208c191
Brian Ingles <brianingles@deephaven.io>
2023-09-08 13:43:23 -0500

    Forbid @deephaven modules from self importing
    #1497

---
+++ packageManifest.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+function buildPackageManifest() {
+  const packageDirNames = fs
+    .readdirSync(path.join(__dirname, 'packages'), { withFileTypes: true })
+    .filter(ent => ent.isDirectory())
+    .map(({ name }) => name);
+
+  // Map of package name to directory name
+  const packageManifest = new Map();
+
+  packageDirNames.forEach(dirName => {
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    const packageName = require(path.join(
+      __dirname,
+      'packages',
+      dirName,
+      'package.json'
+    )).name;
+
+    packageManifest.set(packageName, dirName);
+  });
+
+  return {
+    packageNames: [...packageManifest.keys()],
+    packageManifest,
+  };
+}
+
+module.exports = buildPackageManifest;
