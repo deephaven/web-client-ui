9d905fca86aa8ba4ff53debd1fd12dcc9132299b
George Wan <georgewan@deephaven.io>
2023-09-19 11:21:48 -0700

    refactor: Export reducers from dashboard package (#1522)
    * Exported reducers defined in `dashboard` package
    * Removed unused reducers from `dashboard-core-plugins` and `redux`
    packages
    
    * Resolves #1487

--- packages/dashboard-core-plugins/src/redux/reducers/sessionWrapper.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Set the active session being used to communicate with the server.
- */
-import { replaceReducer } from '@deephaven/redux';
-import { SET_SESSION_WRAPPER } from '../actionTypes';
-
-export default replaceReducer(SET_SESSION_WRAPPER, null);
--- packages/dashboard/src/redux/index.ts
+++ packages/dashboard/src/redux/index.ts
@@ -5,6 +5,7 @@ Object.entries(reducers).map(([name, reducer]) =>
   reducerRegistry.register(name, reducer)
 );
 
+export { reducers };
 export * from './actions';
 export * from './actionTypes';
 export * from './selectors';
