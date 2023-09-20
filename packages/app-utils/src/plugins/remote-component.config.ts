313e9494e0266856589ff1c56c8c99306aa0d7a9
mikebender <mikebender@deephaven.io>
2023-09-19 21:25:39 -0400

    Add some more externalized packages

--- packages/app-utils/src/plugins/remote-component.config.ts
+++ packages/app-utils/src/plugins/remote-component.config.ts
@@ -8,9 +8,11 @@ import react from 'react';
 import * as redux from 'redux';
 import * as reactRedux from 'react-redux';
 import ReactDOM from 'react-dom';
+import * as AdobeReactSpectrum from '@adobe/react-spectrum';
 import * as DeephavenAuthPlugins from '@deephaven/auth-plugins';
 import * as DeephavenChart from '@deephaven/chart';
 import * as DeephavenComponents from '@deephaven/components';
+import * as DeephavenIcons from '@deephaven/icons';
 import * as DeephavenIrisGrid from '@deephaven/iris-grid';
 import * as DeephavenJsapiBootstrap from '@deephaven/jsapi-bootstrap';
 import * as DeephavenJsapiComponents from '@deephaven/jsapi-components';
@@ -24,9 +26,11 @@ export const resolve = {
   'react-dom': ReactDOM,
   redux,
   'react-redux': reactRedux,
+  '@adobe/react-spectrum': AdobeReactSpectrum,
   '@deephaven/auth-plugins': DeephavenAuthPlugins,
   '@deephaven/chart': DeephavenChart,
   '@deephaven/components': DeephavenComponents,
+  '@deephaven/icons': DeephavenIcons,
   '@deephaven/iris-grid': DeephavenIrisGrid,
   '@deephaven/jsapi-bootstrap': DeephavenJsapiBootstrap,
   '@deephaven/jsapi-components': DeephavenJsapiComponents,
