7b092b4c9b037c6ff8eb97c96be9cbb413abfed4
Brian Ingles <brianingles@deephaven.io>
2023-09-28 15:54:17 -0500

    Moved themes inside of src/
    #1504

--- packages/components/src/theme/ThemeUtils.ts
+++ packages/components/src/theme/ThemeUtils.ts
@@ -1,7 +1,7 @@
 import Log from '@deephaven/log';
 import { assertNotNull } from '@deephaven/utils';
-import darkTheme from '../../scss/theme_default_dark.scss?inline';
-import lightTheme from '../../scss/theme_default_light.scss?inline';
+import darkTheme from './theme_default_dark.scss';
+import lightTheme from './theme_default_light.scss';
 import {
   DEFAULT_DARK_THEME_KEY,
   DEFAULT_LIGHT_THEME_KEY,
