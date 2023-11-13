8a4ff3fbeb6bb48545111698cacd2809272a09a5
Don McKenzie <donmckenzie@deephaven.io>
2023-11-13 16:24:58 -0500

    updated based on review

--- packages/components/src/Select.tsx
+++ packages/components/src/Select.tsx
@@ -2,38 +2,20 @@ import React, { useCallback } from 'react';
 import classNames from 'classnames';
 import { useForwardedRef } from '@deephaven/react-hooks';
 
-export type SelectProps = {
-  children: React.ReactNode;
-  onBlur?: React.FocusEventHandler<HTMLSelectElement>;
+type baseSelectProps = Omit<React.HTMLProps<HTMLSelectElement>, 'onChange'>;
+
+export type SelectProps = baseSelectProps & {
   onChange: (value: string) => void;
-  onKeyDown?: React.KeyboardEventHandler<HTMLSelectElement>;
-  id?: string;
-  className?: string;
-  defaultValue?: string;
-  name?: string;
-  title?: string;
-  value?: string;
-  disabled?: boolean;
   'data-testid'?: string;
-  'aria-label'?: string;
 };
 
 const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
   (props, forwardedRef) => {
     const {
       children,
-      onBlur,
-      onChange,
-      onKeyDown,
-      id,
       className,
-      defaultValue,
-      name,
-      title,
-      value,
-      disabled,
+      onChange,
       'data-testid': dataTestId,
-      'aria-label': ariaLabel,
       ...rest
     } = props;
 
@@ -48,19 +30,10 @@ const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
 
     return (
       <select
-        id={id}
         ref={ref}
         className={classNames('custom-select', className)}
-        onBlur={onBlur}
         onChange={handleChange}
-        onKeyDown={onKeyDown}
-        defaultValue={defaultValue}
-        value={value}
-        name={name}
-        title={title}
-        disabled={disabled}
         data-testid={dataTestId}
-        aria-label={ariaLabel}
         // eslint-disable-next-line react/jsx-props-no-spreading
         {...rest}
       >
