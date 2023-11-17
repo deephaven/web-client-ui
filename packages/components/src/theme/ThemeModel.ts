5f46c84aa2d7fa1d23610251c21f8e981cf14dd5
Brian Ingles <brianingles@deephaven.io>
2023-11-17 16:47:34 -0600

    Color var type
    #1634

--- packages/components/src/theme/ThemeModel.ts
+++ packages/components/src/theme/ThemeModel.ts
@@ -1,6 +1,13 @@
 export type BaseThemeType = 'dark' | 'light';
 export type BaseThemeKey = `default-${BaseThemeType}`;
 export type CssVariableStyleContent = `:root{${string}`;
+
+export type ThemePreloadColorVariable =
+  | '--dh-color-loading-spinner-primary'
+  | '--dh-color-loading-spinner-secondary'
+  | '--dh-color-background'
+  | '--dh-color-foreground';
+
 export type ThemeIconVariable =
   | '--dh-svg-icon-close-tab'
   | '--dh-svg-icon-maximise'
@@ -12,15 +19,15 @@ export const DEFAULT_DARK_THEME_KEY = 'default-dark' satisfies BaseThemeKey;
 export const DEFAULT_LIGHT_THEME_KEY = 'default-light' satisfies BaseThemeKey;
 
 // Css properties that are used in preload data with default values.
-export const DEFAULT_PRELOAD_COLOR_VARIABLES = {
+export const DEFAULT_PRELOAD_COLOR_VARIABLES: Record<
+  ThemePreloadColorVariable,
+  string
+> = {
   '--dh-color-loading-spinner-primary': '#3b6bda', // dark theme --dh-color-blue-600
   '--dh-color-loading-spinner-secondary': '#f0f0ee80', // dark theme --dh-color-gray-800 + 50% opacity
   '--dh-color-background': '#1a171a', // dark theme --dh-color-gray-50
   '--dh-color-foreground': '#f0f0ee', // dark theme --dh-color-gray-800
-} satisfies Record<`--dh-${string}`, string>;
-
-export type ThemePreloadColorVariable =
-  keyof typeof DEFAULT_PRELOAD_COLOR_VARIABLES;
+};
 
 export const THEME_CACHE_LOCAL_STORAGE_KEY = 'deephaven.themeCache';
 
