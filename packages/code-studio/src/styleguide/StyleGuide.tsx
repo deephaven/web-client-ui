a2ef05681f348f02f46859909875e61c959a66dc
Brian Ingles <brianingles@deephaven.io>
2023-11-17 08:46:19 -0600

    fix: Isolate Styleguide snapshots (#1649)
    I realized that snapshot tests for Styleguide were a bit brittle in that
    changes to a section in the Styleguide can cause subtle pixel shifts in
    snapshots of other sections. The snapshot tests now hide all sections
    not being tested to ensure they don't impact one another

--- packages/code-studio/src/styleguide/StyleGuide.tsx
+++ packages/code-studio/src/styleguide/StyleGuide.tsx
@@ -39,8 +39,20 @@ const stickyProps = {
 } as const;
 
 function StyleGuide(): React.ReactElement {
+  const isolateSection = window.location.search.includes('isolateSection=true');
+
   return (
     <div className="container style-guide-container">
+      {/* For e2e tests this allows us to isolate sections for snapshots. This 
+      mitigates an issue where a change to a section in the styleguide can cause
+      subtle pixel shifts in other sections */}
+      {isolateSection && (
+        <style>
+          {`.${HIDE_FROM_E2E_TESTS_CLASS}, .sample-section:not(${window.location.hash}), :not(.sample-section) > h2 {
+          display: none;
+        }`}
+        </style>
+      )}
       <Flex
         justifyContent="space-between"
         alignItems="center"
