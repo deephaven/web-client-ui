ef2e7beb7a664c96b3fe7636d0415401759d786b
Brian Ingles <brianingles@deephaven.io>
2023-11-30 14:05:23 -0600

    Added misconfiguration identifiers

--- packages/code-studio/src/styleguide/Swatch.tsx
+++ packages/code-studio/src/styleguide/Swatch.tsx
@@ -42,9 +42,19 @@ export function Swatch({ className, children }: SwatchProps): JSX.Element {
     });
   }, [ref]);
 
+  const hasValue = tooltip != null && tooltip.value !== '';
+
   return (
-    <div ref={ref} className={className}>
-      {tooltip != null && (
+    <div
+      ref={ref}
+      className={className}
+      style={{
+        border: hasValue
+          ? undefined
+          : '2px solid var(--dh-color-notice-default-bg)',
+      }}
+    >
+      {hasValue && (
         <Tooltip>
           <div>{tooltip.value}</div>
           <div>{tooltip.normalized}</div>
