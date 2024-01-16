37ec1ce73e1f6072c323de4b341998fedecbae7f
Don McKenzie <donmckenzie@deephaven.io>
2024-01-16 16:27:05 -0500

    linnttttttt

--- packages/console/src/common/Code.tsx
+++ packages/console/src/common/Code.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, ReactNode } from 'react';
+import React, { useEffect, useRef, ReactNode, useCallback } from 'react';
 import * as monaco from 'monaco-editor';
 import { useTheme } from '@deephaven/components';
 
@@ -7,15 +7,11 @@ interface CodeProps {
   language: string;
 }
 
-function Code({ children, language }: CodeProps) {
+function Code({ children, language }: CodeProps): JSX.Element {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const { activeThemes } = useTheme();
 
-  useEffect(() => {
-    if (activeThemes != null) colorize();
-  }, [activeThemes]);
-
-  const colorize = async () => {
+  const colorize = useCallback(async () => {
     if (containerRef.current && children != null) {
       const result = await monaco.editor.colorize(
         children.toString(),
@@ -24,7 +20,11 @@ function Code({ children, language }: CodeProps) {
       );
       containerRef.current.innerHTML = result;
     }
-  };
+  }, [children, language]);
+
+  useEffect(() => {
+    if (activeThemes != null) colorize();
+  }, [activeThemes, colorize]);
 
   return (
     <div
@@ -32,7 +32,7 @@ function Code({ children, language }: CodeProps) {
       // Add pointerEvents: 'none' has huge benefits on performance with Hit Test testing on large colorized elements.
       // You can still select the text event with this set
       style={{ pointerEvents: 'none' }}
-    ></div>
+    />
   );
 }
 
