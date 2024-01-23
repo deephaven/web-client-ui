e8f88b2d5481e747aebdedf3c11563d1cca3a7e9
Mike Bender <mikebender@deephaven.io>
2024-01-22 21:59:13 -0500

    fix: Fix useDeferredApi export
    - It wasn't actually being exported
    - Also add a DeferredApiBootstrap component to gate on loading the API

---
+++ packages/jsapi-bootstrap/src/DeferredApiBootstrap.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import useDeferredApi from './useDeferredApi';
+import { ApiContext } from './ApiBootstrap';
+
+type DeferredApiBootstrapProps = React.PropsWithChildren<{
+  onError?: (error: unknown) => void;
+  /**
+   * Options to use when fetching the deferred API.
+   */
+  options?: Record<string, unknown>;
+}>;
+
+/**
+ * Does not render children until the deferred API is resolved.
+ */
+const DeferredApiBootstrap = React.memo(
+  ({
+    children,
+    onError,
+    options,
+  }: DeferredApiBootstrapProps): JSX.Element | null => {
+    const [api, apiError] = useDeferredApi(options);
+    if (apiError != null) {
+      onError?.(apiError);
+      return null;
+    }
+    if (api == null) {
+      // Still waiting for the API to load
+      return null;
+    }
+    return <ApiContext.Provider value={api}>{children}</ApiContext.Provider>;
+  }
+);
+
+DeferredApiBootstrap.displayName = 'DeferredApiBootstrap';
+
+export default DeferredApiBootstrap;
