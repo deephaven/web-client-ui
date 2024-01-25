af5f5f45c035fb0e2a9ca4c07a65070cbbd0ad0c
Mike Bender <mikebender@deephaven.io>
2024-01-25 14:44:14 -0500

    fix: Fix useDeferredApi export (#1742)
    - It wasn't actually being exported
    - Also add a DeferredApiBootstrap component to gate on loading the API

--- packages/jsapi-bootstrap/src/index.ts
+++ packages/jsapi-bootstrap/src/index.ts
@@ -1,4 +1,6 @@
 export * from './ApiBootstrap';
 export * from './ClientBootstrap';
+export * from './DeferredApiBootstrap';
 export * from './useApi';
 export * from './useClient';
+export * from './useDeferredApi';
