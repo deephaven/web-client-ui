c5e04f488701111fa57db229a6bde7319a41a7d2
Steven Wu <wusteven815@gmail.com>
2024-02-08 15:17:25 -0500

    style: format python code

--- tests/docker-scripts/data/app.d/common_figures.py
+++ tests/docker-scripts/data/app.d/common_figures.py
@@ -2,22 +2,45 @@ from deephaven import empty_table
 from deephaven.plot.figure import Figure
 import math
 
-simple_plot = Figure().plot_xy(series_name="Test", t=empty_table(100).update(["x=i", "y=Math.sin(i)", "z=Math.cos(i)"]), x="x", y="y").show()
+simple_plot = (
+    Figure()
+    .plot_xy(
+        series_name="Test",
+        t=empty_table(100).update(["x=i", "y=Math.sin(i)", "z=Math.cos(i)"]),
+        x="x",
+        y="y",
+    )
+    .show()
+)
 
 
 math_funcs = ["sin", "cos"]
-shapes = ["SQUARE", "CIRCLE", "UP_TRIANGLE", "DOWN_TRIANGLE", "RIGHT_TRIANGLE", "LEFT_TRIANGLE", "DIAMOND"]
+shapes = [
+    "SQUARE",
+    "CIRCLE",
+    "UP_TRIANGLE",
+    "DOWN_TRIANGLE",
+    "RIGHT_TRIANGLE",
+    "LEFT_TRIANGLE",
+    "DIAMOND",
+]
 # Unsupported shapes: ["ELLIPSE", "HORIZONTAL_RECTANGLE", "VERTICAL_RECTANGLE"]
 
 # Create a generic table that has enough columns to display all the shapes
 # Re-uses some of the funcs
+
 trig_table = empty_table(50).update(["x=i*0.1"])
 for i in range(len(shapes)):
-    trig_table = trig_table.update([f"y{i}=Math.{math_funcs[i % len(math_funcs)]}(x+{math.floor(i / len(math_funcs))})"])
-
+    trig_table = trig_table.update(
+        [
+            f"y{i}=Math.{math_funcs[i % len(math_funcs)]}(x+{math.floor(i / len(math_funcs))})"
+        ]
+    )
 # Generate the table and figure based on the shapes created
+
 trig_figure = Figure()
 for i in range(len(shapes)):
-    trig_figure = trig_figure.plot_xy(series_name=shapes[i], t=trig_table, x="x", y=f"y{i}").point(shape=shapes[i], size=len(shapes) - i, visible=True)
-
-trig_figure = trig_figure.show();
+    trig_figure = trig_figure.plot_xy(
+        series_name=shapes[i], t=trig_table, x="x", y=f"y{i}"
+    ).point(shape=shapes[i], size=len(shapes) - i, visible=True)
+trig_figure = trig_figure.show()
--- tests/docker-scripts/data/app.d/common_tables.py
+++ tests/docker-scripts/data/app.d/common_tables.py
@@ -6,31 +6,44 @@ scale = 999
 
 simple_table = empty_table(100).update(["x=i", "y=Math.sin(i)", "z=Math.cos(i)"])
 
-column_groups = [{ 'name': 'YandZ', 'children': ['y', 'z'] }, { 'name': 'All', 'children': ['x', 'YandZ'], 'color': 'white' }]
+column_groups = [
+    {"name": "YandZ", "children": ["y", "z"]},
+    {"name": "All", "children": ["x", "YandZ"], "color": "white"},
+]
 simple_table_header_group = simple_table.layout_hints(column_groups=column_groups)
-simple_table_header_group_hide = simple_table.layout_hints(column_groups=column_groups, hide=['y', 'z'])
+simple_table_header_group_hide = simple_table.layout_hints(
+    column_groups=column_groups, hide=["y", "z"]
+)
 
-double_and_string = new_table([
-double_col("Doubles", [3.1, 5.45, -1.0, 1.0, 3.0, 4.20]),
-string_col("Strings", ["Creating", "New", "Tables", "Tables", "New", "Creating"])
-])
+double_and_string = new_table(
+    [
+        double_col("Doubles", [3.1, 5.45, -1.0, 1.0, 3.0, 4.20]),
+        string_col(
+            "Strings", ["Creating", "New", "Tables", "Tables", "New", "Creating"]
+        ),
+    ]
+)
 
-all_types = empty_table(size).update([
-  "String=(i%11==0 ? null : `a` + (int)(scale*(i%2==0? i+1 : 1)))",
-  "Int=(i%12==0 ? null : (int)(scale*(i*2-1)))",
-  "Long=(i%13==0 ? null : (long)(scale*(i*2-1)))",
-  "Float=(float)(i%14==0 ? null : i%10==0 ? 1.0F/0.0F: i%5==0 ? -1.0F/0.0F : (float) scale*(i*2-1))",
-  "Double=(double)(i%16==0 ? null : i%10==0 ? 1.0D/0.0D: i%5==0 ? -1.0D/0.0D : (double) scale*(i*2-1))",
-  "Bool = (i%17==0 ? null : (int)(i)%2==0)",
-  "Char = (i%18==0 ? null : new Character((char) (((26+i*i)%26)+97)) )",
-  "Short=(short)(i%19==0 ? null : (int)(scale*(i*2-1)))",
-  "BigDec=(i%21==0 ? null : new java.math.BigDecimal(scale*(i*2-1)))",
-  "BigInt=(i%22==0 ? null : new java.math.BigInteger(Integer.toString((int)(scale*(i*2-1)))))",
-  "Byte=(Byte)(i%19==0 ? null : new Byte( Integer.toString((int)(i))))",
-])
+all_types = empty_table(size).update(
+    [
+        "String=(i%11==0 ? null : `a` + (int)(scale*(i%2==0? i+1 : 1)))",
+        "Int=(i%12==0 ? null : (int)(scale*(i*2-1)))",
+        "Long=(i%13==0 ? null : (long)(scale*(i*2-1)))",
+        "Float=(float)(i%14==0 ? null : i%10==0 ? 1.0F/0.0F: i%5==0 ? -1.0F/0.0F : (float) scale*(i*2-1))",
+        "Double=(double)(i%16==0 ? null : i%10==0 ? 1.0D/0.0D: i%5==0 ? -1.0D/0.0D : (double) scale*(i*2-1))",
+        "Bool = (i%17==0 ? null : (int)(i)%2==0)",
+        "Char = (i%18==0 ? null : new Character((char) (((26+i*i)%26)+97)) )",
+        "Short=(short)(i%19==0 ? null : (int)(scale*(i*2-1)))",
+        "BigDec=(i%21==0 ? null : new java.math.BigDecimal(scale*(i*2-1)))",
+        "BigInt=(i%22==0 ? null : new java.math.BigInteger(Integer.toString((int)(scale*(i*2-1)))))",
+        "Byte=(Byte)(i%19==0 ? null : new Byte( Integer.toString((int)(i))))",
+    ]
+)
 
-ordered_int_and_offset = empty_table(20).update([
-    "MyString=(`str`+i)",
-    "MyInt1=(i+100)",
-    "MyInt2=(i+200)",
-])
+ordered_int_and_offset = empty_table(20).update(
+    [
+        "MyString=(`str`+i)",
+        "MyInt1=(i+100)",
+        "MyInt2=(i+200)",
+    ]
+)
