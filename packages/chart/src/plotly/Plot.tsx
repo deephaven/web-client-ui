d9be4694aee702523dc68f5fda9092415fa76c88
Matthew Runyon <matthewrunyon@deephaven.io>
2024-02-11 19:54:48 -0600

    Add loading spinner as fallback

--- packages/chart/src/plotly/Plot.tsx
+++ packages/chart/src/plotly/Plot.tsx
@@ -1,10 +1,11 @@
+import { LoadingOverlay } from '@deephaven/components';
 import { lazy, Suspense } from 'react';
 
 const PlotBase = lazy(() => import('./PlotBase.js'));
 
 function Plot(props: React.ComponentProps<typeof PlotBase>): JSX.Element {
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={<LoadingOverlay />}>
       {/* eslint-disable react/jsx-props-no-spreading */}
       <PlotBase {...props} />
     </Suspense>
