2c773f3cc8de0e7146051c065c25c9038c445632
Brian Ingles <brianingles@deephaven.io>
2024-02-20 17:12:34 -0600

    Support children and items (#plugins-292)

--- packages/code-studio/src/styleguide/Pickers.tsx
+++ packages/code-studio/src/styleguide/Pickers.tsx
@@ -1,27 +1,64 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React, { useMemo } from 'react';
 import { Item, Picker } from '@deephaven/components';
+import { vsPerson } from '@deephaven/icons';
+import { Flex, Icon, Text } from '@adobe/react-spectrum';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { sampleSectionIdAndClasses } from './utils';
 
 export function Pickers(): JSX.Element {
-  const items = useMemo(
+  const mixedItems = useMemo(
     () => [
-      'x',
-      'y',
-      'z',
-      4,
-      9,
-      <Item key="b" id="a">
-        Aaa
+      'String 1',
+      'String 2',
+      'String 3',
+      444,
+      999,
+      /* eslint-disable react/jsx-key */
+      <Item id="a">Item Aaa</Item>,
+      <Item id="b">Item Bbb</Item>,
+      <Item id="c">
+        <Icon>
+          <FontAwesomeIcon icon={vsPerson} />
+        </Icon>
+        <Text>Complex Ccc</Text>
       </Item>,
+      /* eslint-enable react/jsx-key */
     ],
     []
   );
 
   return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
     <div {...sampleSectionIdAndClasses('pickers')}>
       <h2 className="ui-title">Pickers</h2>
-      <Picker items={items} />
+
+      <Flex gap={14}>
+        <Picker label="Mixed Item Types" items={mixedItems} />
+
+        <Picker label="Single Child">
+          <Item id="a">Aaa</Item>
+        </Picker>
+
+        <Picker label="Multiple Children">
+          <Item id="a">Aaa</Item>
+          <Item id="b">Bbb</Item>
+        </Picker>
+
+        <Picker label="Complex Items">
+          <Item id="a">
+            <Icon>
+              <FontAwesomeIcon icon={vsPerson} />
+            </Icon>
+            <Text>Person A</Text>
+          </Item>
+          <Item id="b">
+            <Icon>
+              <FontAwesomeIcon icon={vsPerson} />
+            </Icon>
+            <Text>Person B</Text>
+          </Item>
+        </Picker>
+      </Flex>
     </div>
   );
 }
--- packages/components/src/Picker.tsx
+++ packages/components/src/Picker.tsx
@@ -1,33 +1,72 @@
+import { ReactElement, ReactNode, useMemo } from 'react';
 import {
   Item as SpectrumItem,
   Picker as SpectrumPicker,
 } from '@adobe/react-spectrum';
-import { ReactElement, useMemo } from 'react';
+import type { SpectrumPickerProps } from '@react-types/select';
 import type { ItemProps } from './Item';
 
-export interface PickerProps<
+function mapItem<
   TID extends number | string,
   TItem extends TID | ReactElement<ItemProps<TID>>,
-> {
-  items: TItem[];
+>(
+  item: TItem
+): {
+  id: TID;
+  display: ReactNode;
+} {
+  return typeof item === 'object'
+    ? { id: item.props.id, display: item.props.children }
+    : { id: item as TID, display: String(item) };
 }
+
+export type PickerProps<
+  TID extends number | string,
+  TItem extends TID | ReactElement<ItemProps<TID>>,
+> = (
+  | {
+      // Items provided via the items prop can be primitives or Item elements
+      items: TItem[];
+      children?: undefined;
+    }
+  | {
+      // Items provided via children prop have to be Item elements
+      children: ReactElement<ItemProps<TID>> | ReactElement<ItemProps<TID>>[];
+      items?: undefined;
+    }
+) &
+  Omit<
+    SpectrumPickerProps<{
+      id: TID;
+      display: ReactNode;
+    }>,
+    'children' | 'items'
+  >;
 
 export function Picker<
   TID extends number | string,
   TItem extends TID | ReactElement<ItemProps<TID>>,
->({ items }: PickerProps<TID, TItem>): JSX.Element {
-  const itemsWithKeys = useMemo(
-    () =>
-      items.map(item =>
-        typeof item === 'object'
-          ? { id: item.props.id, display: item.props.children }
-          : { id: item, display: String(item) }
-      ),
-    [items]
+>({
+  children,
+  items,
+  ...spectrumPickerProps
+}: PickerProps<TID, TItem>): JSX.Element {
+  const itemsInternal = useMemo(() => {
+    if (items == null) {
+      return Array.isArray(children) ? children : [children];
+    }
+
+    return items;
+  }, [children, items]);
+
+  const itemsWithIds = useMemo(
+    () => itemsInternal.map(mapItem),
+    [itemsInternal]
   );
 
   return (
-    <SpectrumPicker items={itemsWithKeys}>
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <SpectrumPicker label {...spectrumPickerProps} items={itemsWithIds}>
       {({ display }) => <SpectrumItem>{display}</SpectrumItem>}
     </SpectrumPicker>
   );
