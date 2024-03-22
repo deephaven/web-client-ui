2b94503b7f33dcf30be24786637bc83feb9986bd
Brian Ingles <brianingles@deephaven.io>
2024-03-22 12:02:53 -0500

    usePopoverOnScrollRef tests (#1858)

--- packages/react-hooks/src/usePopoverOnScrollRef.test.tsx
+++ packages/react-hooks/src/usePopoverOnScrollRef.test.tsx
@@ -20,7 +20,7 @@ describe('usePopoverOnScrollRef', () => {
   const mock = {
     event: createMockProxy<Event>(),
     findScrollArea: jest.fn(),
-    getInitialScrollPosition: jest.fn<Promise<number>, []>(),
+    getInitialScrollPosition: jest.fn<Promise<number | null>, []>(),
     onScrollA: jest.fn(),
     onScrollB: jest.fn(),
     result: {
@@ -47,7 +47,7 @@ describe('usePopoverOnScrollRef', () => {
     asMock(mock.onScrollA).mockName('onScroll');
   });
 
-  function testCommon(getInitialScrollPosition?: () => Promise<number>) {
+  function testCommon(getInitialScrollPosition?: () => Promise<number | null>) {
     const renderResult = renderHook(
       props => usePopoverOnScrollRef(mock.findScrollArea, ...props),
       { initialProps: [mock.onScrollA, getInitialScrollPosition] as const }
@@ -74,75 +74,93 @@ describe('usePopoverOnScrollRef', () => {
     expect(mock.findScrollArea).not.toHaveBeenCalled();
   });
 
-  describe('onOpenChange callback', () => {
-    it.each([undefined, mock.getInitialScrollPosition])(
-      'should initialize scroll position on open if given getInitialScrollPosition callback: %s',
-      async getInitialScrollPosition => {
-        const { result } = testCommon(getInitialScrollPosition);
+  describe.each([null, mock.result.getInitialScrollPosition])(
+    'onOpenChange callback',
+    position => {
+      it.each([undefined, mock.getInitialScrollPosition])(
+        'should initialize scroll position on open if given getInitialScrollPosition callback: %s',
+        async getInitialScrollPosition => {
+          asMock(mock.getInitialScrollPosition).mockResolvedValue(position);
+
+          const { result } = testCommon(getInitialScrollPosition);
+
+          result.current.onOpenChange(true);
+
+          act(() => {
+            // deal with the setTimeout(..., 0)
+            jest.advanceTimersByTime(0);
+          });
+
+          if (getInitialScrollPosition != null) {
+            expect(mock.getInitialScrollPosition).toHaveBeenCalled();
+
+            jest.runAllTimers();
+            await TestUtils.flushPromises();
+
+            if (position == null) {
+              expect(mock.result.findScrollArea.scroll).not.toHaveBeenCalled();
+            } else {
+              expect(mock.result.findScrollArea.scroll).toHaveBeenCalledWith(
+                0,
+                mock.result.getInitialScrollPosition
+              );
+            }
+          }
+        }
+      );
+
+      it.each([true, false])(
+        'should register a scroll handler on open',
+        testMultipleOpens => {
+          const { result } = testCommon();
+
+          result.current.onOpenChange(true);
+
+          if (testMultipleOpens) {
+            result.current.onOpenChange(false);
+            result.current.onOpenChange(true);
+          }
+
+          expect(mock.findScrollArea).not.toHaveBeenCalled();
+
+          act(() => {
+            jest.advanceTimersByTime(0);
+          });
+
+          expect(mock.findScrollArea).toHaveBeenCalledTimes(1);
+
+          expect(mock.findScrollArea).toHaveBeenCalledWith(
+            result.current.ref.current
+          );
+          expect(
+            mock.result.findScrollArea.addEventListener
+          ).toHaveBeenCalledWith('scroll', expect.any(Function));
+        }
+      );
+
+      it('should call latest onScroll handler on scroll event', () => {
+        const { result, rerender } = testCommon();
 
         result.current.onOpenChange(true);
 
         act(() => {
-          // deal with the setTimeout(..., 0)
           jest.advanceTimersByTime(0);
         });
 
-        if (getInitialScrollPosition != null) {
-          expect(mock.getInitialScrollPosition).toHaveBeenCalled();
+        const scrollHandlerA = getLastRegisteredOnScrollHandler();
+        scrollHandlerA(mock.event);
 
-          jest.runAllTimers();
-          await TestUtils.flushPromises();
+        expect(mock.onScrollA).toHaveBeenCalledWith(mock.event);
 
-          expect(mock.result.findScrollArea.scroll).toHaveBeenCalledWith(
-            0,
-            mock.result.getInitialScrollPosition
-          );
-        }
-      }
-    );
+        jest.clearAllMocks();
+        rerender([mock.onScrollB, undefined]);
 
-    it('should register a scroll handler on open', () => {
-      const { result } = testCommon();
+        const scrollHandlerB = getLastRegisteredOnScrollHandler();
+        scrollHandlerB(mock.event);
 
-      result.current.onOpenChange(true);
-
-      expect(mock.findScrollArea).not.toHaveBeenCalled();
-
-      act(() => {
-        jest.advanceTimersByTime(0);
+        expect(mock.onScrollA).not.toHaveBeenCalled();
+        expect(mock.onScrollB).toHaveBeenCalledWith(mock.event);
       });
-
-      expect(mock.findScrollArea).toHaveBeenCalledWith(
-        result.current.ref.current
-      );
-      expect(mock.result.findScrollArea.addEventListener).toHaveBeenCalledWith(
-        'scroll',
-        expect.any(Function)
-      );
-    });
-
-    it('should call latest onScroll handler on scroll event', () => {
-      const { result, rerender } = testCommon();
-
-      result.current.onOpenChange(true);
-
-      act(() => {
-        jest.advanceTimersByTime(0);
-      });
-
-      const scrollHandlerA = getLastRegisteredOnScrollHandler();
-      scrollHandlerA(mock.event);
-
-      expect(mock.onScrollA).toHaveBeenCalledWith(mock.event);
-
-      jest.clearAllMocks();
-      rerender([mock.onScrollB, undefined]);
-
-      const scrollHandlerB = getLastRegisteredOnScrollHandler();
-      scrollHandlerB(mock.event);
-
-      expect(mock.onScrollA).not.toHaveBeenCalled();
-      expect(mock.onScrollB).toHaveBeenCalledWith(mock.event);
-    });
-  });
+    }
+  );
 });
