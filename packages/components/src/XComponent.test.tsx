0b27b4d05cd3fc3f878bcc0c4a33ca9005577755
mikebender <mikebender@deephaven.io>
2024-03-28 16:47:21 -0400

    feat: Create XComponent framework
    - Allows for adding extendable components within your components
    - Similar to swizzling in obj-c, docusaurus

---
+++ packages/components/src/XComponent.test.tsx
@@ -0,0 +1,64 @@
+/**
+ * Test file for ExtendableComponent
+ */
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { createXComponent } from './XComponent';
+import { XComponentMapProvider } from './XComponentMap';
+
+function MyComponent({ children }: { children?: React.ReactNode } = {}) {
+  return (
+    <div>
+      MyComponent
+      <div>{children}</div>
+    </div>
+  );
+}
+
+const XMyComponent = createXComponent(MyComponent);
+
+function MyReplacementComponent() {
+  return <div>MyReplacementComponent</div>;
+}
+
+function MyReplacementWrapperComponent({
+  children,
+}: { children?: React.ReactNode } = {}) {
+  return (
+    <div>
+      <div>MyReplacementWrapperComponent</div>
+      <XMyComponent.Original>{children}</XMyComponent.Original>
+    </div>
+  );
+}
+
+describe('ExtendableComponent', () => {
+  it('should render the original component', () => {
+    const { getByText } = render(<XMyComponent />);
+    expect(getByText('MyComponent')).toBeInTheDocument();
+  });
+
+  it('should render the replacement component', () => {
+    const { getByText } = render(
+      <XComponentMapProvider
+        value={new Map([[XMyComponent, MyReplacementComponent]])}
+      >
+        <XMyComponent />
+      </XComponentMapProvider>
+    );
+    expect(getByText('MyReplacementComponent')).toBeInTheDocument();
+  });
+
+  it('should render the original component inside the replacement component', () => {
+    const { getByText } = render(
+      <XComponentMapProvider
+        value={new Map([[XMyComponent, MyReplacementWrapperComponent]])}
+      >
+        <XMyComponent />
+      </XComponentMapProvider>
+    );
+    expect(getByText('MyReplacementWrapperComponent')).toBeInTheDocument();
+    expect(getByText('MyComponent')).toBeInTheDocument();
+  });
+});
---
+++ packages/components/src/XComponent.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useXComponent, XComponentType } from './XComponentMap';
+
+/**
+ * Helper function that will wrap the provided component, and return an ExtendableComponent type.
+ * Whenever that ExtendableComponent is used, it will check if there is a replacement component for the provided component on the context.
+ * If there is, it will use that component instead of the provided component.
+ * This is a similar concept to how swizzling is done in Docusaurus or obj-c, but for any React component.
+ *
+ * Usage:
+ *
+ * ```tsx
+ * function MyComponent() {
+ *   return <div>MyComponent</div>;
+ * }
+ * const XMyComponent = extendableComponent(MyComponent);
+ *
+ *
+ * function MyReplacementComponent() {
+ *  return <div>MyReplacementComponent</div>;
+ * }
+ *
+ *
+ * ...
+ *
+ * <XMyComponent /> // Will render MyComponent
+ *
+ * ...
+ *
+ * <XComponentMapProvider value={new Map([[XMyComponent, MyReplacementComponent]])}>
+ *   <XMyComponent /> // Will render MyReplacementComponent
+ * </XComponentMapProvider>
+ *
+ * ```
+ *
+ * Is useful in cases where we have a component deep down in the component tree that we want to replace with a different component, but don't want to
+ * have to provide props at the top level just to hook into that.
+ *
+ * @param Component The component to wrap
+ * @returns The wrapped component
+ */
+export function createXComponent<P extends Record<string, unknown>>(
+  Component: React.ComponentType<P>
+): XComponentType<P> {
+  const XComponent = function ExtendableComponent(props: P): JSX.Element {
+    const ReplacementComponent = useXComponent(XComponent);
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    return <ReplacementComponent {...props} />;
+  };
+
+  XComponent.Original = Component;
+
+  return XComponent;
+}
+
+export default createXComponent;
