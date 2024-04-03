f566e720ef46e7b814c0ecc4c6dddcd1e40c86a7
Brian Ingles <brianingles@deephaven.io>
2024-04-03 16:48:25 -0500

    Mocking virtualizer (#1909)

--- packages/code-studio/src/styleguide/StyleGuide.test.tsx
+++ packages/code-studio/src/styleguide/StyleGuide.test.tsx
@@ -14,6 +14,25 @@ describe('<StyleGuide /> mounts', () => {
     // Provide a non-null array to ThemeProvider to tell it to initialize
     const customThemes: ThemeData[] = [];
 
+    // React Spectrum `useVirtualizerItem` depends on `scrollWidth` and `scrollHeight`.
+    // Mocking these to avoid React "Maximum update depth exceeded" errors.
+    // https://github.com/adobe/react-spectrum/blob/0b2a838b36ad6d86eee13abaf68b7e4d2b4ada6c/packages/%40react-aria/virtualizer/src/useVirtualizerItem.ts#L49C3-L49C60
+
+    // From preview docs: https://reactspectrum.blob.core.windows.net/reactspectrum/726a5e8f0ed50fc8d98e39c74bd6dfeb3660fbdf/docs/react-spectrum/testing.html#virtualized-components
+    // The virtualizer will now think it has a visible area of 1000px x 1000px and that the items within it are 40px x 40px
+    jest
+      .spyOn(window.HTMLElement.prototype, 'clientWidth', 'get')
+      .mockImplementation(() => 1000);
+    jest
+      .spyOn(window.HTMLElement.prototype, 'clientHeight', 'get')
+      .mockImplementation(() => 1000);
+    jest
+      .spyOn(window.HTMLElement.prototype, 'scrollHeight', 'get')
+      .mockImplementation(() => 40);
+    jest
+      .spyOn(window.HTMLElement.prototype, 'scrollWidth', 'get')
+      .mockImplementation(() => 40);
+
     expect(() =>
       render(
         <ApiContext.Provider value={dh}>
