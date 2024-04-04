55d0fc2430ee829e0a0bbdb86a4ab1ec5c3482a1
Brian Ingles <brianingles@deephaven.io>
2024-04-04 13:20:14 -0500

    cleanup (#1909)

--- packages/react-hooks/src/useCheckOverflow.ts
+++ packages/react-hooks/src/useCheckOverflow.ts
@@ -2,18 +2,37 @@ import { useCallback, useState } from 'react';
 import type { DOMRefValue } from '@react-types/shared';
 
 export interface CheckOverflowResult {
+  /**
+   * Callback to check if a Spectrum `DOMRefValue` is overflowing. If an
+   * overflowing value is passed, `isOverflowing` will be set to true. Note that
+   * calling again with a non-overflowing value will *not* reset the state.
+   * Instead `resetIsOverflowing` must be called explicitly. This is to allow
+   * multiple `DOMRefValue`s to be checked and `isOverflowing` to remain `true`
+   * if at least one of them is overflowing.
+   */
   checkOverflow: <T extends HTMLElement>(elRef: DOMRefValue<T> | null) => void;
+
+  /**
+   * Will be set to true whenever `checkOverflow` is called with an overflowing
+   * `DOMRefValue`. It will remain `true` until `resetIsOverflowing` is called.
+   * Default state is `false`.
+   */
   isOverflowing: boolean;
+
+  /** Reset `isOverflowing` to false */
   resetIsOverflowing: () => void;
 }
 
+/**
+ * Provides a callback to check a Spectrum `DOMRefValue` for overflow. If
+ * overflow is detected, `isOverflowing` will be set to `true` until reset by
+ * calling `resetIsOverflowing`.
+ */
 export function useCheckOverflow(): CheckOverflowResult {
   const [isOverflowing, setIsOverflowing] = useState(false);
 
   /**
-   * Whenever a Spectrum `DOMRefValue` component renders, see if the content is
-   * overflowing so we know whether to render a tooltip showing the full content
-   * or not.
+   * Check if a Spectrum `DOMRefValue` is overflowing.
    */
   const checkOverflow = useCallback(
     <T extends HTMLElement>(elRef: DOMRefValue<T> | null) => {
@@ -30,6 +49,7 @@ export function useCheckOverflow(): CheckOverflowResult {
     []
   );
 
+  /** Reset `isOverflowing` to false */
   const resetIsOverflowing = useCallback(() => {
     setIsOverflowing(false);
   }, []);
