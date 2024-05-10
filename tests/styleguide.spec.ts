4440b27a76036f249324fb95f0af01b75cb04256
Don McKenzie <donmckenzie@deephaven.io>
2024-05-10 17:24:05 -0400

    use ids for button file names in styleguide test

--- tests/styleguide.spec.ts
+++ tests/styleguide.spec.ts
@@ -98,7 +98,7 @@ sampleSectionIds.forEach(id => {
   });
 });
 
-buttonSectionIds.forEach((id, i) => {
+buttonSectionIds.forEach(id => {
   test(`Buttons regression test - ${id}`, async ({ page }) => {
     // Isolate the section
     await page.goto(`/ide/styleguide?isolateSection=true#${id}`);
@@ -132,7 +132,7 @@ buttonSectionIds.forEach((id, i) => {
       // Focus
       await button.focus();
       await expect(sampleSection).toHaveScreenshot(
-        `buttons-focus-section-${i}-${j}${isDisabled ? '-disabled' : ''}.png`
+        `buttons-focus-section-${id}-${j}${isDisabled ? '-disabled' : ''}.png`
       );
 
       if (!isDisabled) {
@@ -147,7 +147,7 @@ buttonSectionIds.forEach((id, i) => {
       }
 
       await expect(sampleSection).toHaveScreenshot(
-        `buttons-hover-section-${i}-${j}${isDisabled ? '-disabled' : ''}.png`
+        `buttons-hover-section-${id}-${j}${isDisabled ? '-disabled' : ''}.png`
       );
       await page.mouse.move(0, 0);
 
