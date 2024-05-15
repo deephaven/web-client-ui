3b8c3bc5761b97f5dd301b72becf282090ff58c1
Brian Ingles <brianingles@deephaven.io>
2024-05-15 10:07:01 -0500

    Fixed bad rebase (#1989)

--- packages/components/src/spectrum/buttons.ts
+++ packages/components/src/spectrum/buttons.ts
@@ -1,8 +1,6 @@
 export {
   ActionButton,
   type SpectrumActionButtonProps as ActionButtonProps,
-  ActionGroup,
-  type SpectrumActionGroupProps as ActionGroupProps,
   // Button - we want to use our own `Button` component instead of Spectrum's
   // ButtonGroup - will re-export once our `Button` is compatible
   // FileTrigger - we aren't planning to support this component
--- packages/components/src/spectrum/collections.ts
+++ packages/components/src/spectrum/collections.ts
@@ -1,8 +1,6 @@
 export {
   ActionBar,
   type SpectrumActionBarProps as ActionBarProps,
-  ActionMenu,
-  type SpectrumActionMenuProps as ActionMenuProps,
   // ListBox - we aren't planning to support this component
   MenuTrigger,
   type SpectrumMenuTriggerProps as MenuTriggerProps,
