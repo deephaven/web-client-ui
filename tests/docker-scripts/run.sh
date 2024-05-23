af51a9d93469c43e4a9d470f50c44c47ef534b1e
Brian Ingles <brianingles@deephaven.io>
2024-05-23 16:47:18 -0500

    had the CI check backwards (#2016)

--- tests/docker-scripts/run.sh
+++ tests/docker-scripts/run.sh
@@ -6,14 +6,17 @@
 # because docker compose run starts dependent containers, but does not stop them
 pushd "$(dirname "$0")" # Set pwd to this directory
 
+args=("$@")
+
+# If we are not in CI, remove the container after running. In CI we want to keep
+# the container around in case we need to dump the logs in another step of the
+# GH action. It should be cleaned up automatically by the CI runner.
 if [[ -z "${CI}" ]]; then
-  # Don't remove container in CI so we can access the logs if something fails.
-  # This should get automatically cleaned up by the CI runner.
-  docker compose run --service-ports --build "$@" # Passes all arguments to the compose file
-else
-# Locally, remove container after running
-  docker compose run --service-ports --build --rm "$@" # Passes all arguments to the compose file
+  args=(--rm "${args[@]}")
 fi
+
+echo docker compose run --service-ports --build "${args[@]}"
+docker compose run --service-ports --build "${args[@]}" # Passes all arguments to the compose file
 
 exit_code=$?
 docker compose down
