7d3bdbbb0df012c96584dc03d8881260604ec080
Brian Ingles <brianingles@deephaven.io>
2024-05-24 10:18:48 -0500

    Testing error config (#2031)

--- tests/docker-scripts/run.sh
+++ tests/docker-scripts/run.sh
@@ -4,9 +4,20 @@
 # The argument should contain the container name to run as the first argument (e.g. web-ui-tests)
 # Once the container finishes its task, this script shuts down the docker containers
 # because docker compose run starts dependent containers, but does not stop them
-
 pushd "$(dirname "$0")" # Set pwd to this directory
-docker compose run --service-ports --build --rm "$@" # Passes all arguments to the compose file
+
+args=("$@")
+
+# If we are not in CI, remove the container after running. In CI we want to keep
+# the container around in case we need to dump the logs in another step of the
+# GH action. It should be cleaned up automatically by the CI runner.
+if [[ -z "${CI}" ]]; then
+  args=(--rm "${args[@]}")
+fi
+
+echo docker compose run --service-ports --build "${args[@]}"
+docker compose run --service-ports --build "${args[@]}" # Passes all arguments to the compose file
+
 exit_code=$?
 docker compose down
 popd # Reset pwd
