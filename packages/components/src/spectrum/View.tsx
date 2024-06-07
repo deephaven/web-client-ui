abcd08d001fc34d1fa049bde1f7dba4b18a8b312
Ethan Alvizo <ethanalvizo@gmail.com>
2024-06-07 19:17:07 -0400

    PR comments

--- packages/components/src/spectrum/View.tsx
+++ packages/components/src/spectrum/View.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { forwardRef, useMemo } from 'react';
+import { CSSProperties, forwardRef, useMemo } from 'react';
 import {
   View as SpectrumView,
   type ViewProps as SpectrumViewProps,
@@ -7,7 +7,17 @@ import {
 import type { DOMRefValue } from '@react-types/shared';
 import { type ColorValue, colorValueStyle } from '../theme/colorUtils';
 
-export type ViewProps = Omit<SpectrumViewProps<6>, 'backgroundColor'> & {
+export type ViewProps = Omit<
+  SpectrumViewProps<6>,
+  | 'backgroundColor'
+  | 'borderColor'
+  | 'borderStartColor'
+  | 'borderEndColor'
+  | 'borderTopColor'
+  | 'borderBottomColor'
+  | 'borderXColor'
+  | 'borderYColor'
+> & {
   backgroundColor?: ColorValue;
   borderColor?: ColorValue;
   borderStartColor?: ColorValue;
@@ -21,13 +31,12 @@ export type ViewProps = Omit<SpectrumViewProps<6>, 'backgroundColor'> & {
 /**
  * A View component that re-exports the Spectrum View component.
  * However, it overrides ColorValues to accept CSS color strings and
- * our custom variable names from our color paletee and semantic colors.
+ * our custom variable names from our color palette and semantic colors.
  *
  * @param props The props for the View component
  * @returns The View component
  *
  */
-
 export const View = forwardRef<DOMRefValue<HTMLElement>, ViewProps>(
   (props, forwardedRef): JSX.Element => {
     const {
@@ -43,33 +52,38 @@ export const View = forwardRef<DOMRefValue<HTMLElement>, ViewProps>(
       ...rest
     } = props;
 
-    // Using shorthand to define border styling. Removing a style property during rerender can lead to styling bugs
-    // ex. changing from borderXColor = blue to borderColor = blue will cause the left and right borders to disappear
-    const defaultBorderColor = colorValueStyle(borderColor) ?? 'transparent';
-    const defaultBorderXColor =
-      colorValueStyle(borderXColor) ?? defaultBorderColor;
-    const defaultBorderYColor =
-      colorValueStyle(borderYColor) ?? defaultBorderColor;
-    const topColor = colorValueStyle(borderTopColor) ?? defaultBorderYColor;
-    const bottomColor =
-      colorValueStyle(borderBottomColor) ?? defaultBorderYColor;
-    const leftColor = colorValueStyle(borderStartColor) ?? defaultBorderXColor;
-    const rightColor = colorValueStyle(borderEndColor) ?? defaultBorderXColor;
+    const borderStyle: CSSProperties = {};
+    if (borderColor) {
+      borderStyle.borderColor = colorValueStyle(borderColor);
+    }
+    if (borderXColor) {
+      borderStyle.borderLeftColor = colorValueStyle(borderXColor);
+      borderStyle.borderRightColor = colorValueStyle(borderXColor);
+    }
+    if (borderYColor) {
+      borderStyle.borderTopColor = colorValueStyle(borderYColor);
+      borderStyle.borderBottomColor = colorValueStyle(borderYColor);
+    }
+    if (borderStartColor) {
+      borderStyle.borderLeftColor = colorValueStyle(borderStartColor);
+    }
+    if (borderEndColor) {
+      borderStyle.borderRightColor = colorValueStyle(borderEndColor);
+    }
+    if (borderTopColor) {
+      borderStyle.borderTopColor = colorValueStyle(borderTopColor);
+    }
+    if (borderBottomColor) {
+      borderStyle.borderBottomColor = colorValueStyle(borderBottomColor);
+    }
 
     const style = useMemo(
       () => ({
         ...UNSAFE_style,
         backgroundColor: colorValueStyle(backgroundColor),
-        borderColor: `${topColor} ${rightColor} ${bottomColor} ${leftColor}`,
+        ...borderStyle,
       }),
-      [
-        backgroundColor,
-        UNSAFE_style,
-        topColor,
-        rightColor,
-        bottomColor,
-        leftColor,
-      ]
+      [backgroundColor, UNSAFE_style, borderStyle]
     );
 
     return <SpectrumView {...rest} ref={forwardedRef} UNSAFE_style={style} />;
