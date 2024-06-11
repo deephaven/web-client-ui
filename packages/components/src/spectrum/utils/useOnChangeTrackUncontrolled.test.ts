a505236cd2e805e84fb32e2da00e12b233969c44
Brian Ingles <brianingles@deephaven.io>
2024-06-11 15:01:10 -0500

    Unit tests (#2065)

---
+++ packages/components/src/spectrum/utils/useOnChangeTrackUncontrolled.test.ts
@@ -0,0 +1,56 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import { ItemKey } from './itemUtils';
+import {
+  useOnChangeTrackUncontrolled,
+  UseOnChangeTrackUncontrolledOptions,
+} from './useOnChangeTrackUncontrolled';
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+afterEach(() => {
+  expect.hasAssertions();
+});
+
+describe('useOnChangeTrackUncontrolled', () => {
+  const defaultSelectedKey = 'default.selectedKey';
+  const changedKey = 'changed.key';
+  const controlledKey = 'controlled.key';
+
+  const props = {
+    controlled: { selectedKey: controlledKey },
+    controlledWithDefault: {
+      defaultSelectedKey,
+      selectedKey: controlledKey,
+    },
+    uncontrolled: {},
+    uncontrolledWithDefault: { defaultSelectedKey },
+  } satisfies Record<string, UseOnChangeTrackUncontrolledOptions<ItemKey>>;
+
+  it.each([
+    [props.uncontrolled, [undefined, changedKey]],
+    [props.uncontrolledWithDefault, [defaultSelectedKey, changedKey]],
+    [props.controlled, [controlledKey, controlledKey]],
+    [props.controlledWithDefault, [controlledKey, controlledKey]],
+  ] as const)(
+    'should track controlled / uncontrolled state: %s, %s',
+    (initialProps, [expectedInitial, expectedChanged]) => {
+      const { result } = renderHook(() =>
+        useOnChangeTrackUncontrolled(initialProps)
+      );
+
+      expect(result.current.selectedKeyMaybeUncontrolled).toEqual(
+        expectedInitial
+      );
+
+      act(() => {
+        result.current.onChangeMaybeUncontrolled(changedKey);
+      });
+
+      expect(result.current.selectedKeyMaybeUncontrolled).toEqual(
+        expectedChanged
+      );
+    }
+  );
+});
