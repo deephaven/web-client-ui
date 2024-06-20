8416e6e757ed81caefd661dace8af386c83e8e00
Brian Ingles <brianingles@deephaven.io>
2024-06-20 11:47:56 -0500

    Made useSearchableViewportData more compatible with useViewportData (#567)

--- packages/jsapi-components/src/useSearchableViewportData.ts
+++ packages/jsapi-components/src/useSearchableViewportData.ts
@@ -12,26 +12,40 @@ import {
 } from '@deephaven/utils';
 import { useDebouncedCallback } from '@deephaven/react-hooks';
 import { useTableUtils } from './useTableUtils';
-import useViewportData, { UseViewportDataResult } from './useViewportData';
+import useViewportData, {
+  UseViewportDataProps,
+  UseViewportDataResult,
+} from './useViewportData';
 import useFilterConditionFactories from './useFilterConditionFactories';
 import useViewportFilter from './useViewportFilter';
 
-export interface SearchableViewportData<TData> {
-  list: UseViewportDataResult<TData, dh.Table>;
+export interface UseSearchableViewportDataProps<TData>
+  extends UseViewportDataProps<TData, dh.Table> {
+  searchColumnNames: string | string[];
+  additionalFilterConditionFactories?: FilterConditionFactory[];
+}
+
+export interface SearchableViewportData<TData>
+  extends UseViewportDataResult<TData, dh.Table> {
   onSearchTextChange: (searchText: string) => void;
 }
 
 /**
  * Use a viewport data list with a search text filter. Supports additional filters.
- * @param maybeTable The table to use
+ * @param table The table to use
+ * @param itemHeight The height of each item
+ * @param scrollDebounce The debounce time for scroll events
+ * @param viewportSize The size of the viewport
+ * @param viewportPadding The padding around the viewport
+ * @param deserializeRow The row deserializer
  * @param searchColumnNames The column names to search
  * @param additionalFilterConditionFactories Additional filter condition factories
  */
-export function useSearchableViewportData<TData>(
-  maybeTable: dh.Table | null,
-  searchColumnNames: string | string[],
-  ...additionalFilterConditionFactories: FilterConditionFactory[]
-): SearchableViewportData<TData> {
+export function useSearchableViewportData<TData>({
+  searchColumnNames,
+  additionalFilterConditionFactories = [],
+  ...props
+}: UseSearchableViewportDataProps<TData>): SearchableViewportData<TData> {
   const tableUtils = useTableUtils();
 
   const [searchText, setSearchText] = useState<string>('');
@@ -47,10 +61,10 @@ export function useSearchableViewportData<TData>(
   );
 
   const list = useViewportData<TData, dh.Table>({
-    table: maybeTable,
     itemHeight: TABLE_ROW_HEIGHT,
     viewportSize: VIEWPORT_SIZE,
     viewportPadding: VIEWPORT_PADDING,
+    ...props,
   });
 
   const filter = useFilterConditionFactories(
@@ -62,7 +76,7 @@ export function useSearchableViewportData<TData>(
   useViewportFilter(list, filter);
 
   return {
-    list,
+    ...list,
     onSearchTextChange,
   };
 }
