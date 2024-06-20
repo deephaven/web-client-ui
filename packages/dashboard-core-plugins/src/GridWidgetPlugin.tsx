76228a9a89f4e351a91921dc90fd5a9793d4729e
mikebender <mikebender@deephaven.io>
2024-06-20 10:58:56 -0400

    fix: DH-17292 Handle disconnect from GridWidgetPlugin
    - When the model is disconnected, we should just display an error. There's no option to reconnect, as the widget schema could change entirely
    - By unloading the IrisGrid component, it's no longer throwing an error by trying to access table methods after a table.close()
    - Fixes DH-17292 from Enterprise
      - Tested using the steps in the description

--- packages/dashboard-core-plugins/src/GridWidgetPlugin.tsx
+++ packages/dashboard-core-plugins/src/GridWidgetPlugin.tsx
@@ -4,11 +4,13 @@ import { type dh } from '@deephaven/jsapi-types';
 import { useApi } from '@deephaven/jsapi-bootstrap';
 import {
   IrisGrid,
+  IrisGridModel,
   IrisGridModelFactory,
-  type IrisGridModel,
 } from '@deephaven/iris-grid';
 import { useSelector } from 'react-redux';
 import { getSettings, RootState } from '@deephaven/redux';
+import { LoadingOverlay } from '@deephaven/components';
+import { getErrorMessage } from '@deephaven/utils';
 
 export function GridWidgetPlugin(
   props: WidgetComponentProps<dh.Table>
@@ -16,12 +18,14 @@ export function GridWidgetPlugin(
   const dh = useApi();
   const settings = useSelector(getSettings<RootState>);
   const [model, setModel] = useState<IrisGridModel>();
+  const [error, setError] = useState<unknown>();
 
   const { fetch } = props;
 
   useEffect(() => {
     let cancelled = false;
     async function init() {
+      setError(undefined);
       const table = await fetch();
       const newModel = await IrisGridModelFactory.makeModel(dh, table);
       if (!cancelled) {
@@ -35,6 +39,36 @@ export function GridWidgetPlugin(
       cancelled = true;
     };
   }, [dh, fetch]);
+
+  useEffect(
+    function startListeningModel() {
+      if (!model) {
+        return;
+      }
+
+      // If the table inside a widget is disconnected, then don't bother trying to listen to reconnect, just close it and show a message
+      // Widget closes the table already when it is disconnected, so no need to close it again
+      function handleDisconnect() {
+        setError(new Error('Model disconnected'));
+        setModel(undefined);
+      }
+
+      model.addEventListener(IrisGridModel.EVENT.DISCONNECT, handleDisconnect);
+
+      return () => {
+        model.removeEventListener(
+          IrisGridModel.EVENT.DISCONNECT,
+          handleDisconnect
+        );
+      };
+    },
+    [model]
+  );
+
+  const errorMessage = getErrorMessage(error);
+  if (errorMessage != null) {
+    return <LoadingOverlay errorMessage={errorMessage} isLoading={false} />;
+  }
 
   return model ? <IrisGrid model={model} settings={settings} /> : null;
 }
