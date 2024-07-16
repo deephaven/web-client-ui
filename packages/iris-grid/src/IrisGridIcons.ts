6d864036726f46bd2c2462c9d7b6210e14f64c45
Matthew Runyon <matthewrunyon@deephaven.io>
2024-07-16 09:30:31 -0500

    feat: IrisGridTheme iconSize (#2123)
    This is part of the work needed for #885 that I felt would be better as
    a separate PR.
    
    Snapshot changes are going to be slightly shifted sort icon and vertical
    center + slight left shift on tree table arrows.

--- packages/iris-grid/src/IrisGridIcons.ts
+++ packages/iris-grid/src/IrisGridIcons.ts
@@ -1,14 +1,11 @@
 import { memoizeClear } from '@deephaven/grid';
 import {
-  dhSortDown,
-  dhSortUp,
   vsTriangleDown,
   vsTriangleRight,
+  vsTriangleUp,
   vsLinkExternal,
   IconDefinition,
 } from '@deephaven/icons';
-
-export const ICON_SIZE = 16;
 
 export type IconName =
   | 'sortUp'
@@ -18,15 +15,15 @@ export type IconName =
   | 'cellOverflow';
 
 const iconMap = new Map<IconName, IconDefinition>([
-  ['sortUp', dhSortUp],
-  ['sortDown', dhSortDown],
+  ['sortUp', vsTriangleUp],
+  ['sortDown', vsTriangleDown],
   ['caretDown', vsTriangleDown],
   ['caretRight', vsTriangleRight],
   ['cellOverflow', vsLinkExternal],
 ]);
 
 const makeIcon = memoizeClear(
-  (name: IconName) => {
+  (name: IconName, size: number) => {
     const faIcon = iconMap.get(name);
     if (faIcon === undefined) {
       throw new Error('Icon is undefined');
@@ -38,8 +35,8 @@ const makeIcon = memoizeClear(
     const icon = new Path2D(path);
     const scaledIcon = new Path2D();
     const scaleMatrix = {
-      a: ICON_SIZE / faIcon.icon[0],
-      d: ICON_SIZE / faIcon.icon[1],
+      a: size / faIcon.icon[0],
+      d: size / faIcon.icon[1],
     };
     scaledIcon.addPath(icon, scaleMatrix);
     return scaledIcon;
@@ -47,6 +44,6 @@ const makeIcon = memoizeClear(
   { max: 1000 }
 );
 
-export function getIcon(name: IconName): Path2D {
-  return makeIcon(name);
+export function getIcon(name: IconName, size: number): Path2D {
+  return makeIcon(name, size);
 }
