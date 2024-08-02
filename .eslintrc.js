b8f5d0ba577f45d0c41da4c997cd9d103cd5ad75
Akshat Jawne <akshatjawne@gmail.com>
2024-08-02 08:19:20 -0600

    fix import exception logic

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,55 +5,49 @@ const { packageNames, packageManifest } = buildPackageManifest();
 module.exports = {
   root: true,
   extends: ['@deephaven/eslint-config'],
-  ignorePatterns: ['packages/golden-layout/*', 'jest.config.*'],
+  ignorePatterns: ['packages/golden-layout/*', 'jest.config.js'],
   overrides: [
-    {
-      files: ['**/*.@(ts|tsx)'],
-      parserOptions: {
-        project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
-        tsconfigRootDir: __dirname,
-      },
-    },
-    // For each @deephaven package, forbid importing from itself
     ...packageNames.map(packageName => ({
       files: [`packages/${packageManifest.get(packageName)}/**/*.@(ts|tsx)`],
+      parserOptions: {
+        project: ['./tsconfig.eslint.json', './packages/**/tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
       rules: {
         'no-restricted-imports': [
           'error',
           {
-            paths: [
-              {
-                name: packageName,
-                message: 'Forbid importing from owning @deephaven package.',
-              },
-            ],
+            name: packageName,
+            message: 'Forbid importing from owning @deephaven package.',
+          },
+          {
+            name: '@adobe/react-spectrum',
+            message: 'forbid importing from @adobe/react-spectrum.',
           },
         ],
       },
     })),
     {
-      files: ['**/*.@(ts|tsx)'],
+      files: [
+        'packages/components/src/spectrum/**/*.@(ts|tsx)',
+        'packages/components/src/theme/**/*.@(ts|tsx)',
+      ],
+      parserOptions: {
+        project: ['./tsconfig.eslint.json', './packages/**/tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
       rules: {
         'no-restricted-imports': [
           'error',
           {
             paths: [
               {
-                name: '@adobe/react-spectrum',
-                message: 'Forbid importing from @adobe/react-spectrum.',
+                name: 'packageName',
+                message: 'forbid importing from owning deephaven package.',
               },
             ],
           },
         ],
-      },
-    },
-    {
-      files: [
-        'packages/components/src/theme/**/*.@(ts|tsx)',
-        'packages/components/src/spectrum/**/*.@(ts|tsx)',
-      ],
-      rules: {
-        'no-restricted-imports': 'off',
       },
     },
   ],
