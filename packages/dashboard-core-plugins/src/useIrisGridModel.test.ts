818c4b37b17b66831fdc01d95d6f45c453920069
mikebender <mikebender@deephaven.io>
2024-08-20 11:43:01 -0400

    Clean up based on review
    - Add some more unit tests for transitions of states

--- packages/dashboard-core-plugins/src/useIrisGridModel.test.ts
+++ packages/dashboard-core-plugins/src/useIrisGridModel.test.ts
@@ -61,16 +61,76 @@ it('should return success status on fetch success', async () => {
 
 it('should reload the model on reload', async () => {
   const table = TestUtils.createMockProxy<dh.Table>();
-  const fetch = jest.fn(() => Promise.resolve(table));
+  let fetchResolve;
+  const fetch = jest.fn(
+    () =>
+      new Promise<dh.Table>(resolve => {
+        fetchResolve = resolve;
+      })
+  );
   const { result, waitForNextUpdate } = renderHook(() =>
     useIrisGridModel(fetch)
   );
+  expect(result.current.status).toBe('loading');
+  fetchResolve(table);
   await waitForNextUpdate();
-  const fetchResult = result.current;
-  expect(fetchResult.status).toBe('success');
+  expect(result.current.status).toBe('success');
+  // Check that it will reload, transitioning to loading then to success again
+
   fetch.mockClear();
+  fetch.mockReturnValue(
+    new Promise(resolve => {
+      fetchResolve = resolve;
+    })
+  );
   await act(async () => {
-    fetchResult.reload();
+    result.current.reload();
   });
   expect(fetch).toHaveBeenCalledTimes(1);
+  expect(result.current.status).toBe('loading');
+  fetchResolve(table);
+  await waitForNextUpdate();
+  expect(result.current.status).toBe('success');
+  expect(
+    (result.current as IrisGridModelFetchSuccessResult).model
+  ).toBeDefined();
+
+  // Now check that it will handle a failure on reload, transitioning from loading to failure
+  fetch.mockClear();
+
+  let fetchReject;
+  fetch.mockReturnValue(
+    new Promise((resolve, reject) => {
+      fetchReject = reject;
+    })
+  );
+  await act(async () => {
+    result.current.reload();
+  });
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(result.current.status).toBe('loading');
+  const error = new Error('Test error');
+  fetchReject(error);
+  await waitForNextUpdate();
+  expect(result.current.status).toBe('error');
+  expect((result.current as IrisGridModelFetchErrorResult).error).toBe(error);
+
+  // Check that it will reload again after an error
+  fetch.mockClear();
+  fetch.mockReturnValue(
+    new Promise(resolve => {
+      fetchResolve = resolve;
+    })
+  );
+  await act(async () => {
+    result.current.reload();
+  });
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(result.current.status).toBe('loading');
+  fetchResolve(table);
+  await waitForNextUpdate();
+  expect(result.current.status).toBe('success');
+  expect(
+    (result.current as IrisGridModelFetchSuccessResult).model
+  ).toBeDefined();
 });
