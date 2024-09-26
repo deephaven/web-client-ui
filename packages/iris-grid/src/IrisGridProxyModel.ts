ee1bc2f0d5d4bfe69ae667d51cc9d94bfed905d4
dgodinez-dh <77981300+dgodinez-dh@users.noreply.github.com>
2024-09-26 12:53:01 -0600

    fix: DH-17730: Fix Proxy Model Undefined Formatter (#2237)
    Fixes https://deephaven.atlassian.net/browse/DH-17730 in Enterprise.
    Will need to be cherry picked back to v0.85.
    web-client-ui issue is
    https://github.com/deephaven/web-client-ui/issues/2234

--- packages/iris-grid/src/IrisGridProxyModel.ts
+++ packages/iris-grid/src/IrisGridProxyModel.ts
@@ -436,6 +436,14 @@ class IrisGridProxyModel extends IrisGridModel implements PartitionedGridModel {
       : false;
   }
 
+  get formatter(): Formatter {
+    return this.originalModel.formatter;
+  }
+
+  set formatter(formatter: Formatter) {
+    this.originalModel.formatter = formatter;
+  }
+
   setViewport = (
     top: number,
     bottom: number,
