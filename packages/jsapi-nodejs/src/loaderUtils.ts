f37d25f329e780f66e85b27e0cd0e48e70dcfef3
Brian Ingles <brianingles@deephaven.io>
2024-12-16 10:37:02 -0600

    fix: Swapping esbuild for esbuild-wasm to fix OS specific dependency (#2317)
    DH-18191: Swapping esbuild for esbuild-wasm to fix OS specific
    dependency

--- packages/jsapi-nodejs/src/loaderUtils.ts
+++ packages/jsapi-nodejs/src/loaderUtils.ts
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import esbuild, { type BuildOptions } from 'esbuild';
+import esbuild, { type BuildOptions } from 'esbuild-wasm';
 
 import { downloadFromURL, urlToDirectoryName } from './serverUtils.js';
 import { polyfillWs } from './polyfillWs.js';
