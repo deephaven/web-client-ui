36b8156b7fb6159db8e9c67a983842650ff8eed6
mikebender <mikebender@deephaven.io>
2024-12-27 10:37:21 -0500

    fix: Console history did not stick to bottom on visibility changes
    - When debugging, I found that the `Code` block was rendering while the Console tab was in the background (invisible, size 0) so when it did finally render, there was a lot of content added but it wasn't scrolled. By checking sticky bottom when visibility changes, we handle this situtation where content causes a resize while it is invisible
    - Use an IntersectionObserver to detect visibility changes: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
    - Tested by running the following:
    1. Run a code snippet to print a bunch of lines, causing the console history to scroll:
    ```python
    for i in range(0, 200):
        print("i is", i)
    ```
    2. Run a snippet to create a dashboard, and have a lot of text in the command:
    ```python
    from deephaven import ui
    
    ... have lots of blank lines so that it takes up more than a page of scroll space
    
    d = ui.dashboard(ui.panel("Hello"))
    ```
    3. Go back to the Console tab, see that it is still scroll to the bottom.

--- packages/console/src/Console.tsx
+++ packages/console/src/Console.tsx
@@ -7,6 +7,7 @@ import React, {
   type ReactElement,
   type ReactNode,
   type RefObject,
+  type UIEvent,
 } from 'react';
 import {
   ContextActions,
@@ -190,6 +191,7 @@ export class Console extends PureComponent<ConsoleProps, ConsoleState> {
     this.handleLogMessage = this.handleLogMessage.bind(this);
     this.handleOverflowActions = this.handleOverflowActions.bind(this);
     this.handleScrollPaneScroll = this.handleScrollPaneScroll.bind(this);
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
     this.handleToggleAutoLaunchPanels =
       this.handleToggleAutoLaunchPanels.bind(this);
     this.handleToggleClosePanelsOnDisconnect =
@@ -213,6 +215,9 @@ export class Console extends PureComponent<ConsoleProps, ConsoleState> {
     this.consoleHistoryScrollPane = React.createRef();
     this.pending = new Pending();
     this.queuedLogMessages = [];
+    this.visibilityObserver = new IntersectionObserver(
+      this.handleVisibilityChange
+    );
 
     const { objectMap, settings } = this.props;
 
@@ -253,6 +258,10 @@ export class Console extends PureComponent<ConsoleProps, ConsoleState> {
     );
 
     this.updateDimensions();
+
+    if (this.consolePane.current) {
+      this.visibilityObserver.observe(this.consolePane.current);
+    }
   }
 
   componentDidUpdate(prevProps: ConsoleProps, prevState: ConsoleState): void {
@@ -280,6 +289,7 @@ export class Console extends PureComponent<ConsoleProps, ConsoleState> {
     this.processLogMessageQueue.cancel();
 
     this.deinitConsoleLogging();
+    this.visibilityObserver.disconnect();
   }
 
   cancelListener?: () => void;
@@ -293,6 +303,8 @@ export class Console extends PureComponent<ConsoleProps, ConsoleState> {
   pending: Pending;
 
   queuedLogMessages: ConsoleHistoryActionItem[];
+
+  visibilityObserver: IntersectionObserver;
 
   initConsoleLogging(): void {
     const { session } = this.props;
@@ -670,7 +682,8 @@ export class Console extends PureComponent<ConsoleProps, ConsoleState> {
     });
   }
 
-  handleScrollPaneScroll(): void {
+  handleScrollPaneScroll(event: UIEvent<HTMLDivElement>): void {
+    log.debug('handleScrollPaneScroll', event);
     const scrollPane = this.consoleHistoryScrollPane.current;
     assertNotNull(scrollPane);
     this.setState({
@@ -679,6 +692,18 @@ export class Console extends PureComponent<ConsoleProps, ConsoleState> {
         scrollPane.scrollHeight > scrollPane.clientHeight,
       isStuckToBottom: this.isAtBottom(),
     });
+  }
+
+  handleVisibilityChange(entries: IntersectionObserverEntry[]): void {
+    log.debug('handleVisibilityChange', entries);
+
+    const entry = entries[0];
+    if (entry.isIntersecting) {
+      const { isStuckToBottom } = this.state;
+      if (isStuckToBottom && !this.isAtBottom()) {
+        this.scrollConsoleHistoryToBottom();
+      }
+    }
   }
 
   handleToggleAutoLaunchPanels(): void {
