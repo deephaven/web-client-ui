d4eab8addec36b866991c158b5a045f788ccd6ef
dgodinez-dh <77981300+dgodinez-dh@users.noreply.github.com>
2024-12-30 14:30:16 -0700

    feat: Export Spectrum Menu and SubmenuTrigger (#2322)
    Need to export these to implement menu in deephave.ui
    https://deephaven.atlassian.net/browse/DH-18089

--- packages/components/src/spectrum/collections.ts
+++ packages/components/src/spectrum/collections.ts
@@ -5,8 +5,14 @@ export {
   // the Spectrum props type for upstream consumers that need to compose prop types.
   type SpectrumComboBoxProps,
   // ListBox - we aren't planning to support this component
+  Menu as SpectrumMenu,
+  type SpectrumMenuProps,
   MenuTrigger,
   type SpectrumMenuTriggerProps as MenuTriggerProps,
+  SubmenuTrigger,
+  type SpectrumSubmenuTriggerProps as SubmenuTriggerProps,
+  ContextualHelpTrigger,
+  type SpectrumMenuDialogTriggerProps as ContextualHelpTriggerProps,
   // TableView - we aren't planning to support this component
   // Picker is exported from Picker.tsx as a custom DH component. Re-exporting
   // the Spectrum props type for upstream consumers that need to compose prop types.
