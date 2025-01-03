259e0653b99352acd5b4642d630b560fe2c0bd4c
Mike Bender <mikebender@deephaven.io>
2025-01-03 09:24:59 -0500

    fix: Console history did not stick to bottom on visibility changes (#2326)
    - When debugging, I found that the `Code` block was rendering while the
    Console tab was in the background (invisible, size 0) so when it did
    finally render, there was a lot of content added but it wasn't scrolled.
    By checking sticky bottom when visibility changes, we handle this
    situtation where content causes a resize while it is invisible
    - Use an IntersectionObserver to detect visibility changes:
    https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
    - Tested by running the following:
    1. Run a code snippet to print a bunch of lines, causing the console
    history to scroll:
    ```python
    for i in range(0, 200):
        print("i is", i)
    ```
    2. Run a snippet to create a dashboard, and have a lot of text in the
    command:
    ```python
    from deephaven import ui
    
    ... have lots of blank lines so that it takes up more than a page of scroll space
    
    d = ui.dashboard(ui.panel("Hello"))
    ```
    3. Go back to the Console tab, see that it is still scroll to the
    bottom.

--- tests/utils.ts
+++ tests/utils.ts
@@ -126,6 +126,19 @@ export async function openPlot(
 }
 
 /**
+ * Generate a unique Id
+ * @param length Length to give id
+ * @returns A unique valid id
+ */
+export function generateId(length = 21): string {
+  let id = '';
+  for (let i = 0; i < length; i += 1) {
+    id += Math.random().toString(36).substr(2, 1);
+  }
+  return id;
+}
+
+/**
  * Generate a unique python variable name
  * @param prefix Prefix to give the variable name
  * @returns A unique string that is a valid python variable name
