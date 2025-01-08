275d16e172a2d5b2acfb000f37e0490b7ee92e7f
Eric Lin <es3lin@uwaterloo.ca>
2025-01-08 18:05:37 -0500

    add test for testing shortcut in embed-widgets

--- tests/shortcuts.spec.ts
+++ tests/shortcuts.spec.ts
@@ -12,7 +12,18 @@ test('shortcut downloads logs', async ({ page }) => {
 });
 
 test('shortcut downloads logs in full screen error', async ({ page }) => {
-  await gotoPage(page, 'localhost:4010/iframe/widget');
+  // Go to embed-widget page without url parameter to trigger a full screen error
+  await gotoPage(page, 'http://localhost:4010/');
+
+  const downloadPromise = page.waitForEvent('download');
+  await page.keyboard.press('ControlOrMeta+Alt+Shift+KeyL');
+  const download = await downloadPromise;
+
+  expect(download).not.toBeNull();
+});
+
+test('shortcut downloads logs in embeded-widget', async ({ page }) => {
+  await gotoPage(page, 'http://localhost:4010?name=all_types');
 
   const downloadPromise = page.waitForEvent('download');
   await page.keyboard.press('ControlOrMeta+Alt+Shift+KeyL');
