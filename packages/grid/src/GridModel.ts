3f74d4403f332fa65877bc33e418faea3fc974c6
Matthew Runyon <matthewrunyon@deephaven.io>
2025-02-27 10:11:14 -0600

    fix: DH-18798 - token cache growing unbounded (#2374)
    This adds a cache for the token cache. I tested this was actually
    causing the issue by adding some code to the cell renderer to prefill
    the cache to 1 million items. It hung around 200k without the max. With
    max it prefills just fine (cleaning out every 10k) and doesn't hinder
    performance.

--- packages/grid/src/GridModel.ts
+++ packages/grid/src/GridModel.ts
@@ -229,7 +229,8 @@ abstract class GridModel<
       const contentToCheckForLinks = text.substring(0, lengthOfContent);
 
       return GridUtils.findTokensWithProtocolInText(contentToCheckForLinks);
-    }
+    },
+    { max: 10000 }
   );
 
   renderTypeForCell(column: ModelIndex, row: ModelIndex): CellRenderType {
