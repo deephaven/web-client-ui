5ce8bf8927a35fc0219040eeb2f1e8b6345a6cb5
Matthew Runyon <matthewrunyon@deephaven.io>
2025-05-06 16:13:49 -0500

    feat: Add keepMounted param to TabPanels for persisting panels when they are not active (#2434)
    Part of DH-18349. Will need a small update in dh.ui to enable support
    there which is what the ticket wants.
    
    Does not change default behavior of `TabPanels`, just adds an extra
    param to keep static panels mounted (in React, but not actually mounted
    in the DOM).
    
    There are unit tests, but can also test with local plugins from this PR
    https://github.com/deephaven/deephaven-plugins/pull/1177

---
+++ packages/components/src/spectrum/TabPanels.tsx
@@ -0,0 +1,111 @@
+import React, { type Key, useMemo, useRef } from 'react';
+import {
+  createHtmlPortalNode,
+  type HtmlPortalNode,
+  InPortal,
+  OutPortal,
+} from 'react-reverse-portal';
+import {
+  Item,
+  TabPanels,
+  type SpectrumTabPanelsProps,
+} from '@adobe/react-spectrum';
+import { type CollectionChildren } from '@react-types/shared';
+
+export interface DHCTabPanelsProps<T> extends SpectrumTabPanelsProps<T> {
+  /**
+   * If static panels with keys should stay mounted when not visible.
+   * This will not apply to dynamic panels created with a render function.
+   * Defaults to false.
+   */
+  keepMounted?: boolean;
+}
+
+/**
+ * Wrapper for react-spectrum TabPanels that adds support for keeping panels mounted
+ * when not visible using the `keepMounted` prop.
+ * Panels created with a render function will not be kept mounted.
+ */
+export function DHCTabPanels<T extends object>(
+  props: DHCTabPanelsProps<T>
+): JSX.Element {
+  const { children, keepMounted: keepMountedProp = false, ...rest } = props;
+  const keepMounted = keepMountedProp && typeof children !== 'function';
+
+  const portalNodeMap = useRef(new Map<Key, HtmlPortalNode>());
+
+  const portalNodes = useMemo(() => {
+    const nodes: JSX.Element[] = [];
+    const nextNodeMap = new Map<Key, HtmlPortalNode>(); // Keep track of the portals we use so we can clean up stale portals
+    if (!keepMounted) {
+      portalNodeMap.current = nextNodeMap;
+      return nodes;
+    }
+    React.Children.forEach(children, child => {
+      // Spectrum would ignore these anyway because it uses Item key to determine if the panel mounts
+      if (child == null || child.key == null) {
+        return;
+      }
+
+      let portal = portalNodeMap.current.get(child.key);
+      if (portal == null) {
+        portal = createHtmlPortalNode({
+          attributes: {
+            // Should make the placeholder div not affect layout and act as if children are mounted directly to the parent
+            style: 'display: contents',
+          },
+        });
+      }
+      nextNodeMap.set(child.key, portal);
+      nodes.push(
+        <InPortal node={portal} key={child.key}>
+          {child.props.children}
+        </InPortal>
+      );
+    });
+
+    portalNodeMap.current = nextNodeMap;
+
+    return nodes;
+  }, [children, keepMounted]);
+
+  const mappedChildren: CollectionChildren<T> = useMemo(() => {
+    const newChildren: CollectionChildren<T> = [];
+    if (!keepMounted) {
+      return newChildren;
+    }
+    // Need to use forEach instead of map because map always changes the key of the returned elements
+    React.Children.forEach(children, child => {
+      if (child == null || child.key == null) {
+        newChildren.push(child);
+        return;
+      }
+
+      const portal = portalNodeMap.current.get(child.key);
+      if (portal == null) {
+        newChildren.push(child);
+        return;
+      }
+
+      newChildren.push(
+        <Item key={child.key}>
+          <OutPortal node={portal} />
+        </Item>
+      );
+    });
+
+    return newChildren;
+  }, [children, keepMounted]);
+
+  return (
+    <>
+      {keepMounted && portalNodes}
+      <TabPanels
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...rest}
+      >
+        {keepMounted ? mappedChildren : children}
+      </TabPanels>
+    </>
+  );
+}
