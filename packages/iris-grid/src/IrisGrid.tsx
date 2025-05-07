b222ae762062f5e74a43cf5c0b3112ec0c3a3308
Vlad Babich <vladbabich@deephaven.io>
2025-05-07 17:39:16 -0600

    Cleanup diff

--- packages/iris-grid/src/IrisGrid.tsx
+++ packages/iris-grid/src/IrisGrid.tsx
@@ -1540,17 +1540,6 @@ class IrisGrid extends Component<IrisGridProps, IrisGridState> {
       return null;
     }
 
-    const { model } = this.props;
-
-    if (
-      columnIndex != null &&
-      modelColumns.get(columnIndex) != null &&
-      model.columns[columnIndex] == null
-    ) {
-      log.debug('getModelColumn', columnIndex, model.columns);
-      // debugger;
-    }
-
     return columnIndex != null ? modelColumns.get(columnIndex) : null;
   }
 
@@ -3212,7 +3201,7 @@ class IrisGrid extends Component<IrisGridProps, IrisGridState> {
 
   handleTableChanged(): void {
     const { model } = this.props;
-    // movedColumns update triggers metricCalculator update in the Grid component
+    // movedColumns reset triggers metricCalculator update in the Grid component
     this.setState({ movedColumns: model.initialMovedColumns });
   }
 
