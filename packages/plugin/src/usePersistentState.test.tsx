876b4f5961749d48e8f0936d3f5a97a930d6a725
Matthew Runyon <matthewrunyon@deephaven.io>
2025-05-15 11:49:07 -0500

    Fix test

--- packages/plugin/src/usePersistentState.test.tsx
+++ packages/plugin/src/usePersistentState.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { TestUtils } from '@deephaven/test-utils';
+import { TestUtils } from '@deephaven/utils';
 import { PersistentStateProvider } from './PersistentStateContext';
 import usePersistentState, {
   type PersistentStateMigration,
