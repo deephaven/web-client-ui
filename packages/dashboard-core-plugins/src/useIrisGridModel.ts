e26c462e52f8495675989fd4dbed193718953747
Matthew Runyon <matthewrunyon@deephaven.io>
2025-05-15 11:27:23 -0500

    fix: DH-17292 Handle disconnect from GridWidgetPlugin (#2086) (#2444)
    1st of 3 cherry-picks for dh.ui persistent state in grizzly
    
    - When the model is disconnected, we should just display an error.
    There's no option to reconnect, as the widget schema could change
    entirely
    - By unloading the IrisGrid component, it's no longer throwing an error
    by trying to access table methods after a table.close()
    - Fixes DH-17292 from Enterprise
      - Tested using the steps in the description
    
    Co-authored-by: Mike Bender <mikebender@deephaven.io>

---
+++ packages/dashboard-core-plugins/src/useIrisGridModel.ts
@@ -0,0 +1,119 @@
+import { type dh } from '@deephaven/jsapi-types';
+import { useApi } from '@deephaven/jsapi-bootstrap';
+import { IrisGridModel, IrisGridModelFactory } from '@deephaven/iris-grid';
+import { useCallback, useEffect, useState } from 'react';
+
+export type IrisGridModelFetch = () => Promise<dh.Table>;
+
+export type IrisGridModelFetchErrorResult = {
+  error: NonNullable<unknown>;
+  status: 'error';
+};
+
+export type IrisGridModelFetchLoadingResult = {
+  status: 'loading';
+};
+
+export type IrisGridModelFetchSuccessResult = {
+  status: 'success';
+  model: IrisGridModel;
+};
+
+export type IrisGridModelFetchResult = (
+  | IrisGridModelFetchErrorResult
+  | IrisGridModelFetchLoadingResult
+  | IrisGridModelFetchSuccessResult
+) & {
+  reload: () => void;
+};
+
+/** Pass in a table `fetch` function, will load the model and handle any errors */
+export function useIrisGridModel(
+  fetch: IrisGridModelFetch
+): IrisGridModelFetchResult {
+  const dh = useApi();
+  const [model, setModel] = useState<IrisGridModel>();
+  const [error, setError] = useState<unknown>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const makeModel = useCallback(async () => {
+    const table = await fetch();
+    return IrisGridModelFactory.makeModel(dh, table);
+  }, [dh, fetch]);
+
+  const reload = useCallback(async () => {
+    setIsLoading(true);
+    setError(undefined);
+    try {
+      const newModel = await makeModel();
+      setModel(newModel);
+      setIsLoading(false);
+    } catch (e) {
+      setError(e);
+      setIsLoading(false);
+    }
+  }, [makeModel]);
+
+  useEffect(() => {
+    let cancelled = false;
+    async function init() {
+      setIsLoading(true);
+      setError(undefined);
+      try {
+        const newModel = await makeModel();
+        if (!cancelled) {
+          setModel(newModel);
+          setIsLoading(false);
+        }
+      } catch (e) {
+        if (!cancelled) {
+          setError(e);
+          setIsLoading(false);
+        }
+      }
+    }
+
+    init();
+
+    return () => {
+      cancelled = true;
+    };
+  }, [makeModel]);
+
+  useEffect(
+    function startListeningModel() {
+      if (!model) {
+        return;
+      }
+
+      // If the table inside a widget is disconnected, then don't bother trying to listen to reconnect, just close it and show a message
+      // Widget closes the table already when it is disconnected, so no need to close it again
+      function handleDisconnect() {
+        setError(new Error('Table disconnected'));
+        setModel(undefined);
+        setIsLoading(false);
+      }
+
+      model.addEventListener(IrisGridModel.EVENT.DISCONNECT, handleDisconnect);
+
+      return () => {
+        model.removeEventListener(
+          IrisGridModel.EVENT.DISCONNECT,
+          handleDisconnect
+        );
+      };
+    },
+    [model]
+  );
+
+  if (isLoading) {
+    return { reload, status: 'loading' };
+  }
+  if (error != null) {
+    return { error, reload, status: 'error' };
+  }
+  if (model != null) {
+    return { model, reload, status: 'success' };
+  }
+  throw new Error('Invalid state');
+}
