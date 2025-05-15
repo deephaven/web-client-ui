711dea987eb3169c42569cc70d22a5656b5eb165
Matthew Runyon <matthewrunyon@deephaven.io>
2025-05-15 14:44:20 -0500

    feat: Add usePersistentState implementation to GridWidgetPlugin (#2427) (#2446)
    Final cherry pick for usePersistentState
    
    Part of [DH-19000](https://deephaven.atlassian.net/browse/DH-19000).
    This adds the `GridWidgetPlugin` implementation of `usePersistentState`.
    It will effectively do nothing right now since the state will always be
    `undefined` to start. Once the dh.ui side of this merges, then normal
    tables in a `dh.ui` panel will persist their state
    
    [DH-19000]:
    https://deephaven.atlassian.net/browse/DH-19000?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ packages/dashboard-core-plugins/src/GridWidgetPlugin.test.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, waitFor } from '@testing-library/react';
+import { ApiContext } from '@deephaven/jsapi-bootstrap';
+import { TestUtils } from '@deephaven/utils';
+import type { dh as DhType } from '@deephaven/jsapi-types';
+import { createMockStore } from '@deephaven/redux';
+import dh from '@deephaven/jsapi-shim';
+import GridWidgetPlugin from './GridWidgetPlugin';
+
+const MockIrisGrid: React.FC & jest.Mock = jest.fn(() => (
+  <div>MockIrisGrid</div>
+));
+
+jest.mock('@deephaven/iris-grid', () => {
+  const { forwardRef } = jest.requireActual('react');
+  return {
+    ...(jest.requireActual('@deephaven/iris-grid') as Record<string, unknown>),
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    IrisGrid: forwardRef((props, ref) => <MockIrisGrid {...props} />),
+  };
+});
+
+it('mounts without crashing', async () => {
+  const table = TestUtils.createMockProxy<DhType.Table>();
+  const fetch = jest.fn(() => Promise.resolve(table));
+
+  const store = createMockStore();
+
+  const { container, queryByText } = render(
+    <Provider store={store}>
+      <ApiContext.Provider value={dh}>
+        <GridWidgetPlugin fetch={fetch} />
+      </ApiContext.Provider>
+    </Provider>
+  );
+
+  expect(queryByText('MockIrisGrid')).not.toBeInTheDocument();
+
+  await waitFor(() =>
+    expect(
+      container.querySelector('[role=progressbar].loading-spinner-large')
+    ).toBeInTheDocument()
+  );
+
+  await waitFor(() =>
+    expect(
+      container.querySelector('[role=progressbar].loading-spinner-large')
+    ).not.toBeInTheDocument()
+  );
+
+  expect(queryByText('MockIrisGrid')).toBeInTheDocument();
+});
