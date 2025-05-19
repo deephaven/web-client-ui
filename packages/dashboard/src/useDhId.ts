77639d8f60605eee9074e725ef6cfccd99fc91d9
Matthew Runyon <matthewrunyon@deephaven.io>
2025-05-19 14:33:19 -0500

    Add useDhId test

--- packages/dashboard/src/useDhId.ts
+++ packages/dashboard/src/useDhId.ts
@@ -10,24 +10,32 @@ export const DhIdContext = createContext<string | null>(null);
  * This is used to identify the component within Deephaven.
  * Usually this is just a panel ID, but in some contexts such as dh.ui,
  * it may be an ID for a component within a panel.
+ *
+ * Looks for a __dhId prop on the component, and if not found, looks in the DhIdContext.
  * @param props The props of the component using this hook
  * @returns The Deephaven ID of the component
  */
 export function useDhId(): string {
+  // pendingProps are the props passed to the fiber node
   const props =
-    (useFiber()?.memoizedProps as Record<string, unknown> | undefined) ?? {};
+    (useFiber()?.pendingProps as Record<string, unknown> | undefined) ?? {};
   const dhIdProp = props[DH_ID_PROP];
   const dhId = useContext(DhIdContext);
 
-  if (dhIdProp != null && typeof dhIdProp !== 'string') {
-    throw new Error(
-      `useDhId expected prop ${DH_ID_PROP} to be a string, but got ${typeof dhIdProp}`
-    );
-  } else if (dhId == null) {
+  if (dhIdProp != null) {
+    if (typeof dhIdProp !== 'string') {
+      throw new Error(
+        `useDhId expected prop ${DH_ID_PROP} to be a string, but got ${typeof dhIdProp}`
+      );
+    }
+    return dhIdProp;
+  }
+
+  if (dhId == null) {
     throw new Error(
       `useDhId must be used within a DhIdContext provider if there is no ${DH_ID_PROP} prop`
     );
   }
 
-  return dhIdProp ?? dhId;
+  return dhId;
 }
