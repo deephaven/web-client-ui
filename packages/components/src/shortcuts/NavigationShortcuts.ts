5b572c30ac9098cdca20864bfac59fd626f2d4e0
Germain Zhang-Houle <86877307+gzh2003@users.noreply.github.com>
2025-06-24 12:44:30 -0700

    feat: DH-14431: Add shortcuts to cycle dashboards (#2470)
    Part of DH-14431. Introduces keyboard navigation shortcuts to cycle
    between dashboards.
    
    `Cmd/Ctrl + .`: to cycle the active dashboard forward
    `Cmd/Ctrl + ,`: to cycle the active dashboard backward
    
    Changes:
    - Add shortcuts to cycle dashboard to `NavigationShortcuts.ts`
    - Registered the shortcuts as global context actions
    - Add `handleCycleDashboardForward` and `handleCycleDashboardBackward`
    to manage cycling

--- packages/components/src/shortcuts/NavigationShortcuts.ts
+++ packages/components/src/shortcuts/NavigationShortcuts.ts
@@ -30,6 +30,20 @@ const NAVIGATION_SHORTCUTS = {
     macShortcut: [MODIFIER.CMD, MODIFIER.SHIFT, KEY.SEMICOLON],
     isEditable: true,
   }),
+  CYCLE_TO_NEXT_DASHBOARD: ShortcutRegistry.createAndAdd({
+    id: 'NAVIGATION.CYCLE_TO_NEXT_DASHBOARD',
+    name: 'Cycle To Next Dashboard',
+    shortcut: [MODIFIER.CTRL, KEY.PERIOD],
+    macShortcut: [MODIFIER.CMD, KEY.PERIOD],
+    isEditable: true,
+  }),
+  CYCLE_TO_PREVIOUS_DASHBOARD: ShortcutRegistry.createAndAdd({
+    id: 'NAVIGATION.CYCLE_TO_PREVIOUS_DASHBOARD',
+    name: 'Cycle To Previous Dashboard',
+    shortcut: [MODIFIER.CTRL, KEY.COMMA],
+    macShortcut: [MODIFIER.CMD, KEY.COMMA],
+    isEditable: true,
+  }),
 };
 
 export default NAVIGATION_SHORTCUTS;
