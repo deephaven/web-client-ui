998f7246630db42d0430309775b83a579d684a69
Joe Numainville <josephnumainville@deephaven.io>
2025-07-07 12:46:52 -0500

    wip

---
+++ packages/plugin/src/UseElementPluginMapping.test.ts
@@ -0,0 +1,28 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { TestUtils } from '@deephaven/test-utils';
+import type { PluginModuleMap } from './PluginTypes';
+import { useElementPluginMapping } from './useElementPluginMapping';
+import { getElementPluginMapping } from './PluginUtils';
+import { usePlugins } from './usePlugins';
+
+jest.mock('@deephaven/components');
+jest.mock('./PluginUtils');
+
+const { asMock } = TestUtils;
+
+const mockElementPluginMapping = new Map<
+  string,
+  React.ComponentType<unknown>
+>();
+
+const mockPlugins: PluginModuleMap = new Map();
+
+it('should return element plugin mapping from plugins context', () => {
+  asMock(getElementPluginMapping).mockReturnValue(mockElementPluginMapping);
+  asMock(usePlugins).mockReturnValue(mockPlugins);
+
+  const { result } = renderHook(() => useElementPluginMapping());
+
+  expect(getElementPluginMapping).toHaveBeenCalledWith(mockPlugins);
+  expect(result).toEqual(mockElementPluginMapping);
+});
