5544c07cf29c4cb960031b4be16df58fba9b00c4
Matthew Runyon <matthewrunyon@deephaven.io>
2025-07-17 20:12:15 -0500

    Add URI to useDeferredApi

--- packages/jsapi-bootstrap/src/useDeferredApi.ts
+++ packages/jsapi-bootstrap/src/useDeferredApi.ts
@@ -1,16 +1,17 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import type { dh as DhType } from '@deephaven/jsapi-types';
 import { ApiContext } from './ApiBootstrap';
+import { type UriVariableDescriptor } from './useObjectFetcher';
 
 /**
  * Function to fetch an API based on a provided descriptor object.
  * Depending on the context there may be more properties on the descriptor,
  * providing more information about the object, such as a session ID.
- * @param descriptor Descriptor object to fetch the API from.
+ * @param descriptor Descriptor object or URI to fetch the API from.
  * @returns A promise that resolves to the API instance for the provided variable descriptor.
  */
 export type DeferredApiFetcher = (
-  descriptor: DhType.ide.VariableDescriptor
+  descriptor: DhType.ide.VariableDescriptor | UriVariableDescriptor
 ) => Promise<typeof DhType>;
 
 export const DeferredApiContext = createContext<
@@ -20,11 +21,11 @@ export const DeferredApiContext = createContext<
 /**
  * Retrieve the API for the current context, given the widget provided.
  * The API may need to be loaded, and will return `null` until it is ready.
- * @param widget The widget descriptor to use to fetch the API
+ * @param widget The widget descriptor or URI to use to fetch the API
  * @returns A tuple with the API instance, and an error if one occurred.
  */
 export function useDeferredApi(
-  widget: DhType.ide.VariableDescriptor | null
+  widget: DhType.ide.VariableDescriptor | UriVariableDescriptor | null
 ): [dh: typeof DhType | null, error: unknown | null] {
   const [api, setApi] = useState<typeof DhType | null>(null);
   const [error, setError] = useState<unknown | null>(null);
