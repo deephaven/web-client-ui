737f413ebbcd9f43774d976a024f49808c71f70d
Joe Numainville <josephnumainville@deephaven.io>
2025-08-28 14:39:55 -0500

    new test fixes

--- packages/console/src/console-history/ConsoleHistoryItemActions.test.tsx
+++ packages/console/src/console-history/ConsoleHistoryItemActions.test.tsx
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import { TestUtils } from '@deephaven/test-utils';
 import userEvent from '@testing-library/user-event';
 import ConsoleHistoryItemActions from './ConsoleHistoryItemActions';
-import { ThemeProvider } from '@deephaven/components';
 
 describe('clicking calls functionality', () => {
   const user = userEvent.setup();
@@ -11,19 +9,16 @@ describe('clicking calls functionality', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     return render(
-      <ThemeProvider themes={[]}>
-        <ConsoleHistoryItemActions
-          item={{ command: 'Test command' }}
-          handleCommandSubmit={handleCommandSubmit}
-          handleTooltipVisible={jest.fn()}
-        />
-      </ThemeProvider>
+      <ConsoleHistoryItemActions
+        item={{ command: 'Test command' }}
+        handleCommandSubmit={handleCommandSubmit}
+        handleTooltipVisible={jest.fn()}
+      />
     );
   });
 
   it('should render and rerun on click', async () => {
     const button = screen.getAllByRole('button');
-    console.log('thebuttons', button, button.length);
     await user.click(button[1]);
     expect(handleCommandSubmit).toHaveBeenCalledTimes(1);
   });
