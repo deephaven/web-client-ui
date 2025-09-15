acb1323922d4ed7617eba6d9041cbf52fc9a7c67
Matthew Runyon <matthewrunyon@deephaven.io>
2025-09-15 17:57:44 -0500

    Fix wrapper components

--- packages/components/src/transitions/FadeTransition.tsx
+++ packages/components/src/transitions/FadeTransition.tsx
@@ -1,7 +1,10 @@
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import type { CSSTransitionProps } from 'react-transition-group/CSSTransition';
-import type { EndHandler } from 'react-transition-group/Transition';
+import type {
+  EndHandler,
+  EnterHandler,
+} from 'react-transition-group/Transition';
 import classNames from 'classnames';
 import type { RemoveIndexSignature } from '@deephaven/utils';
 import ThemeExport from '../ThemeExport';
@@ -11,10 +14,11 @@ type FadeTransitionProps =
   // However, CSSTransitionProps get confused if you don't provide a timeout, it requires an endHandler
   // We're just making the endHandler optional here, as the timeout has a default
   Omit<
-    RemoveIndexSignature<CSSTransitionProps<HTMLDivElement>>,
-    'addEndHandler' | 'children'
+    RemoveIndexSignature<CSSTransitionProps<HTMLElement>>,
+    'addEndHandler' | 'children' | 'onEnter'
   > & {
-    addEndHandler?: EndHandler<HTMLDivElement> | undefined;
+    addEndHandler?: EndHandler<HTMLElement> | undefined;
+    onEnter?: EnterHandler<undefined>;
     children?: React.ReactNode;
   };
 
@@ -25,19 +29,37 @@ function FadeTransition({
   classNames: classNamesProp,
   timeout = ThemeExport.transitionMs,
   children,
+  onEnter,
   ...props
 }: FadeTransitionProps): JSX.Element {
-  const nodeRef = useRef<HTMLDivElement>(null);
+  const nodeRef = useRef<HTMLElement | null>(null);
+  const handleOnEnter = useCallback(
+    (isAppearing: boolean) => {
+      const elem = nodeRef.current;
+      if (!elem) {
+        return;
+      }
+      onEnter?.(elem, isAppearing);
+    },
+    [onEnter]
+  );
+
+  // Mimics findDOMNode for CSSTransition
+  // The ref should be set before CSSTransition does anything with it
+  const setRef = useCallback((node: HTMLElement | null) => {
+    nodeRef.current = node?.firstElementChild as HTMLElement;
+  }, []);
 
   return (
     <CSSTransition
       nodeRef={nodeRef}
       timeout={timeout}
       classNames={classNames('fade', classNamesProp)}
+      onEnter={handleOnEnter}
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
     >
-      <div ref={nodeRef} style={{ display: 'contents' }}>
+      <div ref={setRef} style={{ display: 'contents' }}>
         {children}
       </div>
     </CSSTransition>
--- packages/components/src/transitions/SlideTransition.tsx
+++ packages/components/src/transitions/SlideTransition.tsx
@@ -1,5 +1,5 @@
 // SlideTransition class uses CSSTransition with slide-left and slide-right classNames, depending on the prop direction. The transition is 250ms long.
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import type { CSSTransitionProps } from 'react-transition-group/CSSTransition';
 import type { EndHandler } from 'react-transition-group/Transition';
@@ -14,10 +14,10 @@ type SlideTransitionProps =
   // However, CSSTransitionProps get confused if you don't provide a timeout, it requires an endHandler
   // We're just making the endHandler optional here, as the timeout has a default
   Omit<
-    RemoveIndexSignature<CSSTransitionProps<HTMLDivElement>>,
+    RemoveIndexSignature<CSSTransitionProps<HTMLElement>>,
     'addEndHandler'
   > & {
-    addEndHandler?: EndHandler<HTMLDivElement> | undefined;
+    addEndHandler?: EndHandler<HTMLElement> | undefined;
     children?: React.ReactNode;
     /**
      * The direction of the slide transition. Defaults to left.
@@ -41,7 +41,13 @@ function SlideTransition({
   timeout = ThemeExport.transitionMidMs,
   ...props
 }: SlideTransitionProps): JSX.Element {
-  const nodeRef = useRef<HTMLDivElement>(null);
+  const nodeRef = useRef<HTMLElement | null>(null);
+
+  // Mimics findDOMNode for CSSTransition
+  // The ref should be set before CSSTransition does anything with it
+  const setRef = useCallback((node: HTMLElement | null) => {
+    nodeRef.current = node?.firstElementChild as HTMLElement;
+  }, []);
 
   return (
     <CSSTransition
@@ -51,7 +57,7 @@ function SlideTransition({
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
     >
-      <div ref={nodeRef} style={{ display: 'contents' }}>
+      <div ref={setRef} style={{ display: 'contents' }}>
         {children}
       </div>
     </CSSTransition>
