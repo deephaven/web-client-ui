eeab61fbef915adf75ec29774c798a717bdd8c73
Matthew Runyon <matthewrunyon@deephaven.io>
2025-10-09 14:14:08 -0500

    Another one

--- packages/iris-grid/src/IrisGridTableModelTemplate.ts
+++ packages/iris-grid/src/IrisGridTableModelTemplate.ts
@@ -18,7 +18,6 @@ import type { dh as DhType } from '@deephaven/jsapi-types';
 import Log from '@deephaven/log';
 import {
   type CancelablePromise,
-  EMPTY_ARRAY,
   EventShimCustomEvent,
   PromiseUtils,
   assertNotNull,
