026cb7d0d60171f4634d3184bb002308f04fac0f
Eric Lin <ericlin@deephaven.io>
2025-10-29 15:45:11 -0400

    simplify cleanup call

--- packages/jsapi-components/src/useSetPaddedViewportCallback.ts
+++ packages/jsapi-components/src/useSetPaddedViewportCallback.ts
@@ -46,12 +46,7 @@ export function useSetPaddedViewportCallback(
     cleanupSubscription();
   }
 
-  useEffect(
-    () => () => {
-      cleanupSubscription();
-    },
-    []
-  );
+  useEffect(() => cleanupSubscription, []);
 
   return useCallback(
     function setPaddedViewport(firstRow: number) {
