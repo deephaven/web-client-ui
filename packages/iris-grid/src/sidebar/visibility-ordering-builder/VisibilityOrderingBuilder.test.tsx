54890cef338ebb5df339f617e594192229c849d7
Matthew Runyon <matthewrunyon@deephaven.io>
2025-11-11 12:01:37 -0600

    Fix tests

--- packages/iris-grid/src/sidebar/visibility-ordering-builder/VisibilityOrderingBuilder.test.tsx
+++ packages/iris-grid/src/sidebar/visibility-ordering-builder/VisibilityOrderingBuilder.test.tsx
@@ -55,6 +55,7 @@ const NESTED_COLUMN_HEADER_GROUPS = [
 ] satisfies (Omit<DhType.ColumnGroup, 'color'> & { color?: string | null })[];
 
 window.HTMLElement.prototype.scroll = jest.fn();
+window.HTMLElement.prototype.scrollTo = jest.fn();
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
@@ -1765,15 +1766,16 @@ describe('Search', () => {
       ).toBe(2 * COLUMNS.length)
     );
 
-    // Globally mocked scrollIntoView for this test file
-    jest.mocked(window.HTMLElement.prototype.scrollIntoView).mockClear();
+    // Virtualizer calls scrollTo on the container element
+    const mockScrollTo = jest.fn();
+    HTMLElement.prototype.scrollTo = mockScrollTo;
 
     // Click the search item
     await user.click(screen.getAllByText(COLUMNS.at(-1)!.name)[1]);
 
     await waitFor(() => {
       const item = screen.getByText(COLUMNS.at(-1)!.name);
-      expect(item.scrollIntoView).toHaveBeenCalledTimes(1);
+      expect(mockScrollTo).toHaveBeenCalledTimes(1);
       expect(item.closest('.tree-item')).toHaveFocus();
     });
   });
