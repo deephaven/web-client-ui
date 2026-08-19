74b5fc0a3f2d46564c58cd3c58fb39d3712d0d4c
davidgodinez <davidgodinez@deephaven.io>
2026-08-19 07:49:10 -0600

    unit test

--- packages/iris-grid/src/mousehandlers/IrisGridContextMenuHandler.test.tsx
+++ packages/iris-grid/src/mousehandlers/IrisGridContextMenuHandler.test.tsx
@@ -1,12 +1,15 @@
+import type React from 'react';
 import { TestUtils } from '@deephaven/test-utils';
 import {
   type ExpandableColumnGridModel,
+  type Grid,
   type GridMetrics,
   type GridPoint,
+  GridSelectionMouseHandler,
   type ModelIndex,
 } from '@deephaven/grid';
 import { type dh } from '@deephaven/jsapi-types';
-import { ContextActionUtils } from '@deephaven/components';
+import { ContextActions, ContextActionUtils } from '@deephaven/components';
 import IrisGridContextMenuHandler from './IrisGridContextMenuHandler';
 import {
   type default as IrisGrid,
@@ -73,6 +76,63 @@ function makeMockIrisGrid({
     getTheme: jest.fn().mockReturnValue(theme),
   });
 }
+
+describe('onContextMenu modelRow prop', () => {
+  const mockDh = createMockProxy<typeof dh>();
+
+  beforeEach(() => {
+    jest
+      .spyOn(GridSelectionMouseHandler, 'getLatestSelection')
+      .mockReturnValue([]);
+    jest.spyOn(ContextActions, 'triggerMenu').mockImplementation(() => null);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it.each([
+    ['defined', 5 as ModelIndex],
+    ['undefined', undefined],
+    ['null', null],
+  ])('is called when modelRow is %s', (_label, modelRowValue) => {
+    const columns = makeColumns();
+    const model = makeMockModel({ columns });
+    (model.sourceForCell as jest.Mock).mockReturnValue({ column: 0, row: 0 });
+
+    const irisGrid = makeMockIrisGrid({ model });
+    const mockOnContextMenu = irisGrid.props.onContextMenu as jest.Mock;
+    mockOnContextMenu.mockReturnValue([]);
+    (irisGrid.getModelColumn as jest.Mock).mockReturnValue(0);
+    (irisGrid.getModelRow as jest.Mock).mockReturnValue(modelRowValue);
+
+    const handler = new IrisGridContextMenuHandler(irisGrid, mockDh);
+
+    const gridPoint: GridPoint = {
+      column: 0 as ModelIndex,
+      row: null,
+      x: 0,
+      y: 1000,
+      columnHeaderDepth: 0,
+    };
+
+    handler.onContextMenu(gridPoint, createMockProxy<Grid>(), {
+      clientX: 0,
+      clientY: 0,
+    } as React.MouseEvent<Element, MouseEvent>);
+
+    expect(mockOnContextMenu).toHaveBeenCalledWith(
+      expect.objectContaining({
+        model,
+        column: columns[0],
+        rowIndex: null,
+        columnIndex: 0,
+        modelRow: modelRowValue,
+        modelColumn: 0,
+      })
+    );
+  });
+});
 
 describe('getHeaderActions', () => {
   const mockDh = createMockProxy<typeof dh>();
