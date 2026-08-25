c6ee8e415a5b02344f2156c051efd7154900354a
davidgodinez <davidgodinez@deephaven.io>
2026-08-25 10:17:06 -0600

    fix selection code

--- packages/code-studio/src/styleguide/TableViews.tsx
+++ packages/code-studio/src/styleguide/TableViews.tsx
@@ -80,9 +80,9 @@ export function TableViews(): JSX.Element {
   const [lastOpenedName, setLastOpenedName] = useState('');
   const [singleSelectedKeys, setSingleSelectedKeys] = useState<
     'all' | Set<Key>
-  >(() => new Set([]));
+  >(() => new Set(['0']));
   const [multiSelectedKeys, setMultiSelectedKeys] = useState<'all' | Set<Key>>(
-    () => new Set([0, 3])
+    () => new Set(['0', '3'])
   );
 
   const onDensityChange = useCallback((value: string) => {
@@ -196,7 +196,7 @@ export function TableViews(): JSX.Element {
             items={rows.slice(0, 8)}
             itemCount={8}
             selectionMode="single"
-            defaultSelectedKeys={[0]}
+            defaultSelectedKeys={['0']}
             onSelectionChange={keys => setSingleSelectedKeys(keys)}
             renderCell={renderCell}
             getTextValue={item => item.name}
@@ -218,7 +218,7 @@ export function TableViews(): JSX.Element {
             itemCount={8}
             selectionMode="multiple"
             selectedKeys={multiSelectedKeys}
-            disabledKeys={[2, 5]}
+            disabledKeys={['2', '5']}
             onSelectionChange={keys => setMultiSelectedKeys(keys)}
             renderCell={renderCell}
             getTextValue={item => item.name}
