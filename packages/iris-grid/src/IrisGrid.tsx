60171670bd5e5e1a4c7d67e4e5cb785929e06743
dgodinez-dh <davidgodinez@deephaven.io>
2026-09-22 07:57:13 -0600

    fix: DH-23761: Make keyed selection type changes backwards compatible (#2763)
    deephaven-plugins sees type error for npm run types with latest
    web-client-ui. This fixes two keyed selection type changes to be
    optional for backwards compatibility.

--- packages/iris-grid/src/IrisGrid.tsx
+++ packages/iris-grid/src/IrisGrid.tsx
@@ -251,7 +251,7 @@ export interface IrisGridContextMenuData {
   columnIndex: GridRangeIndex;
   modelRow?: GridRangeIndex;
   modelColumn: GridRangeIndex;
-  selection: Selection | null;
+  selection?: Selection | null;
 }
 
 export type MouseHandlersProp = readonly (
